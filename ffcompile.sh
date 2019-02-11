410effd909851ae021756c23d0fc8032b83fd590
MetaFlow <metaflow@stanford.edu>
2019-02-11 18:12:00 +0000

    update script

--- ffcompile.sh
+++ ffcompile.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# GPU_ARCH must be one of fermi, k20, pascal, volta
+export GPU_ARCH=volta
+
+# set CUDA_HOME to the directoy of cuda, which should have subdirectories bin/, include/, and lib/
+export CUDA_HOME=/usr/local/cuda
+git submodule init
+git submodule update
+
 APP="$1"
 
 if [ -z "$APP" ]; then echo "Usage: ./ffcompile app"; exit; fi
