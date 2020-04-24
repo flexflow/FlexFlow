2c7639f0169f7ff7f9e055cb833c4b57768f7d7e
Wei Wu <eddy16112@gmail.com>
2020-04-24 14:18:19 -0400

    keras: lazy create layer

--- python/flexflow/keras/models/sequential.py
+++ python/flexflow/keras/models/sequential.py
@@ -11,41 +11,46 @@ class Sequential(object):
     
     self._layers = dict()
     self._nb_layers = 0
+    self.input_tensor = 0
     self.output_tensor = 0
+    
+  def _init_inout(self, input_tensor, label_tensor):
+    int_t = 0
+    out_t = 0
+    self.input_tensor = input_tensor
+    for layer_id in self._layers:
+      layer = self._layers[layer_id]
+      if (layer_id == 0):
+        in_t = input_tensor
+      else:
+        in_t = out_t
+        
+      if (isinstance(layer, Conv2D) == True):
+        out_t = self.ffmodel.conv2d(layer.name, in_t, layer.out_channels, layer.kernel_size, layer.kernel_size, layer.stride, layer.stride, layer.padding, layer.padding)
+      elif (isinstance(layer, MaxPooling2D) == True):
+        out_t = self.ffmodel.pool2d(layer.name, in_t, layer.kernel_size, layer.kernel_size, layer.stride, layer.stride, layer.padding, layer.padding)
+      elif (isinstance(layer, Flatten) == True):
+        out_t = self.ffmodel.flat(layer.name, in_t)
+      elif (isinstance(layer, Dense) == True):
+        out_t = self.ffmodel.dense(layer.name, in_t, layer.out_channels, layer.activation)
+      elif (isinstance(layer, Activation) == True):
+        assert layer_id == self._nb_layers-1, "softmax is not in the last layer"
+        out_t = self.ffmodel.softmax("softmax", in_t, label_tensor)
+      layer.handle = self.ffmodel.get_layer_by_id(layer_id)
+      print(layer.handle)
+    self.output_tensor = out_t
   
   def add(self, layer):
     self._layers[self._nb_layers] = layer
     self._nb_layers += 1
     
-    if (isinstance(layer, Conv2D) == True):
-      layer.handle = self.ffmodel.conv2d_v2(layer.name, layer.in_channels, layer.out_channels, layer.kernel_size, layer.kernel_size, layer.stride, layer.stride, layer.padding, layer.padding)
-    elif (isinstance(layer, MaxPooling2D) == True):
-      layer.handle = self.ffmodel.pool2d_v2(layer.name, layer.kernel_size, layer.kernel_size, layer.stride, layer.stride, layer.padding, layer.padding)
-    elif (isinstance(layer, Flatten) == True):
-      layer.handle = self.ffmodel.flat_v2(layer.name)
-    elif (isinstance(layer, Dense) == True):
-      layer.handle = self.ffmodel.dense_v2(layer.name, layer.in_channels, layer.out_channels, layer.activation)
-    elif (isinstance(layer, Activation) == True):
-      print("add softmax")
-    else:
-      assert 0, "unknow layer"
       
   def compile(self):
     self.ffoptimizer = ff.SGDOptimizer(self.ffmodel, 0.01)
     self.ffmodel.set_sgd_optimizer(self.ffoptimizer)
     
   def fit(self, input_tensor, label_tensor):
-    for layer_id in self._layers:
-      layer = self._layers[layer_id]
-      if (layer_id == 0):
-        self.output_tensor = layer.handle.init_inout(self.ffmodel, input_tensor);
-      else:
-        if (isinstance(layer, Activation) == True):
-          assert layer_id == self._nb_layers-1, "softmax is not in the last layer"
-          self.output_tensor = self.ffmodel.softmax("softmax", self.output_tensor, label_tensor)
-        else:
-          self.output_tensor = layer.handle.init_inout(self.ffmodel, self.output_tensor);
-        
+    self._init_inout(input_tensor, label_tensor)        
     self.ffmodel.init_layers()
     
     epochs = self.ffconfig.get_epochs()
@@ -57,7 +62,10 @@ class Sequential(object):
       for iter in range(0, int(iterations)):
         if (epoch > 0):
           self.ffconfig.begin_trace(111)
-        self.ffmodel.forward()
+        #self.ffmodel.forward()
+        for layer_id in self._layers:
+          layer = self._layers[layer_id]
+          layer.handle.forward(self.ffmodel)
         self.ffmodel.zero_gradients()
         self.ffmodel.backward()
         self.ffmodel.update()
