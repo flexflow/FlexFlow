eef625b9ad028612786b5d41ae939097421c9d07
Wei Wu <eddy16112@gmail.com>
2020-05-13 18:49:28 -0400

    keras: check point

--- python/example/keras/mnist_func.py
+++ python/example/keras/mnist_func.py
@@ -102,6 +102,42 @@ def cnn_concat():
   
   model.fit(x_train, y_train, epochs=1)
   
+def cifar_cnn():
+  num_classes = 10
+  
+  num_samples = 10000
+  
+  (x_train, y_train), (x_test, y_test) = cifar10.load_data(num_samples)
+  
+  x_train = x_train.astype('float32')
+  x_train /= 255
+  #x_train *= 0
+  #y_train = np.random.randint(1, 9, size=(num_samples,1), dtype='int32')
+  y_train = y_train.astype('int32')
+  print("shape: ", x_train.shape)
+  
+  input_tensor1 = Input(batch_shape=[0, 3, 32, 32], dtype="float32")
+  
+  output_tensor = Conv2D(filters=32, input_shape=(3,32,32), kernel_size=(3,3), strides=(1,1), padding=(1,1), activation="relu")(input_tensor1)
+  output_tensor = Conv2D(filters=32, kernel_size=(3,3), strides=(1,1), padding=(1,1), activation="relu")(output_tensor)
+  output_tensor = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="valid")(output_tensor)
+  output_tensor = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding=(1,1), activation="relu")(output_tensor)
+  output_tensor = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding=(1,1), activation="relu")(output_tensor)
+  output_tensor = MaxPooling2D(pool_size=(2,2), strides=(2,2), padding="valid")(output_tensor)
+  output_tensor = Flatten()(output_tensor)
+  output_tensor = Dense(512, activation="relu")(output_tensor)
+  output_tensor = Dense(num_classes)(output_tensor)
+  output_tensor = Activation("softmax")(output_tensor)
+
+  model = Model(input_tensor1, output_tensor)
+  
+  print(model.summary())
+  
+  opt = flexflow.keras.optimizers.SGD(learning_rate=0.01)
+  model.compile(optimizer=opt)
+
+  model.fit(x_train, y_train, epochs=1)
+  
 def cifar_cnn_concat():
   num_classes = 10
   
@@ -198,6 +234,7 @@ def top_level_task():
   
   #cnn()
   #cnn_concat()
+  #cifar_cnn()
   cifar_cnn_concat()
   #cifar_alexnet_concat()
   #mlp()
