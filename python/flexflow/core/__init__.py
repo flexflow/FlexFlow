8113f23e7f65bfec51aa3ec164674f9ba255be02
Wei Wu <eddy16112@gmail.com>
2020-06-03 17:35:31 -0400

    python: remove legion.py

--- python/flexflow/core/__init__.py
+++ python/flexflow/core/__init__.py
@@ -2,6 +2,5 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-from flexflow.core.legion import *
 from flexflow.core.flexflow_cbinding import *
 from flexflow.core.flexflow_top import flexflow_top_level_task, get_legion_runtime, get_legion_context
