5d23f4c6cbf09d62776380b69c0c887d2c115459
dan <dan@nod-labs.com>
2020-06-12 15:32:28 -0700

    [cmake] Added basic CMake support

---
+++ cmake/protobuf.cmake
@@ -0,0 +1,6 @@
+set(protobuf_BUILD_TESTS OFF CACHE BOOL "Disable tests for protobuf")
+set(BUILD_SHARED_LIBS OFF)
+set(LIBRARY_POLICY STATIC)
+add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/../protobuf/cmake ${CMAKE_CURRENT_BINARY_DIR}/protobuf)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/../protobuf/src)
+link_directories(${CMAKE_CURRENT_BINARY_DIR}/protobuf)
