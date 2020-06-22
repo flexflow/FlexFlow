74aabc14fcdc2c9010e52475fe00d4cf38840170
Wei Wu <eddy16112@gmail.com>
2020-06-22 15:51:36 -0400

    [Keras]: update test

--- python/test.sh
+++ python/test.sh
@@ -14,6 +14,7 @@ mpirun -np 1 ./flexflow_python example/keras/mnist_cnn.py -ll:py 1 -ll:gpu 1 -ll
 mpirun -np 1 ./flexflow_python example/keras/mnist_net2net.py -ll:py 1 -ll:gpu 1 -ll:fsize 2048 -ll:zsize 12192
 mpirun -np 1 ./flexflow_python example/keras/cifar10_cnn.py -ll:py 1 -ll:gpu 1 -ll:fsize 2048 -ll:zsize 12192
 mpirun -np 1 ./flexflow_python example/keras/reuters_mlp.py -ll:py 1 -ll:gpu 1 -ll:fsize 2048 -ll:zsize 12192
+mpirun -np 1 ./flexflow_python example/keras/mnist_seq_nested.py -ll:py 1 -ll:gpu 1 -ll:fsize 2048 -ll:zsize 12192
 mpirun -np 1 ./flexflow_python example/alexnet.py -ll:py 1 -ll:gpu 2 -ll:fsize 2048 -ll:zsize 12192
 mpirun -np 1 ./flexflow_python example/mlp.py -ll:py 1 -ll:gpu 1 -ll:fsize 2048 -ll:zsize 12192
 mpirun -np 1 ./flexflow_python example/cnn_attach.py -ll:py 1 -ll:gpu 1 -ll:fsize 2048 -ll:zsize 12192
