afb2d1f3f09b501f5e790d5b055b3bf5632bd812
nod <jiatian@nod-labs.com>
2020-07-19 01:40:57 -0700

    add mnist example

---
+++ examples/mnist/mnist.cc
@@ -0,0 +1,184 @@
+/* Copyright 2020 Stanford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "model.h"
+using namespace Legion;
+
+LegionRuntime::Logger::Category log_app("mnist");
+
+//void parse_input_args(char **argv, int argc, Inceptionv3Config& anConfig);
+
+class DataLoader {
+public:
+  DataLoader(FFModel& ff, Tensor input, Tensor label);
+  static void load_input(const Task *task,
+                         const std::vector<PhysicalRegion> &regions,
+                         Context ctx,
+                         Runtime* runtime);
+public:
+  int num_samples;
+};
+
+void top_level_task(const Task* task,
+                    const std::vector<PhysicalRegion>& regions,
+                    Context ctx, Runtime* runtime)
+{
+  FFConfig ffConfig;
+  {
+    const InputArgs &command_args = HighLevelRuntime::get_input_args();
+    char **argv = command_args.argv;
+    int argc = command_args.argc;
+    ffConfig.parse_args(argv, argc);
+    log_app.print("batchSize(%d) workersPerNodes(%d) numNodes(%d)",
+        ffConfig.batchSize, ffConfig.workersPerNode, ffConfig.numNodes);
+  }
+  ffConfig.lg_ctx = ctx;
+  ffConfig.lg_hlr = runtime;
+  ffConfig.field_space = runtime->create_field_space(ctx);
+  FFModel ff(ffConfig);
+
+  Tensor input;
+  {
+    const int dims[] = {ffConfig.batchSize, 1, 28, 28};
+    input = ff.create_tensor<4>(dims, "", DT_FLOAT);
+  }
+  Tensor label;
+  {
+    const int dims[] = {ffConfig.batchSize, 1};
+    label = ff.create_tensor<2>(dims, "", DT_INT32);
+  }
+ 
+//-----------------------------------------------------------------
+  Tensor t = ff.conv2d(input, 10, 5, 5, 1, 1, 0, 0);
+  t = ff.pool2d(t, 2, 2, 2, 2, 0, 0, POOL_MAX, AC_MODE_RELU);
+
+  t = ff.conv2d(t, 20, 5, 5, 1, 1, 0, 0);
+  t = ff.pool2d(t, 2, 2, 2, 2, 0, 0, POOL_MAX, AC_MODE_RELU);
+
+  t = ff.flat(t);
+
+  t = ff.dense(t, 50, AC_MODE_RELU);
+  t = ff.dense(t, 10);
+  t = ff.softmax(t, label);
+
+//-----------------------------------------------------------------
+  ff.optimizer = new SGDOptimizer(&ff, 0.01f, 0.5f);
+  ff.compile();
+
+  // Data Loader
+  DataLoader data_loader(ff, input, label);
+  ff.init_layers();
+  //Start timer
+  {
+    runtime->issue_execution_fence(ctx);
+    TimingLauncher timer(MEASURE_MICRO_SECONDS);
+    Future future = runtime->issue_timing_measurement(ctx, timer);
+    future.get_void_result();
+  }
+  double ts_start = Realm::Clock::current_time_in_microseconds();
+  for (int epoch = 0; epoch < ffConfig.epochs; epoch++) {
+    //data_loader.reset();
+    ff.reset_metrics();
+    int iterations = data_loader.num_samples / ffConfig.batchSize;
+ 
+    for (int iter = 0; iter < iterations; iter++) {
+      //if (dlrmConfig.dataset_path.length() == 0) {
+        // Only load data once for random input
+        //if (iter == 0 && epoch == 0)
+          //data_loader.next_batch(ff);
+      //} else {
+        //data_loader.next_batch(ff);
+      //}
+      if (epoch > 0)
+        runtime->begin_trace(ctx, 111/*trace_id*/);
+      ff.forward();
+      ff.zero_gradients();
+      ff.backward();
+      ff.update();
+      if (epoch > 0)
+        runtime->end_trace(ctx, 111/*trace_id*/);
+    }
+  }
+  // End timer
+  {
+    runtime->issue_execution_fence(ctx);
+    TimingLauncher timer(MEASURE_MICRO_SECONDS);
+    Future future = runtime->issue_timing_measurement(ctx, timer);
+    future.get_void_result();
+  }
+  double ts_end = Realm::Clock::current_time_in_microseconds();
+  double run_time = 1e-6 * (ts_end - ts_start);
+  printf("ELAPSED TIME = %.4fs, THROUGHPUT = %.2f samples/s\n", run_time,
+         8192 * ffConfig.epochs / run_time);
+}
+
+void register_custom_tasks()
+{
+  // Load Input
+  {
+    TaskVariantRegistrar registrar(CUSTOM_GPU_TASK_ID_1, "Load Inputs");
+    registrar.add_constraint(ProcessorConstraint(Processor::TOC_PROC));
+    registrar.set_leaf();
+    Runtime::preregister_task_variant<DataLoader::load_input>(
+        registrar, "Load Inputs Task");
+  }
+}
+
+DataLoader::DataLoader(FFModel& ff,
+                       Tensor input, Tensor label)
+{
+  Context ctx = ff.config.lg_ctx;
+  Runtime* runtime = ff.config.lg_hlr;
+  num_samples = 0;
+  log_app.print("Use random dataset...");
+  num_samples = 256 * ff.config.workersPerNode * ff.config.numNodes;
+  log_app.print("Number of random samples = %d\n", num_samples);
+  // Init input
+  {
+    IndexSpaceT<4> task_is = IndexSpaceT<4>(ff.get_or_create_task_is(4, ""));
+    ArgumentMap argmap;
+    IndexLauncher launcher(CUSTOM_GPU_TASK_ID_1, task_is,
+                           TaskArgument(NULL, 0), argmap,
+                           Predicate::TRUE_PRED, false/*must*/, 0/*mapper_id*/,
+                           FFConfig::get_hash_id(std::string("")));
+    launcher.add_region_requirement(
+        RegionRequirement(input.part, 0/*projection id*/,
+                          WRITE_ONLY, EXCLUSIVE, input.region));
+    launcher.add_field(0, FID_DATA);
+    runtime->execute_index_space(ctx, launcher);
+  }
+  // Init label
+  {
+    IndexSpaceT<2> task_is = IndexSpaceT<2>(ff.get_or_create_task_is(2, ""));
+    ArgumentMap argmap;
+    IndexLauncher launcher(CUSTOM_GPU_TASK_ID_1, task_is,
+                           TaskArgument(NULL, 0), argmap,
+                           Predicate::TRUE_PRED, false/*must*/, 0/*mapper_id*/,
+                           FFConfig::get_hash_id(std::string("")));
+    launcher.add_region_requirement(
+        RegionRequirement(label.part, 0/*projection id*/,
+                          WRITE_ONLY, EXCLUSIVE, label.region));
+    launcher.add_field(0, FID_DATA);
+    runtime->execute_index_space(ctx, launcher);
+  }
+}
+
+void DataLoader::load_input(const Task *task,
+                            const std::vector<PhysicalRegion> &regions,
+                            Context ctx,
+                            Runtime* runtime)
+{
+  printf("CheckPoint#1\n");
+}
