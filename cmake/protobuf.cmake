df6437b0e3dbf8553523afb53987ebc8729d5154
Wei Wu <eddy16112@gmail.com>
2020-09-14 20:27:56 -0400

    [CMake]: fix gasnet

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -2,8 +2,8 @@ list(APPEND CMAKE_PREFIX_PATH ${PROTOBUF_ROOT})
 find_package(Protobuf REQUIRED)
 
 if(Protobuf_FOUND)
-  list(APPEND FLEXFLOW_EXT_LIBRARIES 
-    ${Protobuf_LIBRARIES})
+  list(APPEND FLEXFLOW_EXT_LIBRARIES ${Protobuf_LIBRARIES})
+  include_directories(${Protobuf_INCLUDE_DIRS})
   message( STATUS "Protobuf version : ${Protobuf_VERSION}" )
   message( STATUS "Protobuf include path : ${Protobuf_INCLUDE_DIRS}" )
   message( STATUS "Protobuf libraries : ${Protobuf_LIBRARIES}" )
@@ -11,6 +11,5 @@ if(Protobuf_FOUND)
 else()
   message( WARNING "Protobuf package not found -> specify search path via PROTOBUF_ROOT variable")
 endif()
-include_directories(${Protobuf_INCLUDE_DIRS})
 
 
