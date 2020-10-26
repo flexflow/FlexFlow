6abdf082dfdb66c4e4ce6de8251caf2d7e207eaf
Wei Wu <eddy16112@gmail.com>
2020-10-26 15:40:38 -0600

    [Pytorch]: check point

--- python/flexflow/torch/nn/modules/__init__.py
+++ python/flexflow/torch/nn/modules/__init__.py
@@ -1,6 +1,8 @@
 from .module import Module
-from .conv import Conv2d
-from .pooling import AvgPool2d, MaxPool2d
-from .linear import Linear
-from .flatten import Flatten
-from .activation import ReLU
+
+from torch.nn import Conv2d as Conv2d
+from torch.nn import MaxPool2d as MaxPool2d
+from torch.nn import Linear as Linear
+from torch.nn import Dropout as Dropout
+from torch.nn import Flatten as Flatten
+from torch.nn import ReLU as ReLU
