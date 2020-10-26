bdfa88e74315001cbe7590028ed31fa162429c39
Wei Wu <eddy16112@gmail.com>
2020-10-26 18:18:39 -0400

    [Pytorch]: monior fix

--- python/flexflow/torch/nn/modules/module.py
+++ python/flexflow/torch/nn/modules/module.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
-
+import flexflow.core as ff
 import flexflow.torch.fx as fx
 
 class Module(nn.Module):
   def __init__(self):
     super(Module, self).__init__()
-    self._ffmodel = None
+    self._ffconfig = ff.FFConfig()
+    self._ffconfig.parse_args()
+    self._ffmodel = ff.FFModel(self._ffconfig)
     self._graph = None
   
   def __call__(self, input):
