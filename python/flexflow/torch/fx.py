eb33a1a3026debfdc93850cb17e5bb8adb65f7de
Wei Wu <eddy16112@gmail.com>
2020-10-27 23:32:44 -0400

    [Pytorch]: minor fix

--- python/flexflow/torch/fx.py
+++ python/flexflow/torch/fx.py
@@ -95,14 +95,14 @@ def torch_to_flexflow(model, filename):
       function_name = str(node.function)
       if function_name.find('add') >= 0:
         assert len(node.inedges) == 2, "wrong number of inputs"
-        op_str = op_str + str(enum_to_int(OpType, OpType.ADD)) + ", "
+        op_str = op_str + str(enum_to_int(OpType, OpType.ADD)) + "\n"
       elif function_name.find('cat') >= 0:
         #FIXME assume it is a merge
         op_str = op_str + str(enum_to_int(OpType, OpType.CONCAT)) + ", "
         op_str = op_str + str(node.inedges[1]) + "\n"
       elif function_name.find('flatten') >= 0:
         assert len(node.inedges) == 1, "wrong number of inputs"
-        op_str = op_str + str(enum_to_int(OpType, OpType.FLAT)) + ", "
+        op_str = op_str + str(enum_to_int(OpType, OpType.FLAT)) + "\n"
       else:
         # Unrecogonized type
         assert False, "Unrecogonized built-in function: {}".format(function_name)
