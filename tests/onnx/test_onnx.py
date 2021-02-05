fefecaebd635fc5b7a435f9619c0f9249e1beb7c
Efrainq07 <efrainq07@hotmail.com>
2021-02-05 11:48:52 -0800

    [CircleCI] Pip install flexflow, onnx and torch on config.yml

--- tests/onnx/test_onnx.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__))+'//..//..')
-import python.flexflow.onnx.model as model
-
-def test_onnx_init():
-	model.ONNXModel("mnist_mlp.onnx")
