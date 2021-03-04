b977df3a3e9da30cf345aa8f52d7a4bf9059e70e
Wei Wu <eddy16112@gmail.com>
2021-03-03 22:49:21 -0700

    [CMake]: add missing files

---
+++ cmake/cudnn.cmake
@@ -0,0 +1,31 @@
+# find cudnn in CUDNN_ROOT and CUDA_ROOT
+find_library(CUDNN_LIBRARY 
+  NAMES libcudnn${LIBEXT}
+  PATHS ${CUDNN_ROOT} ${CUDA_ROOT}
+  PATH_SUFFIXES lib lib64
+  DOC "CUDNN library." )
+  
+find_path(CUDNN_INCLUDE_DIR 
+    NAMES cudnn.h
+    HINTS ${CUDNN_ROOT} ${CUDA_ROOT}
+    PATH_SUFFIXES include 
+    DOC "CUDNN include directory." )
+
+# find cudnn, set cudnn lib and include    
+if(CUDNN_LIBRARY AND CUDNN_INCLUDE_DIR)
+  set(CUDNN_FOUND ON)
+  set(CUDNN_LIBRARIES ${CUDNN_LIBRARY})
+  set(CUDNN_INCLUDE_DIRS ${CUDNN_INCLUDE_DIR})
+endif()
+
+# find cuda and cudnn
+if(CUDNN_FOUND)
+  list(APPEND FLEXFLOW_EXT_LIBRARIES
+    ${CUDNN_LIBRARIES})
+endif()
+
+if(CUDNN_FOUND)
+  message( STATUS "CUDNN libraries : ${CUDNN_LIBRARIES}" )
+else()
+  message( FATAL_ERROR "CUDNN package not found -> specify search path via CUDNN_ROOT variable")
+endif()
