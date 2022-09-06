32d10351292d41d6a8ae5f295a644b887ce9a9a1
virena <vabhyank@ucsd.edu>
2022-09-06 19:05:40 +0000

    Formatting

--- include/flexflow/parallel_ops/reduction.h
+++ include/flexflow/parallel_ops/reduction.h
@@ -24,7 +24,7 @@ public:
             char const *name = NULL);
   Reduction(FFModel &model,
             Params const &params,
-            const Input input,
+            Input const input,
             char const *name = nullptr);
   void create_input_partition(FFModel &model) override;
   void init(FFModel const &) override;
