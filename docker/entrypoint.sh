c0ba73c19eb86f9c715bb83951bff7e3facb0845
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-09-28 17:53:12 -0400

    updated dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,8 +3,10 @@
 FIRST_BOOT_COMPLETE="first_boot_complete"
 
 if [ ! -f "/usr/$FIRST_BOOT_COMPLETE" ]; then
+	cores_available=$(nproc --all)
+	n_build_cores=$(( cores_available -1 )) 
 	echo "Building FlexFlow..."
-	cd /usr/FlexFlow && mkdir -p build && cd build && ../config/config.linux && make -j
+	cd /usr/FlexFlow && mkdir -p build && cd build && ../config/config.linux && make -j $n_build_cores
 	echo "Installing FlexFlow..."
     make install
     echo "Installation of FlexFlow successfully completed!"
