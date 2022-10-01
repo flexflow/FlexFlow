ddb239383371aeb4ea9d52b1902b301c5cd31fca
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-10-01 13:56:15 -0400

    update to python3

--- python/legion_cffi_build.py
+++ python/legion_cffi_build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 # Copyright 2020 Stanford University, Los Alamos National Laboratory
 #
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import argparse
 import os
