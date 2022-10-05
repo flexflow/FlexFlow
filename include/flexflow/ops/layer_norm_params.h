f758daf492c77ec1a617383914df327cc25f82fe
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-10-05 10:44:43 -0400

    Revert "added declaration"
    This reverts commit a9a88acc38c1d25d4815f4854552fee15b637d5b.

--- include/flexflow/ops/layer_norm_params.h
+++ include/flexflow/ops/layer_norm_params.h
@@ -10,7 +10,6 @@ struct LayerNormParams {
   bool elementwise_affine;
   float eps;
   bool is_valid(ParallelTensorShape const &) const;
-  size_t get_hash(const ParallelTensor input) const;
 };
 
 bool operator==(LayerNormParams const &, LayerNormParams const &);
