76ed00de6a9de89501307df410ac405782485710
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-10-11 14:43:42 -0400

    updated build script

--- examples/python/pytorch/mt5/build.sh
+++ examples/python/pytorch/mt5/build.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 # Cd into directory holding this script
 cd "$( echo "${BASH_SOURCE[0]%/*}" )"
 
+# (Idemponent command) Build FlexFlow docker container
+../../../../docker/build.sh
+
+# Build Docker-MT5 container
 docker build -t flexflow-mt5 .
