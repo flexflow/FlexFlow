3e604fd587d3719bc733473c961195a3658bb954
Mengdi Wu <48128384+wmdi@users.noreply.github.com>
2022-10-18 14:41:28 +0800

    Fix parallel operators measure_operator_cost function (#342)
    * set sync_time to 0 for all the parallel ops
    
    * [CostMetrics] Initialize sync_time,, inpuuts_memory, outpputs_memory and weights_memory by 0
    
    * Remove potential confusion around initialization state in parallel ops
    
    Co-authored-by: lockshaw <mastakata3@yahoo.com>

--- src/parallel_ops/reduction.cc
+++ src/parallel_ops/reduction.cc
@@ -180,6 +180,7 @@ void Reduction::backward(FFModel const &ff) {
 bool Reduction::measure_operator_cost(Simulator *sim,
                                       MachineView const &pc,
                                       CostMetrics &cost_metrics) const {
+  cost_metrics = CostMetrics();
   cost_metrics.forward_time = 0.0f;
   cost_metrics.backward_time = 0.0f;
   return true;
