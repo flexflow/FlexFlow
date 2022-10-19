ab7cc8cc91b4a818b7edc4814beb2939ae04005d
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-10-18 23:43:44 -0400

    Revert "formatting"
    This reverts commit 9c781875042704a838ae2c845efe2caca468b829.

--- examples/python/onnx/cifar10_cnn.py
+++ examples/python/onnx/cifar10_cnn.py
@@ -5,85 +5,66 @@ import argparse
 
 from accuracy import ModelAccuracy
 
-
 def top_level_task(test_type=1):
-    ffconfig = FFConfig()
-    alexnetconfig = NetConfig()
-    print(alexnetconfig.dataset_path)
-    print(
-        "Python API batchSize(%d) workersPerNodes(%d) numNodes(%d)"
-        % (ffconfig.batch_size, ffconfig.workers_per_node, ffconfig.num_nodes)
-    )
-    ffmodel = FFModel(ffconfig)
+  ffconfig = FFConfig()
+  alexnetconfig = NetConfig()
+  print(alexnetconfig.dataset_path)
+  print("Python API batchSize(%d) workersPerNodes(%d) numNodes(%d)" %(ffconfig.batch_size, ffconfig.workers_per_node, ffconfig.num_nodes))
+  ffmodel = FFModel(ffconfig)
+  
+  dims_input = [ffconfig.batch_size, 3, 32, 32]
+  input = ffmodel.create_tensor(dims_input, DataType.DT_FLOAT)
 
-    dims_input = [ffconfig.batch_size, 3, 32, 32]
-    input = ffmodel.create_tensor(dims_input, DataType.DT_FLOAT)
+  if test_type == 1:
+    onnx_model = ONNXModel("cifar10_cnn_pt.onnx")
+    t = onnx_model.apply(ffmodel, {"input.1": input})
+  else:
+    onnx_model = ONNXModelKeras("cifar10_cnn_keras.onnx", ffconfig, ffmodel)
+    t = onnx_model.apply(ffmodel, {"input_1": input})
 
-    if test_type == 1:
-        onnx_model = ONNXModel("cifar10_cnn_pt.onnx")
-        t = onnx_model.apply(ffmodel, {"input.1": input})
-    else:
-        onnx_model = ONNXModelKeras("cifar10_cnn_keras.onnx", ffconfig, ffmodel)
-        t = onnx_model.apply(ffmodel, {"input_1": input})
+  ffoptimizer = SGDOptimizer(ffmodel, 0.01)
+  ffmodel.optimizer = ffoptimizer
+  ffmodel.compile(loss_type=LossType.LOSS_SPARSE_CATEGORICAL_CROSSENTROPY, metrics=[MetricsType.METRICS_ACCURACY, MetricsType.METRICS_SPARSE_CATEGORICAL_CROSSENTROPY])
+  label = ffmodel.label_tensor
+  
+  num_samples = 10000
+  
+  (x_train, y_train), (x_test, y_test) = cifar10.load_data(num_samples)
+  
+  x_train = x_train.astype('float32')
+  x_train /= 255
+  full_input_array = x_train
+  print(full_input_array.__array_interface__["strides"])
+  
+  y_train = y_train.astype('int32')
+  full_label_array = y_train
+  
+  dataloader_input = ffmodel.create_data_loader(input, full_input_array)
+  dataloader_label = ffmodel.create_data_loader(label, full_label_array)
+  
+  num_samples = dataloader_input.num_samples
 
-    ffoptimizer = SGDOptimizer(ffmodel, 0.01)
-    ffmodel.optimizer = ffoptimizer
-    ffmodel.compile(
-        loss_type=LossType.LOSS_SPARSE_CATEGORICAL_CROSSENTROPY,
-        metrics=[
-            MetricsType.METRICS_ACCURACY,
-            MetricsType.METRICS_SPARSE_CATEGORICAL_CROSSENTROPY,
-        ],
-    )
-    label = ffmodel.label_tensor
+  ffmodel.init_layers()
 
-    num_samples = 10000
+  epochs = ffconfig.epochs
 
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data(num_samples)
+  ts_start = ffconfig.get_current_time()
+  
+  ffmodel.fit(x=dataloader_input, y=dataloader_label, epochs=epochs)
 
-    x_train = x_train.astype("float32")
-    x_train /= 255
-    full_input_array = x_train
-    print(full_input_array.__array_interface__["strides"])
-
-    y_train = y_train.astype("int32")
-    full_label_array = y_train
-
-    dataloader_input = ffmodel.create_data_loader(input, full_input_array)
-    dataloader_label = ffmodel.create_data_loader(label, full_label_array)
-
-    num_samples = dataloader_input.num_samples
-
-    ffmodel.init_layers()
-
-    epochs = ffconfig.epochs
-
-    ts_start = ffconfig.get_current_time()
-
-    ffmodel.fit(x=dataloader_input, y=dataloader_label, epochs=epochs)
-
-    ts_end = ffconfig.get_current_time()
-    run_time = 1e-6 * (ts_end - ts_start)
-    print(
-        "epochs %d, ELAPSED TIME = %.4fs, THROUGHPUT = %.2f samples/s\n"
-        % (epochs, run_time, num_samples * epochs / run_time)
-    )
-
-    perf_metrics = ffmodel.get_perf_metrics()
-    accuracy = perf_metrics.get_accuracy()
-    if accuracy < ModelAccuracy.CIFAR10_CNN.value:
-        assert 0, "Check Accuracy"
-
+  ts_end = ffconfig.get_current_time()
+  run_time = 1e-6 * (ts_end - ts_start);
+  print("epochs %d, ELAPSED TIME = %.4fs, THROUGHPUT = %.2f samples/s\n" %(epochs, run_time, num_samples * epochs / run_time));
+  
+  perf_metrics = ffmodel.get_perf_metrics()
+  accuracy = perf_metrics.get_accuracy()
+  if accuracy < ModelAccuracy.CIFAR10_CNN.value:
+    assert 0, 'Check Accuracy'
 
 if __name__ == "__main__":
-    print("cifar10 cnn onnx")
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--test_type",
-        type=int,
-        choices=[0, 1],
-        help="Whether to test using Keras (test_type 0) or PyTorch (test_type 1) ",
-    )
-    args, unknown = parser.parse_known_args()
-    test_type = args.test_type
-    top_level_task(test_type)
+  print("cifar10 cnn onnx")
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--test_type', type=int, choices=[0, 1], help="Whether to test using Keras (test_type 0) or PyTorch (test_type 1) ")
+  args, unknown = parser.parse_known_args()
+  test_type = args.test_type
+  top_level_task(test_type)
--- examples/python/onnx/mnist_mlp.py
+++ examples/python/onnx/mnist_mlp.py
@@ -6,79 +6,60 @@ import argparse
 
 from accuracy import ModelAccuracy
 
-
 def top_level_task(test_type=1):
-    ffconfig = FFConfig()
-    print(
-        "Python API batchSize(%d) workersPerNodes(%d) numNodes(%d)"
-        % (ffconfig.batch_size, ffconfig.workers_per_node, ffconfig.num_nodes)
-    )
-    ffmodel = FFModel(ffconfig)
+  ffconfig = FFConfig()
+  print("Python API batchSize(%d) workersPerNodes(%d) numNodes(%d)" %(ffconfig.batch_size, ffconfig.workers_per_node, ffconfig.num_nodes))
+  ffmodel = FFModel(ffconfig)
+  
+  dims1 = [ffconfig.batch_size, 784]
+  input1 = ffmodel.create_tensor(dims1, DataType.DT_FLOAT);
+  
+  num_samples = 60000
+  
+  if test_type == 1:
+    onnx_model = ONNXModel("mnist_mlp_pt.onnx")
+    t = onnx_model.apply(ffmodel, {"input.1": input1})
+  else:
+    onnx_model = ONNXModelKeras("mnist_mlp_keras.onnx", ffconfig, ffmodel)
+    t = onnx_model.apply(ffmodel, {"input_1": input1})
 
-    dims1 = [ffconfig.batch_size, 784]
-    input1 = ffmodel.create_tensor(dims1, DataType.DT_FLOAT)
+  ffoptimizer = SGDOptimizer(ffmodel, 0.01)
+  ffmodel.optimizer = ffoptimizer
+  ffmodel.compile(loss_type=LossType.LOSS_SPARSE_CATEGORICAL_CROSSENTROPY, metrics=[MetricsType.METRICS_ACCURACY, MetricsType.METRICS_SPARSE_CATEGORICAL_CROSSENTROPY])
+  label = ffmodel.label_tensor
 
-    num_samples = 60000
+  (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    if test_type == 1:
-        onnx_model = ONNXModel("mnist_mlp_pt.onnx")
-        t = onnx_model.apply(ffmodel, {"input.1": input1})
-    else:
-        onnx_model = ONNXModelKeras("mnist_mlp_keras.onnx", ffconfig, ffmodel)
-        t = onnx_model.apply(ffmodel, {"input_1": input1})
+  x_train = x_train.reshape(60000, 784)
+  x_train = x_train.astype('float32')
+  x_train /= 255
+  y_train = y_train.astype('int32')
+  y_train = np.reshape(y_train, (len(y_train), 1))
 
-    ffoptimizer = SGDOptimizer(ffmodel, 0.01)
-    ffmodel.optimizer = ffoptimizer
-    ffmodel.compile(
-        loss_type=LossType.LOSS_SPARSE_CATEGORICAL_CROSSENTROPY,
-        metrics=[
-            MetricsType.METRICS_ACCURACY,
-            MetricsType.METRICS_SPARSE_CATEGORICAL_CROSSENTROPY,
-        ],
-    )
-    label = ffmodel.label_tensor
+  dataloader_input = ffmodel.create_data_loader(input1, x_train)
+  dataloader_label = ffmodel.create_data_loader(label, y_train)
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+  ffmodel.init_layers()
 
-    x_train = x_train.reshape(60000, 784)
-    x_train = x_train.astype("float32")
-    x_train /= 255
-    y_train = y_train.astype("int32")
-    y_train = np.reshape(y_train, (len(y_train), 1))
+  epochs = ffconfig.epochs
 
-    dataloader_input = ffmodel.create_data_loader(input1, x_train)
-    dataloader_label = ffmodel.create_data_loader(label, y_train)
+  ts_start = ffconfig.get_current_time()
 
-    ffmodel.init_layers()
+  ffmodel.fit(x=dataloader_input, y=dataloader_label, epochs=epochs)
 
-    epochs = ffconfig.epochs
+  ts_end = ffconfig.get_current_time()
+  run_time = 1e-6 * (ts_end - ts_start);
+  print("epochs %d, ELAPSED TIME = %.4fs, THROUGHPUT = %.2f samples/s\n" %(epochs, run_time, num_samples * epochs / run_time));
 
-    ts_start = ffconfig.get_current_time()
-
-    ffmodel.fit(x=dataloader_input, y=dataloader_label, epochs=epochs)
-
-    ts_end = ffconfig.get_current_time()
-    run_time = 1e-6 * (ts_end - ts_start)
-    print(
-        "epochs %d, ELAPSED TIME = %.4fs, THROUGHPUT = %.2f samples/s\n"
-        % (epochs, run_time, num_samples * epochs / run_time)
-    )
-
-    perf_metrics = ffmodel.get_perf_metrics()
-    accuracy = perf_metrics.get_accuracy()
-    if accuracy < ModelAccuracy.MNIST_MLP.value:
-        assert 0, "Check Accuracy"
-
-
+  perf_metrics = ffmodel.get_perf_metrics()
+  accuracy = perf_metrics.get_accuracy()
+  if accuracy < ModelAccuracy.MNIST_MLP.value:
+    assert 0, 'Check Accuracy'
+  
 if __name__ == "__main__":
-    print("mnist mlp onnx")
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--test_type",
-        type=int,
-        choices=[0, 1],
-        help="Whether to test using Keras (test_type 0) or PyTorch (test_type 1) ",
-    )
-    args, unknown = parser.parse_known_args()
-    test_type = args.test_type
-    top_level_task(test_type)
+  print("mnist mlp onnx")
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--test_type', type=int, choices=[0, 1], help="Whether to test using Keras (test_type 0) or PyTorch (test_type 1) ")
+  args, unknown = parser.parse_known_args()
+  test_type = args.test_type
+  top_level_task(test_type)
