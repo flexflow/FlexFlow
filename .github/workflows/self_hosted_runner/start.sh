d87edfc2a4a08dd0570e241ebd00ff6dd2fb808c
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-10-27 04:47:10 +0000

    fix start script

--- .github/workflows/self_hosted_runner/start.sh
+++ .github/workflows/self_hosted_runner/start.sh
@@ -5,7 +5,7 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 
 REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner
+cd /home/actions-runner
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
