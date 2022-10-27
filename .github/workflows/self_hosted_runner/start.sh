293a84a41c4c8f4de3a759037d2badb74f115ff5
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-10-27 05:07:07 +0000

    fixes

--- .github/workflows/self_hosted_runner/start.sh
+++ .github/workflows/self_hosted_runner/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+set -x
 ORGANIZATION=$ORGANIZATION
-ACCESS_TOKEN=$ACCESS_TOKEN
+#ACCESS_TOKEN=$ACCESS_TOKEN
+REG_TOKEN=$REG_TOKEN
+#REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
-
-cd /home/actions-runner
+cd /home/docker/actions-runner
 
 ./config.sh --url https://github.com/${ORGANIZATION} --token ${REG_TOKEN}
 
