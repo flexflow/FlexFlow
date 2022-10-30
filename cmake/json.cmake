81c193ee86711830ede69f34aabf6a0177d4ac89
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-10-30 17:05:35 -0400

    small edits

--- cmake/json.cmake
+++
@@ -1,4 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(json URL https://github.com/nlohmann/json/releases/download/v3.11.2/json.tar.xz)
-FetchContent_MakeAvailable(json)
--- cmake/variant.cmake
+++ cmake/variant.cmake
@@ -1,9 +1,3 @@
-# add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/variant)
-
-# list(APPEND FLEXFLOW_EXT_LIBRARIES mpark_variant)
-# list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/deps/variant/include/)
-
 FetchContent_Declare(variant GIT_REPOSITORY https://github.com/mpark/variant)
 FetchContent_MakeAvailable(variant)
-#list(APPEND FLEXFLOW_EXT_LIBRARIES mpark_variant)
 list(APPEND FLEXFLOW_INCLUDE_DIRS ${variant_SOURCE_DIR}/include/)
