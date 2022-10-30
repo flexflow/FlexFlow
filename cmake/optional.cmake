ce45f4582c77272bbe49385531709b4ef30d95eb
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-10-30 14:36:34 -0500

    making available optional, variant libraries using FetchContent

--- cmake/optional.cmake
+++ cmake/optional.cmake
@@ -1,7 +1,9 @@
 set(OPTIONAL_BUILD_TESTS OFF)
 set(OPTIONAL_BUILD_PACKAGE OFF)
 
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/optional)
+# add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/optional)
 
-list(APPEND FLEXFLOW_EXT_LIBRARIES optional)
-list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/deps/optional/include/)
+# list(APPEND FLEXFLOW_EXT_LIBRARIES optional)
+# list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/deps/optional/include/)
+FetchContent_Declare(optional GIT_REPOSITORY https://github.com/TartanLlama/optional.git)
+FetchContent_MakeAvailable(optional)
--- cmake/variant.cmake
+++ cmake/variant.cmake
@@ -1,4 +1,7 @@
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/variant)
+# add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/variant)
 
-list(APPEND FLEXFLOW_EXT_LIBRARIES mpark_variant)
-list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/deps/variant/include/)
+# list(APPEND FLEXFLOW_EXT_LIBRARIES mpark_variant)
+# list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/deps/variant/include/)
+
+FetchContent_Declare(variant GIT_REPOSITORY https://github.com/mpark/variant)
+FetchContent_MakeAvailable(variant)
