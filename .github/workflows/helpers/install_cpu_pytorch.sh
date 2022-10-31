8ee064454a7e90dce22cee82987f5a9f0f842b39
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-10-31 18:28:06 -0400

    added separate file for pytorch dependencies

---
+++ .github/workflows/helpers/install_cpu_pytorch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive
+
+echo "Installing PyTorch dependencies"
+rm -rf /var/lib/apt/lists/* \
+/etc/apt/sources.list.d/cuda.list \
+/etc/apt/sources.list.d/nvidia-ml.list && \
+apt-get update && \
+DEBIAN_FRONTEND=noninteractive \
+	apt-get install -y --no-install-recommends \
+    software-properties-common && \
+apt-get update && \
+DEBIAN_FRONTEND=noninteractive \
+	apt-get install -y --no-install-recommends \
+    build-essential \
+    apt-utils \
+    ca-certificates \
+    libssl-dev \
+    curl \
+    unzip \
+    htop \
+    nano && \
+DEBIAN_FRONTEND=noninteractive \
+    apt-get install -y software-properties-common && \
+    add-apt-repository ppa:ubuntu-toolchain-r/test && \
+    apt-get update -y && \
+    apt-get upgrade -y libstdc++6
+
+# Install CPU-only Pytorch
+echo "Installing PyTorch and related packages"
+/opt/conda/bin/conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cpuonly -c pytorch
+# Install Hugging-face/MT5 related packages
+/opt/conda/bin/conda install -c conda-forge pip pandas numpy transformers=4.16.2 sentencepiece
+# Install packages required by other example applications
+pip install onnx tensorflow keras2onnx
--- .github/workflows/helpers/install_dependencies.sh
+++ .github/workflows/helpers/install_dependencies.sh
@@ -26,38 +26,3 @@ wget -c -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 echo "Installing conda packages..."
 /opt/conda/bin/conda install cmake make pillow
 /opt/conda/bin/conda install -c conda-forge numpy keras-preprocessing pybind11 cmake-build-extension
-
-DEBIAN_FRONTEND=noninteractive
-
-# Install basic packages
-rm -rf /var/lib/apt/lists/* \
-    /etc/apt/sources.list.d/cuda.list \
-    /etc/apt/sources.list.d/nvidia-ml.list && \
-    apt-get update && \
-    DEBIAN_FRONTEND=noninteractive \
-        apt-get install -y --no-install-recommends \
-        software-properties-common && \
-    apt-get update && \
-    DEBIAN_FRONTEND=noninteractive \
-        apt-get install -y --no-install-recommends \
-        build-essential \
-        apt-utils \
-        ca-certificates \
-        libssl-dev \
-        curl \
-        unzip \
-        htop \
-        nano && \
-    DEBIAN_FRONTEND=noninteractive \
-        apt-get install -y software-properties-common && \
-        add-apt-repository ppa:ubuntu-toolchain-r/test && \
-        apt-get update -y && \
-        apt-get upgrade -y libstdc++6
-
-# Install CPU-only Pytorch
-/opt/conda/bin/conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cpuonly -c pytorch
-# Install Hugging-face/MT5 related packages
-/opt/conda/bin/conda install -c conda-forge pandas numpy transformers=4.16.2 sentencepiece
-# Install packages required by other example applications
-pip install onnx tensorflow keras2onnx
-
