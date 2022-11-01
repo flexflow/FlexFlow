538a4279e65a9b9d8ea234203f5d1278687ace8a
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-01 00:50:37 -0400

    first attempt

--- cmake/nccl.cmake
+++ cmake/nccl.cmake
@@ -4,21 +4,32 @@ set(NCCL_NAME nccl)
 
 list(TRANSFORM CUDA_GENCODE PREPEND "NVCC_GENCODE=" OUTPUT_VARIABLE NCCL_BUILD_NVCC_GENCODE)
 
-ExternalProject_Add(${NCCL_NAME}
- SOURCE_DIR ${PROJECT_SOURCE_DIR}/deps/${NCCL_NAME}
- PREFIX ${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}
- INSTALL_DIR ${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}
- BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}/lib/libnccl${LIBEXT}
- INSTALL_COMMAND ""
- CONFIGURE_COMMAND ""
- BUILD_COMMAND make src.build "${NCCL_BUILD_NVCC_GENCODE}" "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}" "BUILDDIR=${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}"
- BUILD_IN_SOURCE 1
-)
+# ExternalProject_Add(${NCCL_NAME}
+#  SOURCE_DIR ${PROJECT_SOURCE_DIR}/deps/${NCCL_NAME}
+#  PREFIX ${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}
+#  INSTALL_DIR ${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}
+#  BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}/lib/libnccl${LIBEXT}
+#  INSTALL_COMMAND ""
+#  CONFIGURE_COMMAND ""
+#  BUILD_COMMAND make src.build "${NCCL_BUILD_NVCC_GENCODE}" "CUDA_HOME=${CUDA_TOOLKIT_ROOT_DIR}" "BUILDDIR=${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}"
+#  BUILD_IN_SOURCE 1
+# )
 
-ExternalProject_Get_Property(${NCCL_NAME} INSTALL_DIR)
-message(STATUS "NCCL install dir: ${INSTALL_DIR}")
+# ExternalProject_Get_Property(${NCCL_NAME} INSTALL_DIR)
+# message(STATUS "NCCL install dir: ${INSTALL_DIR}")
+# list(APPEND FLEXFLOW_INCLUDE_DIRS
+#   ${INSTALL_DIR}/include)
+# list(APPEND FLEXFLOW_EXT_LIBRARIES
+#   ${INSTALL_DIR}/lib/libnccl${LIBEXT})
+# set_directory_properties(PROPERTIES ADDITIONAL_CLEAN_FILES "${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}/lib/")
+
+include(FetchContent)
+FetchContent_Declare(nccl URL https://github.com/gabrieleoliaro/flexflow-third-party/releases/latest/download/nccl_11.1.1_ubuntu-20.04.tar.gz)
+FetchContent_MakeAvailable(nccl)
+list(APPEND FLEXFLOW_INCLUDE_DIRS ${nccl_SOURCE_DIR}/include/)
+
 list(APPEND FLEXFLOW_INCLUDE_DIRS
-  ${INSTALL_DIR}/include)
+  ${nccl_SOURCE_DIR}/include)
 list(APPEND FLEXFLOW_EXT_LIBRARIES
-  ${INSTALL_DIR}/lib/libnccl${LIBEXT})
-set_directory_properties(PROPERTIES ADDITIONAL_CLEAN_FILES "${CMAKE_BINARY_DIR}/deps/${NCCL_NAME}/lib/")
+  ${nccl_SOURCE_DIR}/lib/libnccl${LIBEXT})
+
