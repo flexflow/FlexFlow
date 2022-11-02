312aba814a301bb0c12047dafea26a3d8af02fd9
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-02 00:29:53 -0400

    first attempt for legion

--- cmake/legion.cmake
+++ cmake/legion.cmake
@@ -16,30 +16,55 @@ if(FF_USE_EXTERNAL_LEGION)
 	message(STATUS "Use external Legion: ${LEGION_ROOT}")
 	set(LEGION_LIBRARY Legion::Legion)
 else()
-	if(FF_USE_PYTHON)
-	  set(Legion_USE_Python ON CACHE BOOL "enable Legion_USE_Python")
-	endif()
-	if(FF_USE_GASNET)
-	  set(Legion_EMBED_GASNet ON CACHE BOOL "Use embed GASNet")
-	  set(Legion_EMBED_GASNet_VERSION "GASNet-2022.3.0" CACHE STRING "GASNet version")
-	  set(Legion_NETWORKS "gasnetex" CACHE STRING "GASNet conduit")
-	  set(GASNet_CONDUIT ${FF_GASNET_CONDUIT})
-	endif()
-	message(STATUS "GASNET ROOT: $ENV{GASNet_ROOT_DIR}")
-	set(Legion_MAX_DIM ${FF_MAX_DIM} CACHE STRING "Maximum number of dimensions")
-	set(Legion_USE_CUDA ON CACHE BOOL "enable Legion_USE_CUDA")
-	set(Legion_CUDA_ARCH ${FF_CUDA_ARCH} CACHE STRING "Legion CUDA ARCH")
-	add_subdirectory(deps/legion)
-	set(LEGION_LIBRARY Legion)
-
-	# include(FetchContent)
-	# FetchContent_Declare(legion URL https://github.com/gabrieleoliaro/flexflow-third-party/releases/latest/download/legion_11.1.1_ubuntu-20.04.tar.gz)
-	# FetchContent_MakeAvailable(legion)
+	# if(FF_USE_PYTHON)
+	#   set(Legion_USE_Python ON CACHE BOOL "enable Legion_USE_Python")
+	# endif()
+	# if(FF_USE_GASNET)
+	#   set(Legion_EMBED_GASNet ON CACHE BOOL "Use embed GASNet")
+	#   set(Legion_EMBED_GASNet_VERSION "GASNet-2022.3.0" CACHE STRING "GASNet version")
+	#   set(Legion_NETWORKS "gasnetex" CACHE STRING "GASNet conduit")
+	#   set(GASNet_CONDUIT ${FF_GASNET_CONDUIT})
+	# endif()
+	# message(STATUS "GASNET ROOT: $ENV{GASNet_ROOT_DIR}")
+	# set(Legion_MAX_DIM ${FF_MAX_DIM} CACHE STRING "Maximum number of dimensions")
+	# set(Legion_USE_CUDA ON CACHE BOOL "enable Legion_USE_CUDA")
+	# set(Legion_CUDA_ARCH ${FF_CUDA_ARCH} CACHE STRING "Legion CUDA ARCH")
+	# add_subdirectory(deps/legion)
 	# set(LEGION_LIBRARY Legion)
-	# set(LEGION_URL https://github.com/gabrieleoliaro/flexflow-third-party/releases/latest/download/legion_11.1.1_ubuntu-20.04.tar.gz)
- #    execute_process(COMMAND ${BASH} wget ${LEGION_URL} -o ${CMAKE_BINARY_DIR}/deps && 
- #                    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
- #                    RESULT_VARIABLE download_result)
+
+	set(LEGION_LIBRARY legion)
+	set(LEGION_URL https://github.com/gabrieleoliaro/flexflow-third-party/releases/latest/download/legion_11.1.1_ubuntu-20.04.tar.gz)
+
+	ExternalProject_Add(${LEGION_LIBRARY}
+	  SOURCE_DIR ""
+	  PREFIX ${CMAKE_BINARY_DIR}/deps/${LEGION_LIBRARY}
+	  URL ${LEGION_URL}
+	  DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/deps/
+	  #INSTALL_DIR ${CMAKE_BINARY_DIR}/deps/${LEGION_LIBRARY}
+	  #BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/deps/${LEGION_LIBRARY}/lib/liblegion${LIBEXT} ${CMAKE_BINARY_DIR}/deps/${LEGION_LIBRARY}/lib/librealm${LIBEXT}
+	  INSTALL_COMMAND ""
+	  CONFIGURE_COMMAND ""
+	  BUILD_COMMAND ""
+	  UPDATE_COMMAND ""
+	)
+
+	ExternalProject_Get_Property(${LEGION_LIBRARY} INSTALL_DIR)
+	message(STATUS "LEGION install dir: ${INSTALL_DIR}")
+
+	SET(LEGION_BASE_DIR ${INSTALL_DIR}/src/${LEGION_LIBRARY})
+	SET(LEGION_INCLUDE_DIR ${LEGION_BASE_DIR}/include)
+	SET(LEGION_BIN_DIR ${LEGION_BASE_DIR}/bin/)
+	SET(LEGION_LIB_DIR ${LEGION_BASE_DIR}/lib)
+	SET(LEGION_SHARE_DIR ${LEGION_BASE_DIR}/share/)
+
+	add_library(${LEGION_LIBRARY} SHARED IMPORTED)
+	set_target_properties(${LEGION_LIBRARY} PROPERTIES IMPORTED_LOCATION ${LEGION_LIB_DIR}/liblegion${LIBEXT} ${LEGION_LIB_DIR}/librealm${LIBEXT})
+	list(APPEND FLEXFLOW_INCLUDE_DIRS ${LEGION_INCLUDE_DIR})
+	#list(APPEND FLEXFLOW_EXT_LIBRARIES ${LEGION_LIB_DIR}/liblegion${LIBEXT} ${LEGION_LIB_DIR}/librealm${LIBEXT})
+
+	install(DIRECTORY ${LEGION_SHARE_DIR} DESTINATION share)
+	install(DIRECTORY ${LEGION_BIN_DIR} DESTINATION bin)
+
 
 endif()
 
