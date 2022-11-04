5bd49a61d3e03ceb7675741ef258a8dc111aeebc
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-04 10:03:11 -0400

    downgrade pillow

--- .github/workflows/helpers/install_dependencies.sh
+++ .github/workflows/helpers/install_dependencies.sh
@@ -26,7 +26,7 @@ wget -c -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # Install conda packages
 echo "Installing conda packages..."
-/opt/conda/bin/conda install cmake make pillow
+/opt/conda/bin/conda install cmake make pillow=9.0.0
 /opt/conda/bin/conda install -c conda-forge numpy keras-preprocessing pybind11 cmake-build-extension
 
 # Examples
