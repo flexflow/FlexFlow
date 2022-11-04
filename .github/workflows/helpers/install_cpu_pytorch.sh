cd3233d6615ab8aba8b8f4c2a10b2326a55eb191
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-11-04 15:08:38 -0400

    removed unnecessary packages

--- .github/workflows/helpers/install_cpu_pytorch.sh
+++ .github/workflows/helpers/install_cpu_pytorch.sh
@@ -13,8 +13,8 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update -y &&
 echo "Installing PyTorch and related packages"
 export PATH=/opt/conda/bin:$PATH
 # Install CPU-only Pytorch
-conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cpuonly -c pytorch
+conda install pytorch torchvision torchaudio cpuonly -c pytorch
 # Install Hugging-face/MT5 related packages
-conda install -c conda-forge pandas numpy transformers=4.16.2 sentencepiece pillow
+#conda install -c conda-forge pandas numpy transformers=4.16.2 sentencepiece pillow
 # Install packages required by other example applications
 pip install onnx tensorflow keras2onnx
