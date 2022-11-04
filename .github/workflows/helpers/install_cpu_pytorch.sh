6c3642c25736f5bd38b856d6eb47e883f114fbd0
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-04 10:32:47 -0400

    test

--- .github/workflows/helpers/install_cpu_pytorch.sh
+++ .github/workflows/helpers/install_cpu_pytorch.sh
@@ -31,9 +31,10 @@ DEBIAN_FRONTEND=noninteractive \
 
 # Install CPU-only Pytorch
 echo "Installing PyTorch and related packages"
-/opt/conda/bin/conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cpuonly -c pytorch
+#/opt/conda/bin/conda install pytorch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0 cpuonly -c pytorch
 # Install Hugging-face/MT5 related packages
 /opt/conda/bin/conda install -c conda-forge pip pandas numpy transformers=4.16.2 sentencepiece
+pip install torch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0
 # Install packages required by other example applications
 export PATH=/opt/conda/bin:$PATH
 pip install onnx tensorflow keras2onnx
