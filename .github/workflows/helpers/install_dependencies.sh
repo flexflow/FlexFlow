191cd4de19fe7d6b76258f6093d36e4b32dab05f
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-04 10:21:48 -0400

    maybe fix

--- .github/workflows/helpers/install_dependencies.sh
+++ .github/workflows/helpers/install_dependencies.sh
@@ -26,8 +26,7 @@ wget -c -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # Install conda packages
 echo "Installing conda packages..."
-/opt/conda/bin/conda install cmake make
-/opt/conda/bin/conda install -c conda-forge numpy keras-preprocessing pybind11 cmake-build-extension pillow=9.0.0
+/opt/conda/bin/conda install -c conda-forge cmake make numpy keras-preprocessing pybind11 cmake-build-extension pillow=9.0.0
 
 # Examples
 sudo apt-get update -y && sudo apt-get install -y --no-install-recommends libhdf5-dev
