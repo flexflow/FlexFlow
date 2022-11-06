84ccbd2f216cce3c0a50bf43651cdf7dfa80215d
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-05 22:43:55 -0400

    fix

---
+++ .github/workflows/helpers/install_gpu_test_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -euo pipefail
+set -x
+
+# General dependencies
+echo "Installing apt dependencies..."
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+    wget binutils git nano zlib1g-dev libhdf5-dev software-properties-common \
+    build-essential apt-utils ca-certificates libssl-dev curl unzip htop && \
+    sudo rm -rf /var/lib/apt/lists/*
+
+# Install CUDNN
+echo "Installing CUDNN..."
+wget -c -q http://developer.download.nvidia.com/compute/redist/cudnn/v8.0.5/cudnn-11.1-linux-x64-v8.0.5.39.tgz && \
+    sudo tar -xzf cudnn-11.1-linux-x64-v8.0.5.39.tgz -C /usr/local && \
+    rm cudnn-11.1-linux-x64-v8.0.5.39.tgz && \
+    sudo ldconfig
+
+# Install Miniconda
+echo "Installing Miniconda..."
+wget -c -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+    chmod +x ./Miniconda3-latest-Linux-x86_64.sh && \
+    ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda && \
+    rm ./Miniconda3-latest-Linux-x86_64.sh && \
+    /opt/conda/bin/conda upgrade --all && \
+    /opt/conda/bin/conda install conda-build conda-verify && \
+    /opt/conda/bin/conda clean -ya
+
+# Install conda packages
+echo "Installing conda packages..."
+export PATH=/opt/conda/bin:$PATH
+conda install -c pytorch -c conda-forge \
+    cmake make cmake-build-extension pybind11 numpy pandas keras-preprocessing pillow transformers sentencepiece onnx tensorflow keras2onnx \
+    pytorch==1.9.0 torchvision==0.10.1 torchaudio==0.9.1 cpuonly
