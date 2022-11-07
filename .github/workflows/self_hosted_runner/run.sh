f4cb824155429eb35828db01ddbcec6f0ea5ccd1
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-06 22:06:46 -0500

    passing gpu index

--- .github/workflows/self_hosted_runner/run.sh
+++ .github/workflows/self_hosted_runner/run.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 ORGANIZATION="flexflow/FlexFlow"
 REGISTRATION_TOKEN=""
+GPUS=""
 
 # Cd into directory holding this script
 cd "${BASH_SOURCE[0]%/*}"
@@ -12,16 +13,21 @@ if [[ -z "${ORGANIZATION+1}" ]]; then
     echo "Please set the ORGANIZATION before starting the runner"
     exit
 fi
-
 if [[ -z "${REGISTRATION_TOKEN+1}" ]]; then 
     echo "Please set the REGISTRATION_TOKEN before starting the runner"
+    exit
+fi
+
+# Ensure that one or more GPU indexes are passed
+if [[ -z "${GPUS+1}" ]]; then 
+    echo "Please set the GPU index(es) before starting the runner"
     exit
 fi
 
 docker run --detach \
   --env ORGANIZATION=$ORGANIZATION \
   --env REGISTRATION_TOKEN=$REGISTRATION_TOKEN \
-  --gpus 4 \
+  --gpus $GPUS \
   -v /var/run/docker.sock:/var/run/docker.sock \
   --name flexflow-gpu-runner \
   ff-runner-image
