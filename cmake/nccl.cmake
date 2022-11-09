ce9e7558a48c5b906845e89b6eea8aab90d1d8f3
Gabriele Oliaro <ict@gabrieleoliaro.it>
2022-11-09 15:06:59 -0500

    add check for x86_64 processor

--- cmake/nccl.cmake
+++ cmake/nccl.cmake
@@ -3,7 +3,7 @@ set(NCCL_NAME nccl)
 # message("NCCL_CUDA_ARCH: ${NCCL_CUDA_ARCH}")
 
 set(NCCL_URL "")
-if(FF_USE_PREBUILT_NCCL OR FF_USE_ALL_PREBUILT_LIBRARIES)
+if((FF_USE_PREBUILT_NCCL OR FF_USE_ALL_PREBUILT_LIBRARIES) AND CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "x86_64")
   if(LINUX_VERSION MATCHES "20.04")
     if (CUDA_VERSION VERSION_EQUAL "11.0")
       set(NCCL_URL "https://github.com/flexflow/flexflow-third-party/releases/latest/download/nccl_ubuntu-20.04_11.0.3.tar.gz")
