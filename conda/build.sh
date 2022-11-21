ecfcd3899fb4f9af114568ef780b81b6ac104f9b
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-11-21 04:34:45 +0000

    fix issue sc2155

--- conda/build.sh
+++ conda/build.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
 
 # build flexflow
-export CXXFLAGS=$(echo $CXXFLAGS | sed 's/-O2//g')
-export CXXFLAGS=$(echo $CXXFLAGS | sed 's/-std=c++17//g')
-export CXXFLAGS=$(echo $CXXFLAGS | sed 's/-DNDEBUG//g')
-export CXXFLAGS=$(echo $CXXFLAGS | sed 's/-D_FORTIFY_SOURCE=2//g')
-export CPPFLAGS=$(echo $CPPFLAGS | sed 's/-O2//g')
-export CPPFLAGS=$(echo $CPPFLAGS | sed 's/std=c++17//g')
-export CPPFLAGS=$(echo $CXXFLAGS | sed 's/-DNDEBUG//g')
-export CPPFLAGS=$(echo $CXXFLAGS | sed 's/-D_FORTIFY_SOURCE=2//g')
+CXXFLAGS=$(echo $CXXFLAGS | sed 's/-O2//g')
+CXXFLAGS=$(echo $CXXFLAGS | sed 's/-std=c++17//g')
+CXXFLAGS=$(echo $CXXFLAGS | sed 's/-DNDEBUG//g')
+CXXFLAGS=$(echo $CXXFLAGS | sed 's/-D_FORTIFY_SOURCE=2//g')
+export CXXFLAGS
+CPPFLAGS=$(echo $CPPFLAGS | sed 's/-O2//g')
+CPPFLAGS=$(echo $CPPFLAGS | sed 's/std=c++17//g')
+CPPFLAGS=$(echo $CPPFLAGS | sed 's/-DNDEBUG//g')
+CPPFLAGS=$(echo $CPPFLAGS | sed 's/-D_FORTIFY_SOURCE=2//g')
+export CPPFLAGS
 
 #export CUDNN_HOME=/projects/opt/centos7/cuda/10.1
 #export CUDA_HOME=/projects/opt/centos7/cuda/10.1
