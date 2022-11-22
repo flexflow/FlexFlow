1823110f1abe81a1a584328197898506c9d4d9d6
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-11-22 07:23:16 +0000

    fix

--- docker/build.sh
+++ docker/build.sh
@@ -58,8 +58,11 @@ fi
 cores_available=$(nproc --all)
 n_build_cores=$(( cores_available -1 ))
 
-# Get CUDA architecture(s), if GPUs are available
-cat << EOF > ./get_gpu_arch.cu
+if [[ "${FF_AUTODETECT_CUDA_ARCH:-ON}" == "OFF" ]]; then
+  echo "FF_AUTODETECT_CUDA_ARCH is turned off. Building for all compatible architectures."
+else
+  # Get CUDA architecture(s), if GPUs are available
+  cat << EOF > ./get_gpu_arch.cu
 #include <stdio.h>
 int main() {
   int count = 0;
@@ -73,30 +76,27 @@ int main() {
   return 0;
 }
 EOF
-gpu_arch_codes=""
-if command -v nvcc &> /dev/null
-then
-  nvcc ./get_gpu_arch.cu -o ./get_gpu_arch
-  gpu_arch_codes="$(./get_gpu_arch)"
-fi
-gpu_arch_codes="$(echo "$gpu_arch_codes" | xargs -n1 | sort -u | xargs)"
-gpu_arch_codes="${gpu_arch_codes// /,}"
-rm -f ./get_gpu_arch.cu ./get_gpu_arch
+  gpu_arch_codes=""
+  if command -v nvcc &> /dev/null
+  then
+    nvcc ./get_gpu_arch.cu -o ./get_gpu_arch
+    gpu_arch_codes="$(./get_gpu_arch)"
+  fi
+  gpu_arch_codes="$(echo "$gpu_arch_codes" | xargs -n1 | sort -u | xargs)"
+  gpu_arch_codes="${gpu_arch_codes// /,}"
+  rm -f ./get_gpu_arch.cu ./get_gpu_arch
 
-if [[ -n "$gpu_arch_codes" ]]; then
+  if [[ -n "$gpu_arch_codes" ]]; then
   echo "Host machine has GPUs with architecture codes: $gpu_arch_codes"
   echo "Configuring FlexFlow to build for the $gpu_arch_codes code(s)."
   FF_CUDA_ARCH="${gpu_arch_codes}"
-else
-  echo "Could not detect any GPU on the host machine."
-  if [[ "${FF_AUTODETECT_CUDA_ARCH:-ON}" == "OFF" ]]; then
-    echo "FF_AUTODETECT_CUDA_ARCH is turned off. Building for all compatible architectures."
   else
+    echo "Could not detect any GPU on the host machine."
     echo "Letting FlexFlow build for a default GPU architecture: code=70"
     FF_CUDA_ARCH=70
   fi
+  export FF_CUDA_ARCH
 fi
-export FF_CUDA_ARCH
 
 # Build FlexFlow Docker image
 # shellcheck source=/dev/null
