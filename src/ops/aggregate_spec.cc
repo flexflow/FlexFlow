02012650ce4c4100a5064f9a357be856a460efd3
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-11-30 06:40:52 +0000

    [MOE] - Replaced deadcode in AggregateSpec operator

--- src/ops/aggregate_spec.cc
+++ src/ops/aggregate_spec.cc
@@ -39,7 +39,50 @@ Tensor FFModel::aggregate_spec(
     int n,
     float lambda_bal,
     char const *name) {
-  assert(false);
+  //assert(false);
+  Layer *li = new Layer(this,
+                        OP_AGG_SPEC,
+                        name,
+                        n+4 /*inputs*/,
+                        0 /*weights*/,
+                        1 /*outputs*/,
+                        inputs);
+  {
+    // expert inputs
+    int num_dim = inputs[4]->num_dims;
+    int out_dim = inputs[4]->dims[0];
+    for (int i = 1; i < n; i++) {
+      assert(inputs[i + 4]->num_dims == num_dim);
+      assert(inputs[i + 4]->dims[0] == out_dim);
+    }
+    // Set output shape
+    int dims[MAX_TENSOR_DIM];
+    for (int i = 0; i < num_dim - 1; i++)
+      dims[i] = inputs[4]->dims[i];
+    dims[num_dim - 1] = inputs[0]->dims[num_dim - 1];
+    li->outputs[0] = create_tensor_legion_ordering(num_dim, dims, DT_FLOAT, li, 0, true /*create_grad*/);
+  }
+  li->add_int_property("n", n);
+  li->add_float_property("lambda_bal", lambda_bal);
+  layers.push_back(li);
+  return li->outputs[0];
+}
+
+Op *AggregateSpec::create_operator_from_layer(
+    FFModel &model,
+    Layer const *layer,
+    std::vector<ParallelTensor> const &inputs) {
+  long long value1;
+  layer->get_int_property("n", value1);
+  int n = value1;
+  float value2;
+  layer->get_float_property("lambda_bal", value2);
+  float lambda_bal = value2;
+  return new AggregateSpec(model,
+                  inputs.data(),
+                  n,
+                  lambda_bal,
+                  layer->name);
 }
 
 AggregateSpec::AggregateSpec(FFModel &model,
