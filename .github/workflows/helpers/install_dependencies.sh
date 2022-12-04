e11f906fff3bf72b1dd0e3610200efe693b37ea0
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-04 00:12:52 +0000

    test

--- .github/workflows/helpers/install_dependencies.sh
+++ .github/workflows/helpers/install_dependencies.sh
@@ -30,11 +30,7 @@ if [[ "${FF_GPU_BACKEND}" != @(cuda|hip_cuda|hip_rocm|intel) ]]; then
   exit 1
 elif [[ "$FF_GPU_BACKEND" == "hip_cuda" || "$FF_GPU_BACKEND" = "hip_rocm" ]]; then
     echo "FF_GPU_BACKEND: ${FF_GPU_BACKEND}. Installing HIP dependencies"
-    if [[ "$(lsb_release -r --short)" != "18.04" ]]; then 
-        echo "Error, miopen-hip package is only compatible with amdgpu-install for Ubuntu 18.04"
-        exit 1
-    fi
-    wget https://repo.radeon.com/amdgpu-install/22.20.5/ubuntu/bionic/amdgpu-install_22.20.50205-1_all.deb
+    wget https://repo.radeon.com/amdgpu-install/22.20.5/ubuntu/focal/amdgpu-install_22.20.50205-1_all.deb
     sudo apt-get install -y ./amdgpu-install_22.20.50205-1_all.deb
     rm ./amdgpu-install_22.20.50205-1_all.deb
     sudo amdgpu-install -y --usecase=hip,rocm --no-dkms
