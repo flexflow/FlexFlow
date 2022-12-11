8d7f74066aba95d1dbb429c763d02a9ae506bcb6
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-11 00:01:14 -0500

    updated pull & push scripts

--- docker/pull.sh
+++ docker/pull.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 set -euo pipefail
 
-image=${1:-flexflow-environment}
-
 # Cd into directory holding this script
 cd "${BASH_SOURCE[0]%/*}"
+
+image=${1:-"flexflow-cuda"}
+if [[ "${image}" != @(flexflow-environment-cuda|flexflow-environment-hip_cuda|flexflow-environment-hip_rocm|flexflow-environment-intel|flexflow-cuda|flexflow-hip_cuda|flexflow-hip_rocm|flexflow-intel) ]]; then
+  echo "Error, image name ${image} is invalid. Choose between 'flexflow-environment-{cuda,hip_cuda,hip_rocm,intel}' and 'flexflow-{cuda,hip_cuda,hip_rocm,intel}'."
+  exit 1
+fi
 
 # Download image
 docker pull ghcr.io/flexflow/"$image"
