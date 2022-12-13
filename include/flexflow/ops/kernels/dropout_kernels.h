67cbb40fdffdcfc33baecf8882fd5ce5974a947d
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-12 22:49:14 -0500

    removed conv2d accidental copy

--- include/flexflow/ops/kernels/dropout_kernels.h
+++ include/flexflow/ops/kernels/dropout_kernels.h
@@ -29,144 +29,27 @@ public:
 namespace Kernels {
 namespace Dropout {
 
-void init_kernel(Conv2DMeta *m,
-                 int input_w,
-                 int input_h,
-                 int input_c,
-                 int input_n,
-                 int output_w,
-                 int output_h,
-                 int output_c,
-                 int output_n,
-                 int kernel_h,
-                 int kernel_w,
-                 int groups,
-                 int stride_h,
-                 int stride_w,
-                 int pad_h,
-                 int pad_w,
-                 float const *input_ptr,
-                 float *output_ptr,
-                 float const *kernel_ptr,
-                 float *kernel_grad_ptr,
-                 float *forward_time = nullptr,
-                 float *backward_time = nullptr);
-
-void forward_kernel_wrapper(Conv2DMeta const *m,
-                            float const *input_ptr,
-                            float *output_ptr,
-                            float const *filter_ptr,
-                            float const *bias_ptr);
-void backward_kernel_wrapper(Conv2DMeta const *m,
+void forward_kernel_wrapper(DropoutMeta *m,
                              float const *input_ptr,
-                             float *input_grad_ptr,
-                             float const *output_ptr,
-                             float *output_grad_ptr,
-                             float const *kernel_ptr,
-                             float *kernel_grad_ptr,
-                             float *bias_grad_ptr);
+                             float *output_ptr);
+void backward_kernel_wrapper(DropoutMeta *m,
+                              float const *output_grad_ptr,
+                              float *input_grad_ptr);
 
 namespace Internal {
 
-void forward_kernel(Conv2DMeta const *m,
-                    float const *input_ptr,
-                    float *output_ptr,
-                    float const *filter_ptr,
-                    float const *bias_ptr,
-                    ffStream_t stream);
-
-void backward_kernel(Conv2DMeta const *m,
+void forward_kernel(DropoutMeta *m,
                      float const *input_ptr,
-                     float *input_grad_ptr,
-                     float const *output_ptr,
-                     float *output_grad_ptr,
-                     float const *kernel_ptr,
-                     float *kernel_grad_ptr,
-                     float *bias_grad_ptr,
+                     float *output_ptr,
                      ffStream_t stream);
 
-#if defined(FF_USE_CUDA) || defined(FF_USE_HIP_CUDA)
-cudnnConvolutionFwdAlgo_t selectConvolutionForwardAlgorithm(
-    cudnnHandle_t handle,
-    const cudnnTensorDescriptor_t xDesc,
-    void const *x,
-    const cudnnFilterDescriptor_t wDesc,
-    void const *w,
-    const cudnnConvolutionDescriptor_t convDesc,
-    void *workSpace,
-    size_t workSpaceSize,
-    const cudnnTensorDescriptor_t yDesc,
-    void *y,
-    float *time);
-
-cudnnConvolutionBwdDataAlgo_t selectConvolutionBackwardDataAlgorithm(
-    cudnnHandle_t handle,
-    const cudnnFilterDescriptor_t wDesc,
-    void const *w,
-    const cudnnTensorDescriptor_t dyDesc,
-    void const *dy,
-    const cudnnConvolutionDescriptor_t convDesc,
-    void *workSpace,
-    size_t workSpaceSize,
-    const cudnnTensorDescriptor_t dxDesc,
-    void *dx,
-    float *time);
-
-cudnnConvolutionBwdFilterAlgo_t selectConvolutionBackwardFilterAlgorithm(
-    cudnnHandle_t handle,
-    const cudnnTensorDescriptor_t xDesc,
-    void const *x,
-    const cudnnTensorDescriptor_t dyDesc,
-    void const *dy,
-    const cudnnConvolutionDescriptor_t convDesc,
-    void *workSpace,
-    size_t workSpaceSize,
-    const cudnnFilterDescriptor_t dwDesc,
-    void *dw,
-    float *time);
-#else
-miopenConvFwdAlgorithm_t selectConvolutionForwardAlgorithm(
-    miopenHandle_t handle,
-    const miopenTensorDescriptor_t xDesc,
-    void const *x,
-    const miopenTensorDescriptor_t wDesc,
-    void const *w,
-    const miopenConvolutionDescriptor_t convDesc,
-    void *workSpace,
-    size_t workSpaceSize,
-    const miopenTensorDescriptor_t yDesc,
-    void *y,
-    float *time);
-
-miopenConvBwdWeightsAlgorithm_t selectConvolutionBackwardFilterAlgorithm(
-    miopenHandle_t handle,
-    const miopenTensorDescriptor_t xDesc,
-    void const *x,
-    const miopenTensorDescriptor_t dyDesc,
-    void const *dy,
-    const miopenConvolutionDescriptor_t convDesc,
-    void *workSpace,
-    size_t workSpaceSize,
-    const miopenTensorDescriptor_t dwDesc,
-    void *dw,
-    float *time);
-
-miopenConvBwdDataAlgorithm_t selectConvolutionBackwardDataAlgorithm(
-    miopenHandle_t handle,
-    const miopenTensorDescriptor_t wDesc,
-    void const *w,
-    const miopenTensorDescriptor_t dyDesc,
-    void const *dy,
-    const miopenConvolutionDescriptor_t convDesc,
-    void *workSpace,
-    size_t workSpaceSize,
-    const miopenTensorDescriptor_t dxDesc,
-    void *dx,
-    float *time);
-#endif
+void backward_kernel(DropoutMeta *m,
+                      float const *output_grad_ptr,
+                      float *input_grad_ptr,
+                      ffStream_t stream);
 
 } // namespace Internal
-} // namespace Dropout
+} // namespace Conv2D
 } // namespace Kernels
 } // namespace FlexFlow
 
