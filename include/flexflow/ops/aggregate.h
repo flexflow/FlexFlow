1352a8763a6368b6b1e208fbd4627ec2fa837188
Rae Wong <33883582+yingyee0111@users.noreply.github.com>
2022-12-13 21:06:06 -0500

    feat: implement inference batch tensors (#521)

--- include/flexflow/ops/aggregate.h
+++ include/flexflow/ops/aggregate.h
@@ -36,6 +36,9 @@ public:
             char const *name = nullptr);
   void init(FFModel const &) override;
   void forward(FFModel const &) override;
+  void inference(FFModel const &,
+                 std::vector<ParallelTensor> const &,
+                 std::vector<ParallelTensor> const &) override;
   void backward(FFModel const &) override;
   void print_layer(FFModel const &model) override {
     assert(0);
