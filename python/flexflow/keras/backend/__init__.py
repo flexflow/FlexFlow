b6aa0e38dd392cbae6b8ad4270df4c4f65302c16
Soumya Chatterjee <soumyac@stanford.edu>
2022-12-21 22:37:16 -0800

    [Keras] remove unused import

--- python/flexflow/keras/backend/__init__.py
+++ python/flexflow/keras/backend/__init__.py
@@ -17,7 +17,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 import sys
 from .backend_functions import batch_dot
-import .internal
 
 # Default backend: FlexFlow.
 _BACKEND = 'flexflow'
