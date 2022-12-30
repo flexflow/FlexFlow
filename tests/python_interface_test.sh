ee065f1a615be49d23d6fbe65916410a4dcbbc68
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-30 11:47:54 +0100

    fix

--- tests/python_interface_test.sh
+++ tests/python_interface_test.sh
@@ -10,11 +10,11 @@ ZSIZE=12192
 if [ -z "$FF_HOME" ]; then echo "FF_HOME variable is not defined, aborting tests"; exit; fi
 interpreter=${1:-python}
 if [[ "$interpreter" == "python" ]]; then
-	EXE="python"
 	export FF_USE_NATIVE_PYTHON=1
-	export PYTHONPATH="${FF_HOME}/build/python:${PYTHONPATH}"
+	EXE="python"
 	echo "Running a single-GPU Python test to check the Python interface (native python interpreter)"
 	$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
+	unset FF_USE_NATIVE_PYTHON
 elif [[ "$interpreter" == "flexflow_python" ]]; then
 	EXE="$FF_HOME"/python/flexflow_python
 	echo "Running a single-GPU Python test to check the Python interface (flexflow_python interpreter)"
