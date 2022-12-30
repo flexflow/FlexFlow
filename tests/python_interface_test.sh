3649b7f8f0eba6a698a2b5cd56ddd1ed4a2904c1
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-30 11:15:18 +0100

    fix

--- tests/python_interface_test.sh
+++ tests/python_interface_test.sh
@@ -13,7 +13,7 @@ if [[ "$EXE" == "python" ]]; then
 	export FF_USE_NATIVE_PYTHON=1
 	echo "Running a single-GPU Python test to check the Python interface (native python interpreter)"
 	$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
-elif [[ "$EXE" == "flexflow_python" ]]
+elif [[ "$EXE" == "flexflow_python" ]]; then
 	echo "Running a single-GPU Python test to check the Python interface (flexflow_python interpreter)"
 	$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:py 1 -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
 else
