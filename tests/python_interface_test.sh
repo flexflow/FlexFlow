92dc2b3382eff9662608c5a58ef8b1aad8373702
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-30 11:19:42 +0100

    fix

--- tests/python_interface_test.sh
+++ tests/python_interface_test.sh
@@ -8,12 +8,14 @@ FSIZE=14048
 ZSIZE=12192
 
 if [ -z "$FF_HOME" ]; then echo "FF_HOME variable is not defined, aborting tests"; exit; fi
-EXE=${1:-python}
-if [[ "$EXE" == "python" ]]; then
-	export FF_USE_NATIVE_PYTHON=1
+interpreter=${1:-python}
+if [[ "$interpreter" == "python" ]]; then
+	EXE="python"
+	FF_USE_NATIVE_PYTHON=1
 	echo "Running a single-GPU Python test to check the Python interface (native python interpreter)"
 	$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
-elif [[ "$EXE" == "flexflow_python" ]]; then
+elif [[ "$interpreter" == "flexflow_python" ]]; then
+	EXE="$FF_HOME"/python/flexflow_python
 	echo "Running a single-GPU Python test to check the Python interface (flexflow_python interpreter)"
 	$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:py 1 -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
 else
