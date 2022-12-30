0b999cc79e0b73bfff007ceb0f6a7a30dc46c8ee
Gabriele Oliaro <goliaro@cs.cmu.edu>
2022-12-30 11:32:52 +0100

    revert unnecessary changes

--- tests/python_interface_test.sh
+++ tests/python_interface_test.sh
@@ -12,6 +12,7 @@ interpreter=${1:-python}
 if [[ "$interpreter" == "python" ]]; then
 	EXE="python"
 	export FF_USE_NATIVE_PYTHON=1
+	export PYTHONPATH="${FF_HOME}/build/python:${PYTHONPATH}"
 	echo "Running a single-GPU Python test to check the Python interface (native python interpreter)"
 	$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
 elif [[ "$interpreter" == "flexflow_python" ]]; then
