dc9a5bb01f9fb6d43a9d1c812ee06334748c7424
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-01-08 05:37:19 +0000

    added comments

--- tests/multinode_helpers/mpi_wrapper1.sh
+++ tests/multinode_helpers/mpi_wrapper1.sh
@@ -6,4 +6,7 @@ if [ -z "$FF_HOME" ]; then echo "FF_HOME variable is not defined, aborting tests
 if [ -z "$NUM_NODES" ]; then echo "NUM_NODES variable is not defined, aborting tests"; exit; fi
 if [ -z "$GPUS" ]; then echo "GPUS variable is not defined, aborting tests"; exit; fi
 
+# We need to wrap the instruction below in its own script because MPI throws an error if we try
+# to run "mpirun" more than once in the same script. Hence, we cannot simply call "mpirun" in the
+# multi_gpu_tests.sh script
 mpirun -np "$NUM_NODES" "$FF_HOME"/tests/multinode_helpers/mpi_wrapper2.sh "$@"
