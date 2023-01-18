ef5fea026cd2e0f4a378104b7cf94e10f44a0762
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-01-17 23:51:45 -0500

    [Inference][MLP] removed non-working dataloader

--- examples/cpp/inference/MLP_Unify/mlp.cc
+++ examples/cpp/inference/MLP_Unify/mlp.cc
@@ -1,4 +1,4 @@
-/* Copyright 2021 Stanford University
+/* Copyright 2023 CMU, Facebook, LANL, MIT, NVIDIA, and Stanford (alphabetical)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,120 +22,6 @@
 
 using namespace Legion;
 using namespace FlexFlow;
-
-DataLoader::DataLoader(FFModel &ff,
-                       MLPConfig const &mlpConfig,
-                       InferenceManager const *im, 
-                       Tensor input) {
-  Context ctx = ff.config.lg_ctx;
-  Runtime *runtime = ff.config.lg_hlr;
-  printf("Use random dataset...");
-
-  // The number of samples is the total number of request samples that can ever
-  // be loaded into memory at the same time. In the case of training, the value
-  // is batchSize * workersPerNode * numNodes, since each worker can only
-  // process one batch at a time. In inference,  batchSize
-  int max_parallel_requests =
-      im->max_num_inflight_batches *
-      (ff.config.batchSize * im->max_num_requests_per_batch);
-  num_samples =
-      max_parallel_requests * ff.config.workersPerNode * ff.config.numNodes;
-  printf("Number of random samples = %d\n", num_samples);
-
-  // return;
-  
-  // Create full input
-  {
-    batch_input = input;
-    int const dims[] = {num_samples,
-                        mlpConfig.sequence_length * mlpConfig.embedding_size};
-    full_input = ff.create_tensor<2>(dims, DT_FLOAT);
-  }
-
-  // Load entire dataset
-  // TODO: Use index launcher instead of task launcher
-  TaskLauncher launcher(CUSTOM_CPU_TASK_ID_1, TaskArgument(NULL, 0));
-  launcher.add_region_requirement(
-      RegionRequirement(full_input->parallel_tensor->region,
-                        WRITE_ONLY,
-                        EXCLUSIVE,
-                        full_input->parallel_tensor->region,
-                        MAP_TO_ZC_MEMORY));
-  launcher.add_field(0, FID_DATA);
-  runtime->execute_task(ctx, launcher);
-  reset();
-  //next_batch(ff);
-}
-
-void DataLoader::load_entire_dataset(Task const *task,
-                                     std::vector<PhysicalRegion> const &regions,
-                                     Context ctx,
-                                     Runtime *runtime) {
-  assert(regions.size() == 1); // no labels
-  assert(task->regions.size() == 1);
-  // Note that these instances are in ZCM, can only use
-  // TensorAccessorW with readOutput flag
-  AccessorWO<float, 2> const acc_input(regions[0], FID_DATA);
-  Rect<2> rect_input = runtime->get_index_space_domain(
-      ctx, task->regions[0].region.get_index_space());
-  assert(acc_input.accessor.is_dense_arbitrary(rect_input));
-  float *input_ptr = acc_input.ptr(rect_input.lo);
-  // Fill dataset with random data
-  for (int i = 0; i < rect_input.volume(); i++) {
-    input_ptr[i] = ((float)std::rand()) / RAND_MAX;
-  }
-  printf("finish loading data\n");
-}
-
-void DataLoader::next_batch(FFModel &ff) {
-  Context ctx = ff.config.lg_ctx;
-  Runtime *runtime = ff.config.lg_hlr;
-  // Load Input
-  {
-    Rect<2> rect = runtime->get_index_space_domain(ctx, batch_input->parallel_tensor->parallel_is);
-    ArgumentMap argmap;
-    int idx = next_index;
-    for (PointInRectIterator<2> it(rect); it(); it++) {
-      SampleIdxs meta;
-      assert(ff.config.batchSize % (rect.hi[1] - rect.lo[1] + 1) == 0);
-      meta.num_samples = ff.config.batchSize / (rect.hi[1] - rect.lo[1] + 1);
-      for (int i = 0; i < meta.num_samples; i++)
-        meta.idxs[i] = idx++;
-      argmap.set_point(*it, TaskArgument(&meta, sizeof(SampleIdxs)));
-    }
-
-    IndexLauncher launcher(CUSTOM_GPU_TASK_ID_1,
-                           batch_input->parallel_tensor->parallel_is,
-                           TaskArgument(NULL, 0),
-                           argmap,
-                           Predicate::TRUE_PRED,
-                           false /*must*/,
-                           0 /*mapper_id*/,
-                           batch_input->parallel_tensor->machine_view.hash());
-    launcher.add_region_requirement(
-        RegionRequirement(full_input->parallel_tensor->region,
-                          0 /*projection id*/,
-                          READ_ONLY,
-                          EXCLUSIVE,
-                          full_input->parallel_tensor->region,
-                          MAP_TO_FB_MEMORY));
-    launcher.add_field(0, FID_DATA);
-    launcher.add_region_requirement(
-        RegionRequirement(batch_input->parallel_tensor->part,
-                          0 /*projection id*/,
-                          WRITE_ONLY,
-                          EXCLUSIVE,
-                          batch_input->parallel_tensor->region));
-    launcher.add_field(1, FID_DATA);
-    runtime->execute_index_space(ctx, launcher);
-  }
-  // progress to the next_index
-  next_index += ff.config.batchSize;
-}
-
-void DataLoader::reset() {
-  next_index = 0;
-}
 
 
 Tensor create_mlp(FFModel *model,
@@ -255,22 +141,4 @@ void FlexFlow::top_level_task(Task const *task,
          ffConfig.batchSize * 128 * ffConfig.epochs / run_time);
 }
 
-void FlexFlow::register_custom_tasks() {
-  // Load entire dataset
-  {
-    TaskVariantRegistrar registrar(CUSTOM_CPU_TASK_ID_1, "Load Entire Dataset");
-    registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
-    registrar.set_leaf();
-    Runtime::preregister_task_variant<DataLoader::load_entire_dataset>(
-        registrar, "Load Entire Dataset Task");
-  }
-  // Load input
-  {
-    TaskVariantRegistrar registrar(FlexFlow::CUSTOM_GPU_TASK_ID_1,
-                                   "Load Inputs");
-    registrar.add_constraint(ProcessorConstraint(Processor::TOC_PROC));
-    registrar.set_leaf();
-    Runtime::preregister_task_variant<DataLoader::load_input>(
-        registrar, "Load Input Task");
-  }
-}
+void FlexFlow::register_custom_tasks() {}
