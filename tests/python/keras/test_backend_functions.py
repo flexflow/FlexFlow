a90c8d29a2eefc4decb440a155bb29dba7426fd3
Soumya Chatterjee <soumyac@stanford.edu>
2023-01-19 22:02:09 -0800

    changes

--- tests/python/keras/test_backend_functions.py
+++ tests/python/keras/test_backend_functions.py
@@ -1,24 +1,22 @@
-from flexflow.keras.layers import Input
-from flexflow.keras.backend import sin, cos
-import flexflow.keras.optimizers
-
 import numpy as np
+import flexflow
+from flexflow.keras.backend import cos, sin
+from flexflow.keras.layers import Input
 
 
 def test_sin():
-  inp = Input(shape=(16,), dtype="float32")
-  out = sin(inp)
+    inp = Input(shape=(16,), dtype="float32")
+    out = sin(inp)
 
-  model = flexflow.keras.models.Model(inp, out)
+    model = flexflow.keras.models.Model(inp, out)
 
-  opt = flexflow.keras.optimizers.SGD(learning_rate=0.01)
-  model.compile(
-    optimizer=opt, loss='mean_squared_error', metrics=['mean_squared_error'])
+    opt = flexflow.keras.optimizers.SGD(learning_rate=0.01)
+    model.compile(optimizer=opt, loss='mean_squared_error',
+                  metrics=['mean_squared_error'])
 
-  model.fit(
-    x = np.random.randn(300, 16).astype(np.float32),
-    y = np.random.randn(300, 16).astype(np.float32)
-  )
+    model.fit(
+        x = np.random.randn(300, 16).astype(np.float32),
+        y = np.random.randn(300, 16).astype(np.float32))
 
 
 if __name__ == '__main__':
