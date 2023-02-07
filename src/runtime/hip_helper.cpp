72087e93066c22d13b548aad4f4567c36c230ba1
Zhihao Jia <zhihao@cmu.edu>
2023-02-07 06:23:33 -0800

    Update hip_helper.cpp

--- src/runtime/hip_helper.cpp
+++ src/runtime/hip_helper.cpp
@@ -330,7 +330,7 @@ hipblasDatatype_t ff_to_cuda_datatype(DataType type) {
 
 void handle_unimplemented_hip_kernel(OperatorType op_type) {
   throw std::runtime_error("Unimplemented hip kernel for Operator: " +
-                           get_operator_type_name(op_type));
+                           FlexFlow::get_operator_type_name(op_type));
 }
 
 template __global__ void
