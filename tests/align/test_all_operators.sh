03ee494c7d43dd7ea84f088bf2c5c11e7d791282
xinhaoc <chengxh_98@163.com>
2023-02-10 21:50:55 +0000

    refractor test_all_operators

--- tests/align/test_all_operators.sh
+++ tests/align/test_all_operators.sh
@@ -3,65 +3,74 @@ eval "$(conda shell.bash hook)";
 
 rm -rf align/out
 
+function generate_ff_tensor(){
+    ./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu "$1" -ll:fsize "$2" -ll:zsize "$3" -b "$4" -o "$5";
+}
+
+function generate_torch_tensor(){
+    generate_torch_tensor "$1";
+}
+
 #create flexflow tensors
 conda activate flexflow;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o add;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o concat;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o conv2d;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o cos;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o embedding;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o exp;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o flat;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o getitem;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o identity;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o multiply;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o pool2d;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o reducesum;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o relu;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o reshape;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o scalar_add;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o scalar_multiply;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o scalar_sub;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o scalar_truediv;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o sigmoid;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o sin;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o subtract;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o tanh;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o transpose;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o view_embedding;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o max;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o min;
-./python/flexflow_python tests/align/align_create_tensor_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16 -o linear;
+generate_ff_tensor 1 5000 4096 16 add
+generate_ff_tensor 1 5000 4096 16 concat
+generate_ff_tensor 1 5000 4096 16 conv2d
+generate_ff_tensor 1 5000 4096 16 cos
+generate_ff_tensor 1 5000 4096 16 embedding
+generate_ff_tensor 1 5000 4096 16 exp
+generate_ff_tensor 1 5000 4096 16 flat
+generate_ff_tensor 1 5000 4096 16 getitem
+generate_ff_tensor 1 5000 4096 16 identity
+generate_ff_tensor 1 5000 4096 16 multiply
+generate_ff_tensor 1 5000 4096 16 pool2d
+generate_ff_tensor 1 5000 4096 16 reducesum
+generate_ff_tensor 1 5000 4096 16 relu
+generate_ff_tensor 1 5000 4096 16 reshape
+generate_ff_tensor 1 5000 4096 16 scalar_add
+generate_ff_tensor 1 5000 4096 16 scalar_multiply
+generate_ff_tensor 1 5000 4096 16 scalar_sub
+generate_ff_tensor 1 5000 4096 16 scalar_truediv
+generate_ff_tensor 1 5000 4096 16 sigmoid
+generate_ff_tensor 1 5000 4096 16 sin
+generate_ff_tensor 1 5000 4096 16 subtract
+generate_ff_tensor 1 5000 4096 16 tanh
+generate_ff_tensor 1 5000 4096 16 transpose
+generate_ff_tensor 1 5000 4096 16 view_embedding
+generate_ff_tensor 1 5000 4096 16 max
+generate_ff_tensor 1 5000 4096 16 min
+generate_ff_tensor 1 5000 4096 16 linear
+
 
 #create torch tensorss
 conda activate pytorch;
-python tests/align/align_create_tensor_torch.py -o add;
-python tests/align/align_create_tensor_torch.py -o concat;
-python tests/align/align_create_tensor_torch.py -o conv2d;
-python tests/align/align_create_tensor_torch.py -o cos;
-python tests/align/align_create_tensor_torch.py -o embedding;
-python tests/align/align_create_tensor_torch.py -o exp;
-python tests/align/align_create_tensor_torch.py -o flat;
-python tests/align/align_create_tensor_torch.py -o getitem;
-python tests/align/align_create_tensor_torch.py -o identity;
-python tests/align/align_create_tensor_torch.py -o multiply;
-python tests/align/align_create_tensor_torch.py -o pool2d;
-python tests/align/align_create_tensor_torch.py -o reducesum;
-python tests/align/align_create_tensor_torch.py -o relu;
-python tests/align/align_create_tensor_torch.py -o reshape;
-python tests/align/align_create_tensor_torch.py -o scalar_add;
-python tests/align/align_create_tensor_torch.py -o scalar_multiply;
-python tests/align/align_create_tensor_torch.py -o scalar_sub;
-python tests/align/align_create_tensor_torch.py -o scalar_truediv;
-python tests/align/align_create_tensor_torch.py -o sigmoid;
-python tests/align/align_create_tensor_torch.py -o sin;
-python tests/align/align_create_tensor_torch.py -o subtract;
-python tests/align/align_create_tensor_torch.py -o tanh;
-python tests/align/align_create_tensor_torch.py -o transpose;
-python tests/align/align_create_tensor_torch.py -o view_embedding;
-python tests/align/align_create_tensor_torch.py -o max;
-python tests/align/align_create_tensor_torch.py -o min;
-python tests/align/align_create_tensor_torch.py -o linear;
+generate_torch_tensor add;
+generate_torch_tensor concat;
+generate_torch_tensor conv2d;
+generate_torch_tensor cos;
+generate_torch_tensor embedding;
+generate_torch_tensor exp;
+generate_torch_tensor flat;
+generate_torch_tensor getitem;
+generate_torch_tensor identity;
+generate_torch_tensor multiply;
+generate_torch_tensor pool2d;
+generate_torch_tensor reducesum;
+generate_torch_tensor relu;
+generate_torch_tensor reshape;
+generate_torch_tensor scalar_add;
+generate_torch_tensor scalar_multiply;
+generate_torch_tensor scalar_sub;
+generate_torch_tensor scalar_truediv;
+generate_torch_tensor sigmoid;
+generate_torch_tensor sin;
+generate_torch_tensor subtract;
+generate_torch_tensor tanh;
+generate_torch_tensor transpose;
+generate_torch_tensor view_embedding;
+generate_torch_tensor max;
+generate_torch_tensor min;
+generate_torch_tensor linear;
 
 conda activate flexflow;
 python -m pytest tests/align/align_test.py
