3e30a1007c9f5c4f6c89f93811e967a28ed3a0b3
xinhaoc <99570243+xinhaoc@users.noreply.github.com>
2023-02-14 00:15:24 -0500

    add pytorch alignment for operators (#608)
    * add op concat
    
    * add get last operator, add concat alignment
    
    * add alignment pool2d, reducesum, reshape, flat, sin
    
    * create tests and CI for pytorch alignment
    
    * c format
    
    * src and include format
    
    * fix test script
    
    * fix workflow file
    
    * fix pytorch alignment workflow file
    
    * discard the workflow, add in  another branch
    
    * revert readme file
    
    * remove commented tests
    
    * refactor tests, remove old test files
    
    * remove redundancy
    
    * remove code
    
    * remove env
    
    * remove old tests in layernorm, linear
    
    * update align readme
    
    * add test for min/max. linear operators, fix code
    
    * refractor test_all_operators
    
    * fix function
    
    * fix code style

--- align/embedding/gen_tensors.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-eval "$(conda shell.bash hook)";
-rm align/embedding/out/*.pt;
-conda activate flexflow;
-./python/flexflow_python align/embedding/align_embedding_ff.py -ll:py 1 -ll:gpu 1 -ll:fsize 5000 -ll:zsize 4096 -b 16;
-conda activate pytorch;
-python align/embedding/align_embedding_torch.py;
