83cbbfc81cdce72afc70b4c25f3b5f231815f897
xinhaoc <chengxh_98@163.com>
2023-03-14 06:40:40 +0000

    fix

--- tests/align/test_all_operators.sh
+++ tests/align/test_all_operators.sh
@@ -11,70 +11,22 @@ function generate_torch_tensor(){
     python tests/align/align_create_tensor_torch.py -o "$1"
 }
 
+ops=(add concat conv2d cos embedding exp flat getitem identity multiply pool2d reducesum relu reshape scalar_add scalar_multiply scalar_sub scalar_truediv sigmoid sin subtract tanh transpose view_embedding max min linear gather)
+
 #create flexflow tensors
 conda activate flexflow
 conda info --envs
-generate_ff_tensor add
-generate_ff_tensor concat
-generate_ff_tensor conv2d
-generate_ff_tensor cos
-generate_ff_tensor embedding
-generate_ff_tensor exp
-generate_ff_tensor flat
-generate_ff_tensor getitem
-generate_ff_tensor identity
-generate_ff_tensor multiply
-generate_ff_tensor pool2d
-generate_ff_tensor reducesum
-generate_ff_tensor relu
-generate_ff_tensor reshape
-generate_ff_tensor scalar_add
-generate_ff_tensor scalar_multiply
-generate_ff_tensor scalar_sub
-generate_ff_tensor scalar_truediv
-generate_ff_tensor sigmoid
-generate_ff_tensor sin
-generate_ff_tensor subtract
-generate_ff_tensor tanh
-generate_ff_tensor transpose
-generate_ff_tensor view_embedding
-generate_ff_tensor max
-generate_ff_tensor min
-generate_ff_tensor linear
-generate_ff_tensor gather
-
+for(( i=0;i<${#ops[@]};i++)) 
+do
+    generate_ff_tensor ${ops[i]};
+done;
 
 #create torch tensorss
 conda activate pytorch
-generate_torch_tensor add
-generate_torch_tensor concat
-generate_torch_tensor conv2d
-generate_torch_tensor cos
-generate_torch_tensor embedding
-generate_torch_tensor exp
-generate_torch_tensor flat
-generate_torch_tensor getitem
-generate_torch_tensor identity
-generate_torch_tensor multiply
-generate_torch_tensor pool2d
-generate_torch_tensor reducesum
-generate_torch_tensor relu
-generate_torch_tensor reshape
-generate_torch_tensor scalar_add
-generate_torch_tensor scalar_multiply
-generate_torch_tensor scalar_sub
-generate_torch_tensor scalar_truediv
-generate_torch_tensor sigmoid
-generate_torch_tensor sin
-generate_torch_tensor subtract
-generate_torch_tensor tanh
-generate_torch_tensor transpose
-generate_torch_tensor view_embedding
-generate_torch_tensor max
-generate_torch_tensor min
-generate_torch_tensor linear
-generate_torch_tensor gather
+for(( i=0;i<${#ops[@]};i++)) 
+do
+    generate_torch_tensor ${ops[i]};
+done;
 
-conda info --envs
 conda activate flexflow
 python -m pytest tests/align/align_test.py
