4bc729f913f2b72c58a12462032469fe864899aa
Mengdi Zhu <mengdiz@andrew.cmu.edu>
2023-04-03 22:20:42 +0000

    bug fix

--- scripts/multinode_run.sh
+++ scripts/multinode_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /opt/conda/etc/profile.d/conda.sh
+eval "$(conda shell.bash hook)"
 conda activate flexflow
 ~/FlexFlow/python/flexflow_python ~/FlexFlow/examples/python/native/mnist_mlp.py -ll:py 1 -ll:gpu 1 -ll:fsize 8000 -ll:zsize 8000
