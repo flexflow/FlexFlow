c28dd3f4083db129a5295c4e873bb645372c71db
xinhaoc <chengxh_98@163.com>
2023-05-01 03:22:39 +0000

    manually change

--- include/flexflow/batch_config.h
+++ include/flexflow/batch_config.h
@@ -16,109 +16,58 @@
 #pragma once
 
 #include <cstdlib>
-#include <vector>
 
 // #define MAX_SEQ_LEN 1024
 // #define BATCH_SIZE 2
 #define MAX_SEQ_LEN 20
-#define BATCH_SIZE 16
-#define MAX_REQUESTS 256
-#define MAX_BEAM_SIZE 3
+// #define BATCH_SIZE 16
+// #define MAX_REQUESTS 256
 
 namespace FlexFlow {
 
-struct InferenceResult {
-  static int const MAX_NUM_TOKENS = MAX_SEQ_LEN * BATCH_SIZE * MAX_BEAM_SIZE;
-  int results[MAX_NUM_TOKENS];
-  float probs[MAX_NUM_TOKENS];
-  int parent_id[MAX_NUM_TOKENS];
-};
+class InferenceResult;
 
 class BatchConfig {
 public:
+  using RequestGuid = size_t;
+  using TokenId = int;
   BatchConfig();
   bool register_new_request(size_t guid,
                             int initial_len,
-                            int tokens_to_generate,
-                            int beam_width = 1);
-  bool update_beam_tree();
+                            int tokens_to_generate);
   void prepare_next_batch();
-  int update_results(InferenceResult const &ir);
+  int update_results(InferenceResult const *ir);
   void update_num_active_requests_tokens();
-  void update_num_active_requests_tokens_v2();
-  void create_beam_slots(int req_index, int beam_width);
   int num_active_requests() const;
   int num_active_tokens() const;
   void print() const;
-  static int const MAX_NUM_REQUESTS = MAX_REQUESTS;
-  static int const MAX_NUM_TOKENS = InferenceResult::MAX_NUM_TOKENS;
-  static int const MAX_NUM_SUB_REQUESTS = MAX_REQUESTS * MAX_BEAM_SIZE;
-  static int const MAX_NUM_BEAMS = MAX_BEAM_SIZE;
-  // static int const MAX_SEQUENCE_LENGTH = MAX_SEQ_LEN;
+  static int const MAX_NUM_REQUESTS = 8;
+  static int const MAX_NUM_TOKENS = 64;
+
   //  These are set by update
-  int num_tokens, num_requests;
-  bool cached_results;
-  int token_start_idx[MAX_NUM_REQUESTS]; // index of first token in a request
-                                         // that should be processed in the
-                                         // current batch/iteration
-  int token_last_available_idx
-      [MAX_NUM_REQUESTS]; // last valid token index in a request. This includes
-                          // both the prompt and generated tokens
-  int num_processing_tokens[MAX_NUM_REQUESTS]; // a request's number of tokens
-                                               // being processed in the current
-                                               // batch/iteration
-  size_t initial_length[MAX_NUM_REQUESTS];
-  size_t max_sequence_length[MAX_NUM_REQUESTS];
+  int num_tokens;
 
-  struct token_idxs {
-    size_t request_index; // the index within the BatchConfig of the request
-                          // that the token belongs to
-
-    size_t sub_request_index; // the index within the BatchConfig of the sub
-                              // request
-    size_t token_position;    // the index indicating the position of each token
-                              // within its request
-    size_t initial_length;
-    size_t beam_width;
-    int parent_id;
-    int beam_depth;
+  struct PerRequestInfo {
+    int token_start_offset;
+    int num_tokens_in_batch;
+    int max_sequence_length;
+    RequestGuid request_guid;
   };
-
-  struct SampleIdxs {
-    size_t num_samples;
-    size_t guids[InferenceResult::MAX_NUM_TOKENS]; // the guid of the request
-                                                   // each token belongs to
-    token_idxs token_indexes[InferenceResult::MAX_NUM_TOKENS];
+  struct PerTokenInfo {
+    int abs_depth_in_request;
+    int request_index;
+    TokenId token_id;
   };
+  PerRequestInfo requestsInfo[MAX_NUM_REQUESTS];
+  PerTokenInfo tokensInfo[MAX_NUM_TOKENS];
 
-  //<request_id, sub_req_num = corresponding beam width>
-  int sub_requests[MAX_NUM_REQUESTS];
-  // how many parents for the req, init to be 0.
-  struct BeamSlot {
-    BeamSlot(int beam_size){
-      for(int i = 0; i < beam_size; i++){
-         this->probs[i] = 1;
-         this->parent_id[i] = 0;
-      }
-      this->beam_size = beam_size;
-      this->sub_req_size = 1;
-      this->current_depth = 0;
-    }
-    int beam_size;
-    int sub_req_size;
-    int current_depth;
-    long tokens[MAX_BEAM_SIZE];
-    float probs[MAX_BEAM_SIZE];
-    int parent_id[MAX_BEAM_SIZE];
-  };
-
-  // remember the parent id, accumalate probs
-  std::vector<BeamSlot> beam_slots;
-  // std::unordered_map<int, BeamSlot> beam_slots;
-
-  SampleIdxs token2ids;
-  size_t request_guid[MAX_NUM_REQUESTS];
+  // size_t max_sequence_length[MAX_NUM_REQUESTS];
   bool request_completed[MAX_NUM_REQUESTS];
+};
+
+struct InferenceResult {
+  static int const MAX_NUM_TOKENS = BatchConfig::MAX_NUM_TOKENS;
+  BatchConfig::TokenId token_ids[MAX_NUM_TOKENS];
 };
 
 }; // namespace FlexFlow
