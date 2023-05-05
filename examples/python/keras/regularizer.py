f654f513429e5d78102a61766fdbbf0aba7263f1
Soumya Chatterjee <soumyac@stanford.edu>
2023-05-04 23:44:10 -0700

    keras example

---
+++ examples/python/keras/regularizer.py
@@ -0,0 +1,29 @@
+import flexflow.keras as keras
+from flexflow.keras.layers import Dense, Input, Reshape
+from flexflow.keras.backend.internal import gather
+import flexflow.keras.optimizers
+
+import numpy as np
+
+
+def regularizer_example():
+    input0 = Input(shape=(10,), dtype="float32")
+
+    reg = keras.regularizers.L2(0.001)
+    x0 = Dense(16, activation='relu', kernel_regularizer=reg)(input0)
+    out = Dense(1)(x0)
+
+    model = flexflow.keras.models.Model(input0, out)
+
+    opt = flexflow.keras.optimizers.Adam(learning_rate=0.001)
+    model.compile(
+      optimizer=opt, loss='mean_squared_error', metrics=['mean_squared_error'])
+    model.fit(
+        x=np.random.randn(300, 10).astype(np.float32),
+        y=np.random.randn(300, 1).astype(np.float32),
+        epochs=2
+    )
+
+
+if __name__ == '__main__':
+    regularizer_example()
