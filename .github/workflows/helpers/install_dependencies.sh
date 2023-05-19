35d8b8695a59a82f2f6792fb7e84bf4151267a71
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-05-19 13:39:40 +0000

    fix hip_rocm build with sentencepiece

--- .github/workflows/helpers/install_dependencies.sh
+++ .github/workflows/helpers/install_dependencies.sh
@@ -25,6 +25,23 @@ elif [[ "$FF_GPU_BACKEND" == "hip_cuda" || "$FF_GPU_BACKEND" = "hip_rocm" ]]; th
     rm ./amdgpu-install_22.20.50205-1_all.deb
     sudo amdgpu-install -y --usecase=hip,rocm --no-dkms
     sudo apt-get install -y hip-dev hipblas miopen-hip rocm-hip-sdk
+
+    # Install protobuf v3.20.x manually
+    sudo apt-get update -y && sudo apt-get install -y pkg-config zip g++ zlib1g-dev unzip python autoconf automake libtool curl make
+    wget https://github.com/bazelbuild/bazel/releases/download/6.2.0/bazel-6.2.0-installer-linux-x86_64.sh
+    chmod +x bazel-6.2.0-installer-linux-x86_64.sh 
+    ./bazel-6.2.0-installer-linux-x86_64.sh 
+    source /usr/local/lib/bazel/bin/bazel-complete.bash
+    ln -s /usr/local/lib/bazel/bin/bazel.fish /root/.config/fish/completions/bazel.fish
+    git clone -b 3.20.x https://github.com/protocolbuffers/protobuf.git
+    cd protobuf/
+    git submodule update --init --recursive
+    ./autogen.sh
+    ./configure
+    make -j
+    sudo make install
+    sudo ldconfig
+    cd ..
 else
     echo "FF_GPU_BACKEND: ${FF_GPU_BACKEND}. Skipping installing HIP dependencies"
 fi
