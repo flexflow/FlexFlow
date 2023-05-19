66561a61afd3d230059797d35da3cc4576b91a01
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-05-19 13:43:16 +0000

    shellcheck 1

--- .github/workflows/helpers/install_dependencies.sh
+++ .github/workflows/helpers/install_dependencies.sh
@@ -31,7 +31,8 @@ elif [[ "$FF_GPU_BACKEND" == "hip_cuda" || "$FF_GPU_BACKEND" = "hip_rocm" ]]; th
     wget https://github.com/bazelbuild/bazel/releases/download/6.2.0/bazel-6.2.0-installer-linux-x86_64.sh
     chmod +x bazel-6.2.0-installer-linux-x86_64.sh 
     ./bazel-6.2.0-installer-linux-x86_64.sh 
-    source /usr/local/lib/bazel/bin/bazel-complete.bash
+    echo "source /usr/local/lib/bazel/bin/bazel-complete.bash" >> ~/.bashrc
+    source ~/.bashrc
     ln -s /usr/local/lib/bazel/bin/bazel.fish /root/.config/fish/completions/bazel.fish
     git clone -b 3.20.x https://github.com/protocolbuffers/protobuf.git
     cd protobuf/
