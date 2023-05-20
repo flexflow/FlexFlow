fb33a29c21a5744fbeafb1cfc4265ac0354ebe11
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-05-20 17:48:24 +0000

    add inference test, script to downlaod weights

---
+++ inference/utils/download_weights.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import requests
+from transformers import AutoModelForCausalLM
+
+# Change working dir to folder storing this script
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+def convert_hf_model(model, dst_folder):
+    os.makedirs(dst_folder, exist_ok=True)
+    for name, params in model.named_parameters():
+        name = (
+            name.replace(".", "_")
+            .replace("self_attn", "attention")
+            .replace("q_proj", "wq")
+            .replace("k_proj", "wk")
+            .replace("v_proj", "wv")
+            .replace("o_proj", "wo")
+            .replace("mlp", "feed_forward")
+            .replace("gate_proj", "w1")
+            .replace("down_proj", "w2")
+            .replace("up_proj", "w3")
+            .replace("input_layernorm", "attention_norm")
+            .replace("post_attention_layernorm", "ffn_norm")
+            .replace("embed_tokens", "tok_embeddings")
+            .replace("lm_head", "output")
+            .replace("model_", "")
+        )
+        params.detach().cpu().numpy().tofile(f"{dst_folder}/{name}")
+
+# Download and convert big model weights
+model = AutoModelForCausalLM.from_pretrained("decapoda-research/llama-7b-hf")
+dst_folder="../weights/llama_7B_weights"
+convert_hf_model(model, dst_folder)
+
+# Download and convert small model weights
+model = AutoModelForCausalLM.from_pretrained("Bingsu/llama-190m-arch")
+dst_folder="../weights/llama_190M_weights"
+convert_hf_model(model, dst_folder)
+
+# Download tokenizer
+os.makedirs("../tokenizer", exist_ok=True)
+tokenizer_filepath = '../tokenizer/tokenizer.model'
+url = 'https://specinfer.s3.us-east-2.amazonaws.com/tokenizer/tokenizer.model'
+r = requests.get(url)
+open(tokenizer_filepath , 'wb').write(r.content)
---
+++ tests/inference_tests.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env bash
+set -x
+set -e
+
+cleanup() {
+    rm -rf ../inference/prompt ../inference/weights ../inference/tokenizer
+}
+
+# Cd into directory holding this script
+cd "${BASH_SOURCE[0]%/*}"
+
+# Clean up before test (just in case)
+cleanup
+
+# Download the weights
+python3 ../inference/utils/download_weights.py
+
+# Create test prompt file
+mkdir -p ../inference/prompt
+echo '["Give three tips for staying healthy."]' > ../inference/prompt/test.json
+
+# Run test
+../build/inference/spec_infer/spec_infer -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-weight ../inference/weights/llama_7B_weights/ -ssm-weight ../inference/weights/llama_190M_weights/ -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json -ssm-config ../inference/models/configs/llama_190M.json -llm-config ../inference/models/configs/llama_7B.json
+
+
+# Clean up after test
+cleanup
