d7dd6bbe0fbe018623bfde715ab50795f77c9dd6
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-06-01 04:55:51 +0800

    [Inference] - Alignment tests (#742)
    * save output to file
    
    * add alignment tests
    
    * fix
    
    * change conflicting name, add comments
    
    * fix typo
    
    * formatting
    
    * more comments and clean dead code
    
    * formatting
    
    * fixed issue with length mismatch
    
    * fix ci skip
    
    * update inf test
    
    * add precision selection support in incr decoding

--- include/flexflow/batch_config.h
+++ include/flexflow/batch_config.h
@@ -74,6 +74,7 @@ public:
   TreeVerifyBatchConfig();
   ~TreeVerifyBatchConfig();
   InferenceMode get_mode() const;
+  void print() const;
   // struct PerTokenInfo : BatchConfig::PerTokenInfo {
   //   int tree_branch_idx;
   // };
@@ -86,7 +87,7 @@ public:
   // void compute_tree_branch_indexes();
 
   int num_tokens_to_commit;
-  CommittedTokensInfo commited_tokens[MAX_NUM_TOKENS];
+  CommittedTokensInfo committed_tokens[MAX_NUM_TOKENS];
 };
 
 struct InferenceResult {
@@ -104,6 +105,8 @@ public:
 
   void print() const;
   bool done() const;
+  int max_beam_depth_all_requests() const;
+  int current_depth_all_requests() const;
 
   size_t beam_width;
   size_t target_iterations;
@@ -111,7 +114,6 @@ public:
   static int const MAX_BEAM_DEPTH = 8;
 
   struct BeamSearchPerRequestInfo {
-    bool request_completed;
     int beam_size;
     int current_depth = -1;
     int max_depth = MAX_BEAM_DEPTH;
--- tests/inference_tests.sh
+++ tests/inference_tests.sh
@@ -31,31 +31,47 @@ copy_embedding_weights
 mkdir -p ../inference/prompt
 echo '["Give three tips for staying healthy."]' > ../inference/prompt/test.json
 
+# Create output folder
+mkdir -p ../inference/output
+
 ###############################################################################################
 ############################ Speculative inference tests ######################################
 ###############################################################################################
 
 # LLAMA
-../build/inference/spec_infer/spec_infer -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model llama -llm-weight ../inference/weights/llama_7B_weights/ -llm-config ../inference/models/configs/llama_7B.json -ssm-model llama -ssm-weight ../inference/weights/llama_160M_weights/ -ssm-config ../inference/models/configs/llama_160M.json -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json
+../build/inference/spec_infer/spec_infer -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 --use-full-precision -llm-model llama -llm-weight ../inference/weights/llama_7B_weights/ -llm-config ../inference/models/configs/llama_7B.json -ssm-model llama -ssm-weight ../inference/weights/llama_160M_weights/ -ssm-config ../inference/models/configs/llama_160M.json -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json -output-file ../inference/output/spec_inference_llama.txt
 
 # OPT
-../build/inference/spec_infer/spec_infer -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model opt -llm-weight ../inference/weights/opt_6B_weights/ -llm-config ../inference/models/configs/opt_6B.json -ssm-model opt -ssm-weight ../inference/weights/opt_125M_weights/ -ssm-config ../inference/models/configs/opt_125M.json -tokenizer ../inference/tokenizer/ -prompt ../inference/prompt/test.json
+../build/inference/spec_infer/spec_infer -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 --use-full-precision -llm-model opt -llm-weight ../inference/weights/opt_6B_weights/ -llm-config ../inference/models/configs/opt_6B.json -ssm-model opt -ssm-weight ../inference/weights/opt_125M_weights/ -ssm-config ../inference/models/configs/opt_125M.json -tokenizer ../inference/tokenizer/ -prompt ../inference/prompt/test.json -output-file ../inference/output/spec_inference_opt.txt
 
 ###############################################################################################
 ############################ Incremental decoding tests #######################################
 ###############################################################################################
 
 # LLAMA (small model)
-../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model llama -llm-weight ../inference/weights/llama_160M_weights/ -llm-config ../inference/models/configs/llama_160M.json -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json
+../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 --use-full-precision -llm-model llama -llm-weight ../inference/weights/llama_160M_weights/ -llm-config ../inference/models/configs/llama_160M.json -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json -output-file ../inference/output/incr_decoding_llama_160M.txt
 
 # LLAMA (big model)
-../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model llama -llm-weight ../inference/weights/llama_7B_weights/ -llm-config ../inference/models/configs/llama_7B.json -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json
+../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 --use-full-precision -llm-model llama -llm-weight ../inference/weights/llama_7B_weights/ -llm-config ../inference/models/configs/llama_7B.json -tokenizer ../inference/tokenizer/tokenizer.model -prompt ../inference/prompt/test.json -output-file ../inference/output/incr_decoding_llama_7B.txt
 
 # OPT (small model)
-../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model opt -llm-weight ../inference/weights/opt_125M_weights/ -llm-config ../inference/models/configs/opt_125M.json -tokenizer ../inference/tokenizer/ -prompt ../inference/prompt/test.json
+../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 --use-full-precision -llm-model opt -llm-weight ../inference/weights/opt_125M_weights/ -llm-config ../inference/models/configs/opt_125M.json -tokenizer ../inference/tokenizer/ -prompt ../inference/prompt/test.json -output-file ../inference/output/incr_decoding_opt_125M.txt
 
 # OPT (big model)
-../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model opt -llm-weight ../inference/weights/opt_6B_weights/ -llm-config ../inference/models/configs/opt_6B.json -tokenizer ../inference/tokenizer/ -prompt ../inference/prompt/test.json
+../build/inference/incr_decoding/incr_decoding -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 --use-full-precision -llm-model opt -llm-weight ../inference/weights/opt_6B_weights/ -llm-config ../inference/models/configs/opt_6B.json -tokenizer ../inference/tokenizer/ -prompt ../inference/prompt/test.json -output-file ../inference/output/incr_decoding_opt_6B.txt
+
+
+###############################################################################################
+################################## Alignment tests ############################################
+###############################################################################################
+
+diff ../inference/output/incr_decoding_llama_7B.txt ../inference/output/spec_inference_llama.txt
+diff ../inference/output/incr_decoding_opt_6B.txt ../inference/output/spec_inference_opt.txt
+
+
+###############################################################################################
+###################################### Cleanup ################################################
+###############################################################################################
 
 # Clean up after test
 cleanup
