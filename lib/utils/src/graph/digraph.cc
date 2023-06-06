521aca502d5cba349ffa2720e39758ad46cb8426
lambda7xx <lambda7xx@gmail.com>
2023-06-06 09:50:23 +0000

    finish DirectedEdgeQuery query_intersection method

--- lib/utils/src/graph/digraph.cc
+++ lib/utils/src/graph/digraph.cc
@@ -1,4 +1,6 @@
 #include "utils/graph/digraph.h"
+#include "utils/containers.h"
+
 
 namespace FlexFlow {
 
@@ -15,6 +17,16 @@ std::ostream &operator<<(std::ostream &s, DirectedEdge const &e) {
 DirectedEdgeQuery::DirectedEdgeQuery(tl::optional<std::unordered_set<Node>> const &srcs, tl::optional<std::unordered_set<Node>> const &dsts) 
   : srcs(srcs), dsts(dsts)
 { }
+
+DirectedEdgeQuery query_intersection(DirectedEdgeQuery const &lhs, DirectedEdgeQuery const &rhs){
+  assert (lhs.srcs.has_value() && lhs.dsts.has_value() && rhs.srcs.has_value() && rhs.dsts.has_value());
+
+
+  tl::optional<std::unordered_set<Node>> srcs_t1 = intersection(*lhs.srcs, *rhs.srcs);
+  tl::optional<std::unordered_set<Node>> dsts_t1 = intersection(*lhs.dsts, *rhs.dsts);
+
+  return DirectedEdgeQuery(srcs_t1, dsts_t1);
+}
 
 DiGraph::DiGraph(DiGraph const &other)
   : ptr(other.ptr->clone())
