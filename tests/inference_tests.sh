e13190852c98f084b49d5ade1b92d7bf0c4354cd
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-06-15 18:52:41 +0800

    Fix inference test (#767)
    * fix
    
    * fix workflow

--- tests/inference_tests.sh
+++ tests/inference_tests.sh
@@ -13,7 +13,7 @@ cd "${BASH_SOURCE[0]%/*}"
 cleanup
 
 # Update the transformers library to support the LLAMA model
-pip3 install --upgrade transformers
+pip3 install --upgrade transformers sentencepiece 
 
 # Download the weights in both half and full precision
 python3 ../inference/utils/download_llama_weights.py
