ae16ede000f1fdc9596805268fb47e24d7975171
DerrickYLJ <lijiey@andrew.cmu.edu>
2023-06-21 00:34:12 -0400

    update container name

--- docker/publish.sh
+++ docker/publish.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # Cd into directory holding this script
 cd "${BASH_SOURCE[0]%/*}"
 
-image=${1:-"flexflow-cuda"}
-if [[ "${image}" != @(flexflow-environment-cuda|flexflow-environment-hip_cuda|flexflow-environment-hip_rocm|flexflow-environment-intel|flexflow-cuda|flexflow-hip_cuda|flexflow-hip_rocm|flexflow-intel) ]]; then
-  echo "Error, image name ${image} is invalid. Choose between 'flexflow-environment-{cuda,hip_cuda,hip_rocm,intel}' and 'flexflow-{cuda,hip_cuda,hip_rocm,intel}'."
+image=${1:-"specinfer-cuda"}
+if [[ "${image}" != @(specinfer-environment-cuda|specinfer-environment-hip_cuda|specinfer-environment-hip_rocm|specinfer-environment-intel|specinfer-cuda|specinfer-hip_cuda|specinfer-hip_rocm|specinfer-intel) ]]; then
+  echo "Error, image name ${image} is invalid. Choose between 'specinfer-environment-{cuda,hip_cuda,hip_rocm,intel}' and 'specinfer-{cuda,hip_cuda,hip_rocm,intel}'."
   exit 1
 fi
 
@@ -18,13 +18,10 @@ FLEXFLOW_CONTAINER_TOKEN=${FLEXFLOW_CONTAINER_TOKEN:-}
 if [ -z "$FLEXFLOW_CONTAINER_TOKEN" ]; then echo "FLEXFLOW_CONTAINER_TOKEN secret is not available, cannot publish the docker image to ghrc.io"; exit; fi
 echo "$FLEXFLOW_CONTAINER_TOKEN" | docker login ghcr.io -u flexflow --password-stdin
 
-# Obtain Branch Name
-branch=$(git branch | sed -n -e 's/^\* \(.*))/\1/p')
-
 # Tag image to be uploaded
 git_sha=${GITHUB_SHA:-$(git rev-parse HEAD)}
 if [ -z "$git_sha" ]; then echo "Commit hash cannot be detected, cannot publish the docker image to ghrc.io"; exit; fi
-docker tag "$image":latest ghcr.io/flexflow/"$image"_"$branch":latest
+docker tag "$image":latest ghcr.io/flexflow/"$image":latest
 
 # Upload image
-docker push ghcr.io/flexflow/"$image"_"$branch":latest
+docker push ghcr.io/flexflow/"$image":latest
