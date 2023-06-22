e5322df55af4f76f1f3053538c6a4e2b13fa0711
DerrickYLJ <lijiey@andrew.cmu.edu>
2023-06-22 14:12:41 -0400

    specinfer env publish

--- docker/publish.sh
+++ docker/publish.sh
@@ -4,9 +4,16 @@ set -euo pipefail
 # Cd into directory holding this script
 cd "${BASH_SOURCE[0]%/*}"
 
-image=${1:-"specinfer-cuda"}
-if [[ "${image}" != @(specinfer-environment-cuda|specinfer-environment-hip_cuda|specinfer-environment-hip_rocm|specinfer-environment-intel|specinfer-cuda|specinfer-hip_cuda|specinfer-hip_rocm|specinfer-intel) ]]; then
-  echo "Error, image name ${image} is invalid. Choose between 'specinfer-environment-{cuda,hip_cuda,hip_rocm,intel}' and 'specinfer-{cuda,hip_cuda,hip_rocm,intel}'."
+image=${1:-"flexflow-cuda"}
+# Check publish specinfer environment image
+if [[ "${image}" == @(specinfer-environment-cuda|specinfer-environment-hip_cuda|specinfer-environment-hip_rocm|specinfer-environment-intel) ]]; then
+  echo "specinfer does not publish environment images"
+  exit 1
+fi
+
+# Check valid image name
+if [[ "${image}" != @(flexflow-environment-cuda|flexflow-environment-hip_cuda|flexflow-environment-hip_rocm|flexflow-environment-intel|flexflow-cuda|flexflow-hip_cuda|flexflow-hip_rocm|flexflow-intel|specinfer-cuda|specinfer-hip_cuda|specinfer-hip_rocm|specinfer-intel) ]]; then
+  echo "Error, image name ${image} is invalid. Choose between 'flexflow-environment-{cuda,hip_cuda,hip_rocm,intel}' and 'flexflow-{cuda,hip_cuda,hip_rocm,intel}'."
   exit 1
 fi
 
