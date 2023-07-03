142db3b16c49603396153f41d22f4701049e4c54
Wei Wu <eddy16112@gmail.com>
2023-07-02 22:15:58 -0700

    fixup

--- python/flexflow/keras/backend/__init__.py
+++ python/flexflow/keras/backend/__init__.py
@@ -24,7 +24,6 @@ _BACKEND = 'flexflow'
 # import backend
 if _BACKEND == 'flexflow':
     sys.stderr.write('Using flexflow backend.\n')
-    assert 0
     from .flexflow_backend import *
 else:
     raise ValueError('Unknown backend: ' + str(_BACKEND))
