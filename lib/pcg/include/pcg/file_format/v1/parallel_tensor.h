cda0f7960ffc0cea84eab79f2463ac96a9bde9fc
Colin Unger <lockshaw@lockshaw.net>
2023-07-05 16:34:35 -0700

    Update v1/parallel_tensor.h to new visitable interface

--- lib/pcg/include/pcg/file_format/v1/parallel_tensor.h
+++ lib/pcg/include/pcg/file_format/v1/parallel_tensor.h
@@ -10,39 +10,27 @@
 
 namespace FlexFlow {
 
-struct V1ParallelDim : public use_visitable_cmp<V1ParallelDim> {
-  size_t size;
-  int degree;
-  bool is_replica_dim;
+struct V1ParallelDim {
+  req<size_t> size;
+  req<int> degree;
+  req<bool> is_replica_dim;
 };
+FF_VISITABLE_STRUCT(V1ParallelDim, size, degree, is_replica_dim);
 
-struct V1ParallelTensorShape : public use_visitable_cmp<V1ParallelTensorShape> {
-  std::vector<V1ParallelDim> dims;
-  V1DataType data_type;
+struct V1ParallelTensorShape {
+  req<std::vector<V1ParallelDim>> dims;
+  req<V1DataType> data_type;
 };
+FF_VISITABLE_STRUCT(V1ParallelTensorShape, dims, data_type);
 
-struct V1ParallelTensor : public use_visitable_cmp<V1ParallelTensor> {
+struct V1ParallelTensor {
   V1ParallelTensorShape shape;
-  optional<V1ParamSync> sync_type;
-  optional<V1Initializer> initializer;
-  bool create_grad;
+  req<optional<V1ParamSync>> sync_type;
+  req<optional<V1Initializer>> initializer;
+  req<bool> create_grad;
 };
+FF_VISITABLE_STRUCT(V1ParallelTensor, shape, sync_type, initializer, create_grad);
 
-}
-
-VISITABLE_STRUCT(::FlexFlow::V1ParallelDim, size, degree, is_replica_dim);
-MAKE_VISIT_HASHABLE(::FlexFlow::V1ParallelDim);
-
-VISITABLE_STRUCT(::FlexFlow::V1ParallelTensorShape, dims, data_type);
-MAKE_VISIT_HASHABLE(::FlexFlow::V1ParallelTensorShape);
-
-VISITABLE_STRUCT(::FlexFlow::V1ParallelTensor, shape, sync_type, initializer, create_grad);
-MAKE_VISIT_HASHABLE(::FlexFlow::V1ParallelTensor);
-
-namespace FlexFlow {
-static_assert(is_jsonable<V1ParallelDim>::value, "");
-static_assert(is_jsonable<V1ParallelTensorShape>::value, "");
-static_assert(is_jsonable<V1ParallelTensor>::value, "");
 }
 
 #endif
