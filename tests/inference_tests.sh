eb948f84bef29f7db070deea1749bd8e0e25e704
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-07-07 23:02:04 +0000

    check incr decoding steps instead of latency

--- tests/inference_tests.sh
+++ tests/inference_tests.sh
@@ -111,30 +111,6 @@ fi
 ###############################################################################################
 
 ##################################### Helper functions #######################################
-function compare_speed_spec_infer_incr_decoding {
-    local incrDec_file="$1"
-    local specInf_file="$2"
-
-    # Read the float numbers from the first line of the files
-    incrDec=$(sed -n '1 s/end-to-end latency: \(.*\)/\1/p' "$incrDec_file")
-    specInf=$(sed -n '1 s/end-to-end latency: \(.*\)/\1/p' "$specInf_file")
-
-    if ! command -v bc &> /dev/null; then
-        echo "bc is not installed. Installing..."
-        sudo apt-get install -y bc
-    fi
-    
-    # Perform the comparison
-    threshold=$(bc <<< "$specInf * 1.5")
-    if (( $(echo "$incrDec >= $threshold" | bc -l) )); then
-        #echo "The latency in $specInf_file is at least 1.5x smaller than the latency from $incrDec_file."
-        :
-    else
-        echo "Error: The latency in $specInf_file is not at least 1.5x smaller than the latency in $incrDec_file!"
-        exit 1
-    fi
-}
-
 function check_partial_token_match {
     local file1="$1"
     local file2="$2"
@@ -162,44 +138,92 @@ function check_partial_token_match {
     #echo "The first $num_tokens_to_match integers are identical."
 }
 
+function compare_speed_spec_infer_incr_decoding {
+    local incrDec_file="$1"
+    local specInf_file="$2"
+
+    # Read the float numbers from the first line of the files
+    incrDec=$(sed -n '1 s/end-to-end latency: \(.*\)/\1/p' "$incrDec_file")
+    specInf=$(sed -n '1 s/end-to-end latency: \(.*\)/\1/p' "$specInf_file")
+
+    if ! command -v bc &> /dev/null; then
+        echo "bc is not installed. Installing..."
+        sudo apt-get install -y bc
+    fi
+    
+    # Perform the comparison
+    threshold=$(bc <<< "$specInf * 1.5")
+    if (( $(echo "$incrDec >= $threshold" | bc -l) )); then
+        #echo "The latency in $specInf_file is at least 1.5x smaller than the latency from $incrDec_file."
+        :
+    else
+        echo "Error: The latency in $specInf_file is not at least 1.5x smaller than the latency in $incrDec_file!"
+        exit 1
+    fi
+}
+
+function compare_decoding_steps_spec_infer_incr_decoding {
+    local incrDec_file="$1"
+    local specInf_file="$2"
+
+    # Read the number of decoding steps from the second line of the files
+    second_line=$(sed -n '2p' "$incrDec_file")
+    read -r line <<< "$second_line"
+    incrDec=${line#*: }
+    second_line=$(sed -n '2p' "$specInf_file")
+    read -r line <<< "$second_line"
+    specInf=${line#*: }
+
+    if ! command -v bc &> /dev/null; then
+        echo "bc is not installed. Installing..."
+        sudo apt-get install -y bc
+    fi
+    
+    # Perform the comparison
+    threshold=$(bc <<< "$specInf * 1.5")
+    if (( $(echo "$incrDec >= $threshold" | bc -l) )); then
+        #echo "The decoding steps in $specInf_file are at least 1.5x less than those in $incrDec_file."
+        :
+    else
+        echo "Error: The decoding steps in $specInf_file are not at least 1.5x less than those in $incrDec_file!"
+        exit 1
+    fi
+}
+
 ############ Alignment between speculative inference and incremental decoding #################
 # Full precision
-diff <(tail -n +2 "../inference/output/incr_decoding_llama_7B.txt") <(tail -n +2 "../inference/output/spec_inference_llama.txt")
-diff <(tail -n +2 "../inference/output/incr_decoding_opt_6B.txt") <(tail -n +2 "../inference/output/spec_inference_opt.txt")
+diff <(tail -n +3 "../inference/output/incr_decoding_llama_7B.txt") <(tail -n +3 "../inference/output/spec_inference_llama.txt")
+diff <(tail -n +3 "../inference/output/incr_decoding_opt_6B.txt")   <(tail -n +3 "../inference/output/spec_inference_opt.txt")
 # Half precision
 check_partial_token_match "../inference/output/incr_decoding_llama_7B_half.txt" "../inference/output/spec_inference_llama_half.txt"
-#diff <(tail -n +2 "../inference/output/incr_decoding_llama_7B_half.txt") <(tail -n +2 "../inference/output/spec_inference_llama_half.txt")
 check_partial_token_match "../inference/output/incr_decoding_opt_6B_half.txt" "../inference/output/spec_inference_opt_half.txt"
-#diff <(tail -n +2 "../inference/output/incr_decoding_opt_6B_half.txt" ) <(tail -n +2 "../inference/output/spec_inference_opt_half.txt")
 
 # Speed test: speculative inference should be at very least 1.5x faster than incremental decoding
 # Full precision
-compare_speed_spec_infer_incr_decoding "../inference/output/incr_decoding_llama_7B.txt" "../inference/output/spec_inference_llama.txt"
-compare_speed_spec_infer_incr_decoding "../inference/output/incr_decoding_opt_6B.txt" "../inference/output/spec_inference_opt.txt"
+#compare_speed_spec_infer_incr_decoding "../inference/output/incr_decoding_llama_7B.txt" "../inference/output/spec_inference_llama.txt"
+#compare_speed_spec_infer_incr_decoding "../inference/output/incr_decoding_opt_6B.txt" "../inference/output/spec_inference_opt.txt"
+compare_decoding_steps_spec_infer_incr_decoding "../inference/output/incr_decoding_llama_7B.txt" "../inference/output/spec_inference_llama.txt"
+compare_decoding_steps_spec_infer_incr_decoding "../inference/output/incr_decoding_opt_6B.txt" "../inference/output/spec_inference_opt.txt"
 # Half precision
 #compare_speed_spec_infer_incr_decoding "../inference/output/incr_decoding_llama_7B_half.txt" "../inference/output/spec_inference_llama_half.txt"
 #compare_speed_spec_infer_incr_decoding "../inference/output/incr_decoding_opt_6B_half.txt" "../inference/output/spec_inference_opt_half.txt"
+compare_decoding_steps_spec_infer_incr_decoding "../inference/output/incr_decoding_llama_7B_half.txt" "../inference/output/spec_inference_llama_half.txt"
+compare_decoding_steps_spec_infer_incr_decoding "../inference/output/incr_decoding_opt_6B_half.txt" "../inference/output/spec_inference_opt_half.txt"
 
 ############ Alignment between tensor model parallelism and pipeline parallelism only #################
 if [ "$TENSOR_PARALLELISM_TESTS" = "ON" ]; then
-    diff <(tail -n +2 "../inference/output/spec_inference_llama_tp.txt") <(tail -n +2 "../inference/output/spec_inference_llama.txt")
-    diff <(tail -n +2 "../inference/output/spec_inference_opt_tp.txt") <(tail -n +2 "../inference/output/spec_inference_opt.txt")
+    diff <(tail -n +3 "../inference/output/spec_inference_llama_tp.txt") <(tail -n +3 "../inference/output/spec_inference_llama.txt")
+    diff <(tail -n +3 "../inference/output/spec_inference_opt_tp.txt")  <(tail -n +3 "../inference/output/spec_inference_opt.txt")
     check_partial_token_match "../inference/output/spec_inference_llama_half_tp.txt" "../inference/output/spec_inference_llama_half.txt"
-    #diff <(tail -n +2 "../inference/output/spec_inference_llama_half_tp.txt") <(tail -n +2 "../inference/output/spec_inference_llama_half.txt")
     check_partial_token_match "../inference/output/spec_inference_opt_half_tp.txt" "../inference/output/spec_inference_opt_half.txt"
-    #diff <(tail -n +2 "../inference/output/spec_inference_opt_half_tp.txt") <(tail -n +2 "../inference/output/spec_inference_opt_half.txt")
-    diff <(tail -n +2 "../inference/output/incr_decoding_llama_160M_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_llama_160M.txt")
+    diff <(tail -n +3 "../inference/output/incr_decoding_llama_160M_tp.txt") <(tail -n +3 "../inference/output/incr_decoding_llama_160M.txt")
     check_partial_token_match "../inference/output/incr_decoding_llama_160M_half_tp.txt" "../inference/output/incr_decoding_llama_160M_half.txt"
-    # diff <(tail -n +2 "../inference/output/incr_decoding_llama_160M_half_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_llama_160M_half.txt")
-    diff <(tail -n +2 "../inference/output/incr_decoding_llama_7B_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_llama_7B.txt")
+    diff <(tail -n +3 "../inference/output/incr_decoding_llama_7B_tp.txt") <(tail -n +3 "../inference/output/incr_decoding_llama_7B.txt")
     check_partial_token_match "../inference/output/incr_decoding_llama_7B_half_tp.txt" "../inference/output/incr_decoding_llama_7B_half.txt"
-    # diff <(tail -n +2 "../inference/output/incr_decoding_llama_7B_half_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_llama_7B_half.txt")
-    diff <(tail -n +2 "../inference/output/incr_decoding_opt_125M_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_opt_125M.txt")
+    diff <(tail -n +3 "../inference/output/incr_decoding_opt_125M_tp.txt") <(tail -n +3 "../inference/output/incr_decoding_opt_125M.txt")
     check_partial_token_match "../inference/output/incr_decoding_opt_125M_half_tp.txt" "../inference/output/incr_decoding_opt_125M_half.txt"
-    # diff <(tail -n +2 "../inference/output/incr_decoding_opt_125M_half_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_opt_125M_half.txt")
-    diff <(tail -n +2 "../inference/output/incr_decoding_opt_6B_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_opt_6B.txt")
+    diff <(tail -n +3 "../inference/output/incr_decoding_opt_6B_tp.txt") <(tail -n +3 "../inference/output/incr_decoding_opt_6B.txt")
     check_partial_token_match "../inference/output/incr_decoding_opt_6B_half_tp.txt" "../inference/output/incr_decoding_opt_6B_half.txt"
-    # diff <(tail -n +2 "../inference/output/incr_decoding_opt_6B_half_tp.txt") <(tail -n +2 "../inference/output/incr_decoding_opt_6B_half.txt")
 fi
 
 ######################### Alignment tests with HuggingFace ####################################
@@ -229,15 +253,15 @@ python3 ./inference/huggingface_inference.py --model-name "facebook/opt-125m" --
 # OPT (big model, half precision)
 #python3 ./inference/huggingface_inference.py --model-name "facebook/opt-6.7b" --tokenizer-model-name "facebook/opt-6.7b" --prompt-file "../../inference/prompt/test.json" --output-file "../../inference/output/huggingface_opt_6B_half.txt" --gpu --max-length 127
 
-diff <(tail -n +2 "../inference/output/huggingface_llama_160M.txt") <(tail -n +4 "../inference/output/incr_decoding_llama_160M.txt")
-diff <(tail -n +2 "../inference/output/huggingface_llama_160M_half.txt") <(tail -n +4 "../inference/output/incr_decoding_llama_160M_half.txt")
-diff <(tail -n +2 "../inference/output/huggingface_llama_7B.txt") <(tail -n +4 "../inference/output/incr_decoding_llama_7B.txt")
-diff <(tail -n +2 "../inference/output/huggingface_llama_7B_half.txt") <(tail -n +4 "../inference/output/incr_decoding_llama_7B_half.txt")
+diff <(tail -n +2 "../inference/output/huggingface_llama_160M.txt") <(tail -n +5 "../inference/output/incr_decoding_llama_160M.txt")
+diff <(tail -n +2 "../inference/output/huggingface_llama_160M_half.txt") <(tail -n +5 "../inference/output/incr_decoding_llama_160M_half.txt")
+diff <(tail -n +2 "../inference/output/huggingface_llama_7B.txt") <(tail -n +5 "../inference/output/incr_decoding_llama_7B.txt")
+diff <(tail -n +2 "../inference/output/huggingface_llama_7B_half.txt") <(tail -n +5 "../inference/output/incr_decoding_llama_7B_half.txt")
 
-diff <(tail -n +2 "../inference/output/huggingface_opt_125M.txt") <(tail -n +4 "../inference/output/incr_decoding_opt_125M.txt")
-diff <(tail -n +2 "../inference/output/huggingface_opt_125M_half.txt") <(tail -n +4 "../inference/output/incr_decoding_opt_125M_half.txt")
-#diff <(tail -n +2 "../inference/output/huggingface_opt_6B.txt") <(tail -n +4 "../inference/output/incr_decoding_opt_6B.txt")
-#diff <(tail -n +2 "../inference/output/huggingface_opt_6B_half.txt") <(tail -n +4 "../inference/output/incr_decoding_opt_6B_half.txt")
+diff <(tail -n +2 "../inference/output/huggingface_opt_125M.txt") <(tail -n +5 "../inference/output/incr_decoding_opt_125M.txt")
+diff <(tail -n +2 "../inference/output/huggingface_opt_125M_half.txt") <(tail -n +5 "../inference/output/incr_decoding_opt_125M_half.txt")
+#diff <(tail -n +2 "../inference/output/huggingface_opt_6B.txt") <(tail -n +5 "../inference/output/incr_decoding_opt_6B.txt")
+#diff <(tail -n +2 "../inference/output/huggingface_opt_6B_half.txt") <(tail -n +5 "../inference/output/incr_decoding_opt_6B_half.txt")
 
 ###############################################################################################
 ###################################### Cleanup ################################################
