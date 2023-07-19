8304b3d776b8bebf8b81b473b72776267c84287e
wmdi <wmengd@gmail.com>
2023-07-20 00:58:24 +0800

    add unit test for unity algorithm

--- lib/compiler/test/test_generator.h
+++ lib/compiler/test/test_generator.h
@@ -3,6 +3,7 @@
 
 #include "compiler/machine_mapping.h"
 #include "compiler/sub_parallel_computation_graph.h"
+#include "rapidcheck.h"
 
 namespace rc {
 
@@ -31,6 +32,84 @@ struct Arbitrary<MachineMapping> {
         gen::set(&MachineMapping::machine_views,
                  gen::container<std::unordered_map<Node, MachineView>>(
                      gen::arbitrary<Node>(), gen::arbitrary<MachineView>())));
+  }
+}
+
+template <>
+struct Arbitrary<ComputationGraph> {
+  static Gen<ComputationGraph> arbitrary() {
+    return gen::apply(
+        [](int num_nodes,
+           std::vector<int> const &lhs,
+           std::vector<int> const &rhs,
+           std::vector<int> const &compn_type) {
+          auto g =
+              ::create<UnorderedOutputLabelledMultiDiGraph<Layer, Tensor>>();
+
+          std::vector<Node> nodes, source, sink;
+
+          for (int i = 0; i < num_nodes; ++i) {
+            Node new_node = g.add_node(Layer(NoopAttrs{}, nullopt));
+            nodes.push_back(new_node);
+            source.push_back(new_node);
+            sink.push_back(new_node);
+          }
+
+          for (int i = 0; i < lhs.size(); ++i) {
+            if (i >= rhs.size()) {
+              break;
+            }
+            if (i >= compn_type.size()) {
+              break;
+            }
+
+            int n0 = lhs[i] % num_nodes, n1 = rhs[i] % num_nodes,
+                t = compn_type[i] % 2;
+
+            Node source0 = source[n0], source1 = source[n1];
+            Node sink0 = sink[n0], sink1 = sink[n1];
+
+            if (source0 == source1 && sink0 == sink1) {
+              continue;
+            }
+
+            RC_ASSERT(source0 != source1 && sink0 != sink1);
+
+            if (source0 == sink0 || t == 0) {
+              // sequential composition
+              g.add_edge(MultiDiOutput{sink0, NodePort(0)},
+                         MultiDiInput{source1, NodePort(0)});
+              for (int j = 0; j < nodes.size(); ++j) {
+                if (source[j] == source1) {
+                  source[j] = source0;
+                }
+                if (sink[j] == sink0) {
+                  sink[j] = sink1;
+                }
+              }
+            } else {
+              // parallel composition
+              g.add_edge(MultiDiOutput{source0, NodePort(0)},
+                         MultiDiInput{source1, NodePort(0)});
+              g.add_edge(MultiDiOutput{sink1, NodePort(0)},
+                         MultiDiInput{sink0, NodePort(0)});
+              for (int j = 0; j < nodes.size(); ++j) {
+                if (source[j] == source1) {
+                  source[j] = source0;
+                }
+                if (sink[j] == sink1) {
+                  sink[j] = sink0;
+                }
+              }
+            }
+          }
+
+          return ComputationGraph(g);
+        },
+        gen::inRange(1, 200),
+        gen::arbitrary<vector<int>>(),
+        gen::arbitrary<vector<int>>(),
+        gen::arbitrary<vector<int>>());
   }
 }
 
@@ -89,9 +168,9 @@ struct Arbitrary<ParallelComputationGraph> {
             } else {
               // parallel composition
               g.add_edge(MultiDiOutput{source0, NodePort(0)},
-                         MultiDiInput(source1, NodePort(0)));
-              g.add_edge(MultiDiOutput{
-                  sink1, NodePort(0), MultiDiInput(sink0, NodePort(0))});
+                         MultiDiInput{source1, NodePort(0)});
+              g.add_edge(MultiDiOutput{sink1, NodePort(0)},
+                         MultiDiInput{sink0, NodePort(0)});
               for (int j = 0; j < nodes.size(); ++j) {
                 if (source[j] == source1) {
                   source[j] = source0;
