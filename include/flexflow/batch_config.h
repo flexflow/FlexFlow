2ba481b2aec29dcc9fa34eb140cb41a3651443c2
xinhaoc <99570243+xinhaoc@users.noreply.github.com>
2023-07-21 10:53:53 -0400

    Inference: fix batch_size issue. (#863)
    Co-authored-by: Gabriele Oliaro <goliaro@cs.cmu.edu>

--- include/flexflow/batch_config.h
+++ include/flexflow/batch_config.h
@@ -44,7 +44,7 @@ public:
   int num_active_tokens() const;
   void print() const;
   virtual InferenceMode get_mode() const;
-  static int const MAX_NUM_REQUESTS = 1;
+  static int const MAX_NUM_REQUESTS = 16;
   static int const MAX_NUM_TOKENS = 64;
   static int const MAX_SEQ_LENGTH = 256;
 
