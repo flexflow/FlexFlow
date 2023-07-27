ba9e04fa652794bcba1dcf55c157655cc4b280ff
lambda7xx <lambda7xx@gmail.com>
2023-07-27 06:50:57 +0000

    remove the filter_keys for bidict

--- lib/utils/include/utils/containers.h
+++ lib/utils/include/utils/containers.h
@@ -168,17 +168,6 @@ std::unordered_map<K, V> filter_keys(std::unordered_map<K, V> const &m,
 }
 
 template <typename K, typename V, typename F>
-std::unordered_map<K, V> filter_keys(bidict<K, V> const &m, F const &f) {
-  std::unordered_map<K, V> result;
-  for (auto const &kv : m) {
-    if (f(kv.first)) {
-      result.insert(kv);
-    }
-  }
-  return result;
-}
-
-template <typename K, typename V, typename F>
 std::unordered_map<K, V> filter_values(bidict<K, V> const &m, F const &f) {
   std::unordered_map<K, V> result;
   for (auto const &kv : m) {
