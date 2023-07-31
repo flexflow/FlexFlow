8c9b0eaa6c7b6a1196686a6be2b607b5fd601e9a
goliaro <goliaro@cs.cmu.edu>
2023-07-31 21:33:32 +0000

    updated arguments parsing

--- inference/flexflow_inference.py
+++ inference/flexflow_inference.py
@@ -12,62 +12,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# temporary workaround to avoid having to pass arguments
-import sys, argparse, json
+import flexflow.serve as ff
+import argparse, json
 
-sys.argv += [
-    "-ll:gpu",
-    "4",
-    "-ll:fsize",
-    "14000",
-    "-ll:zsize",
-    "30000",
-    # "-tokenizer",
-    # "../inference/tokenizer/tokenizer.model",
-    # "-output-file",
-    # "../inference/output/llama_python_inference.txt",
-    # "-clean-model-cache",
-    "-pipeline-parallelism-degree",
-    "4",
-    "-model-name",
-    "decapoda-research/llama-7b-hf",
-    "-prompt",
-    # "../inference/prompt/chatgpt.json",
-    "../inference/prompt/test.json",
-]
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-model-name",
-    help="the name of the HuggingFace model to use",
-    type=str,
-    required=True,
-)
-parser.add_argument(
-    "-llm-weight", help="path to the weights for the LLM", type=str, default=""
-)
-parser.add_argument(
-    "-tokenizer", help="path to the tokenizer file or folder", type=str, default=""
-)
-parser.add_argument("-prompt", help="path to the prompt file", type=str, required=True)
-parser.add_argument(
-    "-output-file", help="path to the output file", type=str, default=""
-)
-parser.add_argument(
-    "-clean-model-cache",
-    help="whether to discard previous weights/tokenizer cache for this model",
-    action="store_true",
-)
-args, unknown = parser.parse_known_args()
-
-from flexflow.serve import LLM, SamplingConfig
-from flexflow.core import *
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-model-name",
+        help="the name of the HuggingFace model to use",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-llm-weight", help="path to the weights for the LLM", type=str, default=""
+    )
+    parser.add_argument(
+        "-tokenizer", help="path to the tokenizer file or folder", type=str, default=""
+    )
+    parser.add_argument(
+        "-prompt", help="path to the prompt file", type=str, required=True
+    )
+    parser.add_argument(
+        "-output-file", help="path to the output file", type=str, default=""
+    )
+    parser.add_argument(
+        "-clean-model-cache",
+        help="whether to discard previous weights/tokenizer cache for this model",
+        action="store_true",
+    )
+    return parser.parse_args()
 
 
 def top_level_task():
+    args = parse_args()
+
+    ff.init(
+        {
+            "num_gpus": 4,
+            "memory_per_gpu": 14000,
+            "zero_copy_memory_per_gpu": 30000,
+            "pipeline_parallelism_degree": 4,
+        }
+    )
+
     # Incremental decoding
+    llama = ff.LLM(
+        args.model_name,
+        data_type=ff.DataType.DT_FLOAT,
+        tokenizer_path=args.tokenizer,
+        weights_path=args.llm_weight,
+        clean_cache=args.clean_model_cache,
+        output_file=args.output_file,
+    )
+    sampling_config = ff.SamplingConfig(
+        do_sample=False, temperature=0.9, topp=0.8, topk=1
+    )
+    llama.compile(
+        ff.InferenceMode.INC_DECODING_MODE,
+        sampling_config,
+        max_batch_size=1,
+        max_seq_length=256,
+        max_tokens_per_batch=64,
+        tensor_parallel_degree=1,
+        pipeline_parallel_degree=1,
+    )
+    prompts = [s for s in json.load(open(args.prompt))]
+    results = llama.generate(prompts)
+
+    # # Speculative inference
     # llama = LLM(
-    #     args.model_name,
+    #     "decapoda-research/llama-7b-hf",
+    #     data_type=DataType.DT_FLOAT,
+    #     tokenizer_path=args.tokenizer,
+    #     weights_path=args.llm_weight,
+    #     clean_cache=args.clean_model_cache,
+    #     output_file=args.output_file,
+    # )
+    # ssm1 = LLM(
+    #     "Jackfram/llama-160m",
+    #     data_type=DataType.DT_FLOAT,
+    #     tokenizer_path=args.tokenizer,
+    #     weights_path=args.llm_weight,
+    #     clean_cache=args.clean_model_cache,
+    #     output_file=args.output_file,
+    # )
+    # ssm2 = LLM(
+    #     "facebook/opt-125m",
     #     data_type=DataType.DT_FLOAT,
     #     tokenizer_path=args.tokenizer,
     #     weights_path=args.llm_weight,
@@ -75,8 +106,8 @@ def top_level_task():
     #     output_file=args.output_file,
     # )
     # sampling_config = SamplingConfig(do_sample=False, temperature=0.9, topp=0.8, topk=1)
-    # llama.compile(
-    #     InferenceMode.INC_DECODING_MODE,
+    # ssm1.compile(
+    #     InferenceMode.BEAM_SEARCH_MODE,
     #     sampling_config,
     #     max_batch_size=1,
     #     max_seq_length=256,
@@ -84,66 +115,28 @@ def top_level_task():
     #     tensor_parallel_degree=1,
     #     pipeline_parallel_degree=1,
     # )
+    # ssm2.compile(
+    #     InferenceMode.BEAM_SEARCH_MODE,
+    #     sampling_config,
+    #     max_batch_size=1,
+    #     max_seq_length=256,
+    #     max_tokens_per_batch=64,
+    #     tensor_parallel_degree=1,
+    #     pipeline_parallel_degree=1,
+    # )
+    # llama.compile(
+    #     InferenceMode.TREE_VERIFY_MODE,
+    #     sampling_config,
+    #     max_batch_size=1,
+    #     max_seq_length=256,
+    #     max_tokens_per_batch=64,
+    #     tensor_parallel_degree=1,
+    #     pipeline_parallel_degree=1,
+    #     ssms=[ssm1, ssm2],
+    # )
+
     # prompts = [s for s in json.load(open(args.prompt))]
     # results = llama.generate(prompts)
-
-    # Speculative inference
-    llama = LLM(
-        "decapoda-research/llama-7b-hf",
-        data_type=DataType.DT_FLOAT,
-        tokenizer_path=args.tokenizer,
-        weights_path=args.llm_weight,
-        clean_cache=args.clean_model_cache,
-        output_file=args.output_file,
-    )
-    ssm1 = LLM(
-        "Jackfram/llama-160m",
-        data_type=DataType.DT_FLOAT,
-        tokenizer_path=args.tokenizer,
-        weights_path=args.llm_weight,
-        clean_cache=args.clean_model_cache,
-        output_file=args.output_file,
-    )
-    ssm2 = LLM(
-        "facebook/opt-125m",
-        data_type=DataType.DT_FLOAT,
-        tokenizer_path=args.tokenizer,
-        weights_path=args.llm_weight,
-        clean_cache=args.clean_model_cache,
-        output_file=args.output_file,
-    )
-    sampling_config = SamplingConfig(do_sample=False, temperature=0.9, topp=0.8, topk=1)
-    ssm1.compile(
-        InferenceMode.BEAM_SEARCH_MODE,
-        sampling_config,
-        max_batch_size=1,
-        max_seq_length=256,
-        max_tokens_per_batch=64,
-        tensor_parallel_degree=1,
-        pipeline_parallel_degree=1,
-    )
-    ssm2.compile(
-        InferenceMode.BEAM_SEARCH_MODE,
-        sampling_config,
-        max_batch_size=1,
-        max_seq_length=256,
-        max_tokens_per_batch=64,
-        tensor_parallel_degree=1,
-        pipeline_parallel_degree=1,
-    )
-    llama.compile(
-        InferenceMode.TREE_VERIFY_MODE,
-        sampling_config,
-        max_batch_size=1,
-        max_seq_length=256,
-        max_tokens_per_batch=64,
-        tensor_parallel_degree=1,
-        pipeline_parallel_degree=1,
-        ssms=[ssm1, ssm2],
-    )
-
-    prompts = [s for s in json.load(open(args.prompt))]
-    results = llama.generate(prompts)
 
 
 if __name__ == "__main__":
--- python/flexflow/serve/__init__.py
+++ python/flexflow/serve/__init__.py
@@ -12,4 +12,100 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .serve import LLM, SamplingConfig
+import json, sys
+from ..type import *
+
+
+def parse_positive_int_config(name, variable, ff_cli_name=None):
+    if variable is not None:
+        if type(variable) is not int:
+            raise ValueError(
+                f"The following configs take positive integers only: {name}"
+            )
+        elif variable <= 0:
+            raise ValueError(
+                f"The following configs take positive integers only: {name}"
+            )
+        if not ff_cli_name:
+            sys.argv += ["-{name}", str(variable)]
+        else:
+            sys.argv += [f"{ff_cli_name}", str(variable)]
+
+
+def init(configs):
+    configs_dict = {}
+    if type(configs) == str:
+        try:
+            with open(configs) as f:
+                configs_dict = json.load(f)
+        except json.JSONDecodeError as e:
+            print("JSON format error:")
+            print(e)
+    elif type(configs) == dict:
+        configs_dict = configs
+    else:
+        raise ValueError(
+            "configs should be a dictionary or the path to a valid JSON file"
+        )
+
+    # configs should contain the following mandatory keys with non-zero integer values:
+    num_gpus = configs_dict.get("num_gpus")
+    memory_per_gpu = configs_dict.get("memory_per_gpu")
+    zero_copy_memory_per_gpu = configs_dict.get("zero_copy_memory_per_gpu")
+    if not num_gpus or not memory_per_gpu or not zero_copy_memory_per_gpu:
+        raise ValueError(
+            "Missing one of the following configs: num_gpus, memory_per_gpu, zero_copy_memory_per_gpu"
+        )
+    parse_positive_int_config("num_gpus", num_gpus, "-ll:gpu")
+    parse_positive_int_config("memory_per_gpu", memory_per_gpu, "-ll:fsize")
+    parse_positive_int_config(
+        "zero_copy_memory_per_gpu", zero_copy_memory_per_gpu, "-ll:zsize"
+    )
+
+    # parse optional arguments
+    num_cpus = configs_dict.get("num_cpus")
+    parse_positive_int_config("num_cpus", num_cpus, "-ll:cpu")
+
+    data_parallelism_degree = configs_dict.get("data_parallelism_degree")
+    tensor_parallelism_degree = configs_dict.get("tensor_parallelism_degree")
+    pipeline_parallelism_degree = configs_dict.get("pipeline_parallelism_degree")
+    parse_positive_int_config(
+        "data_parallelism_degree", data_parallelism_degree, "-data-parallelism-degree"
+    )
+    parse_positive_int_config(
+        "tensor_parallelism_degree",
+        tensor_parallelism_degree,
+        "-tensor-parallelism-degree",
+    )
+    parse_positive_int_config(
+        "pipeline_parallelism_degree",
+        pipeline_parallelism_degree,
+        "-pipeline-parallelism-degree",
+    )
+
+    offload = configs_dict.get("offload", False)
+    if offload:
+        sys.argv += ["-offload"]
+    offload_reserve_space_size = configs_dict.get("offload_reserve_space_size")
+    parse_positive_int_config(
+        "offload_reserve_space_size",
+        offload_reserve_space_size,
+        "-offload-reserve-space-size",
+    )
+    use_4bit_quantization = configs_dict.get("use_4bit_quantization", False)
+    if use_4bit_quantization:
+        sys.argv += ["--4bit-quantization"]
+    use_8bit_quantization = configs_dict.get("use_8bit_quantization", False)
+    if use_8bit_quantization:
+        sys.argv += ["--8bit-quantization"]
+
+    profiling = configs_dict.get("profiling", False)
+    if profiling:
+        sys.argv += ["--profiling"]
+    fusion = configs_dict.get("fusion", True)
+    if fusion:
+        sys.argv += ["--fusion"]
+
+    global LLM
+    global SamplingConfig
+    from .serve import LLM, SamplingConfig
