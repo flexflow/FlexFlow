452b117959d221d1690861f549080f1a4e1c0a65
goliaro <goliaro@cs.cmu.edu>
2023-08-02 03:18:23 +0000

    update

--- python/flexflow/serve/__init__.py
+++ python/flexflow/serve/__init__.py
@@ -17,7 +17,7 @@ from typing import Union
 from ..type import *
 
 
-def parse_positive_int_config(name: str, variable: str, ff_cli_name: str = None):
+def _parse_positive_int_config(name: str, variable: str, ff_cli_name: str = None):
     if variable is not None:
         if type(variable) is not int:
             raise ValueError(
@@ -35,6 +35,8 @@ def parse_positive_int_config(name: str, variable: str, ff_cli_name: str = None)
 
 def init(configs: Union[str, dict]):
     """Configure FlexFlow for inference and start the FlexFlow runtime by importing the flexflow.core package.
+
+    The configurations are passed down to the FlexFlow runtime (implemented in C++) via command line arguments.
 
     The init function takes three mandatory parameters, which cannot be changed after starting the runtime. These are:
     - num_gpus: the number of GPUs to reserve for the runtime
@@ -82,32 +84,32 @@ def init(configs: Union[str, dict]):
         raise ValueError(
             "Missing one of the following configs: num_gpus, memory_per_gpu, zero_copy_memory_per_gpu"
         )
-    parse_positive_int_config("num_gpus", num_gpus, "-ll:gpu")
-    parse_positive_int_config("memory_per_gpu", memory_per_gpu, "-ll:fsize")
-    parse_positive_int_config(
+    _parse_positive_int_config("num_gpus", num_gpus, "-ll:gpu")
+    _parse_positive_int_config("memory_per_gpu", memory_per_gpu, "-ll:fsize")
+    _parse_positive_int_config(
         "zero_copy_memory_per_gpu", zero_copy_memory_per_gpu, "-ll:zsize"
     )
 
     # parse optional arguments
     num_cpus = configs_dict.get("num_cpus")
-    parse_positive_int_config("num_cpus", num_cpus, "-ll:cpu")
+    _parse_positive_int_config("num_cpus", num_cpus, "-ll:cpu")
     legion_utility_processors = configs_dict.get("legion_utility_processors")
-    parse_positive_int_config(
+    _parse_positive_int_config(
         "legion_utility_processors", legion_utility_processors, "-ll:util"
     )
 
     data_parallelism_degree = configs_dict.get("data_parallelism_degree")
     tensor_parallelism_degree = configs_dict.get("tensor_parallelism_degree")
     pipeline_parallelism_degree = configs_dict.get("pipeline_parallelism_degree")
-    parse_positive_int_config(
+    _parse_positive_int_config(
         "data_parallelism_degree", data_parallelism_degree, "-data-parallelism-degree"
     )
-    parse_positive_int_config(
+    _parse_positive_int_config(
         "tensor_parallelism_degree",
         tensor_parallelism_degree,
         "-tensor-parallelism-degree",
     )
-    parse_positive_int_config(
+    _parse_positive_int_config(
         "pipeline_parallelism_degree",
         pipeline_parallelism_degree,
         "-pipeline-parallelism-degree",
@@ -117,7 +119,7 @@ def init(configs: Union[str, dict]):
     if offload:
         sys.argv += ["-offload"]
     offload_reserve_space_size = configs_dict.get("offload_reserve_space_size")
-    parse_positive_int_config(
+    _parse_positive_int_config(
         "offload_reserve_space_size",
         offload_reserve_space_size,
         "-offload-reserve-space-size",
