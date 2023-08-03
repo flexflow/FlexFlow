f106c9a2af394b63570abd586688c8c01e6d90bd
lambda7xx <lambda7xx@gmail.com>
2023-08-03 11:16:14 +0000

    implement some logic LocalTaskArgumentAccessor::get_tensor

--- lib/runtime/src/task_spec/task_argument_accessor.cc
+++ lib/runtime/src/task_spec/task_argument_accessor.cc
@@ -14,6 +14,25 @@ template <typename T> T const & LocalTaskArgumentAccessor::get_argument(slot_id 
 
 template <Permissions PRIV> privilege_mode_to_accessor<PRIV> LocalTaskArgumentAccessor::get_tensor(slot_id slot) const {
     SimTensorSpec const & spec = this->tensor_shape_bindings.at(slot);
+    if(slot == GATE_PREDS) {
+        InputParallelTensorDesc  gate_preds = get<InputParallelTensorDesc>(this->sim_task_binding->tensor_shape_bindings.at(slot));
+        //TODO, I should convert gate_pred to privilege_mode_to_accessor<PRIV>
+        NOT_IMPLEMENTED();
+    } else if(slot == GATE_ASSIGN) {
+        InputVariadicParallelTensorDesc  gate_assign = get<InputVariadicParallelTensorDesc>(this->sim_task_binding->tensor_shape_bindings.at(slot));
+        //TODO, I should convert gate_assign to privilege_mode_to_accessor<PRIV>
+        NOT_IMPLEMENTED();
+    } else if(slot == EXP_PREDS) {
+        InputVariadicParallelTensorDesc exp_preds = get<InputVariadicParallelTensorDesc>(this->sim_task_binding->tensor_shape_bindings.at(slot));
+        //TODO, I should convert exp_preds to privilege_mode_to_accessor<PRIV>
+        NOT_IMPLEMENTED();
+    } else if(slot == OUTPUT) {
+        ParallelTensorShape  output_shape = get<ParallelTensorShape>(this->sim_task_binding->tensor_shape_bindings.at(slot));
+        //TODO, I should convert output_shape to privilege_mode_to_accessor<PRIV>
+        NOT_IMPLEMENTED();
+    } else {
+        throw std::runtime_error("Unknown Slot ID in LocalTaskArgumentAccessor::get_tensor");
+    }
 //     NOT_IMPLEMENTED();//TODO, I should convert spec to privilege_mode_to_accessor<PRIV>
 //lib/runtime/src/accessor.h
 //     template <>
