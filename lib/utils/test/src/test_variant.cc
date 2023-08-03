ea9eec541d21d1c90dfe1e0facf33837008fd86b
lambda7xx <lambda7xx@gmail.com>
2023-08-03 03:57:28 +0000

    add comments for test_variant

--- lib/utils/test/src/test_variant.cc
+++ lib/utils/test/src/test_variant.cc
@@ -9,11 +9,18 @@ TEST_CASE("widen and narrow functions") {
     CHECK(result == expected);
   }
 
-  SUBCASE("narrow function") {
-    variant<int, float, double> v2 = 3.14;
+  SUBCASE("narrow function  fail") {
+    variant<int, float, double> v2 = 3.14;//this is a doule, because 3.14 default to double
     optional<variant<int, float>> result = narrow<variant<int, float>>(v2);
     optional<variant<int, float>> expected = float(3.14);
-    CHECK_FALSE(result.has_value()); // result should be empty due to narrowing
+    CHECK(!result.has_value()); // result should be empty due to narrowing
+  }
+
+  SUBCASE("narrow function  success") {
+    variant<int, float, double> v2 = 3.14;//this is a doule, because 3.14 default to double
+    optional<variant<int, double>> result = narrow<variant<int, double>>(v2);
+    optional<variant<int, double>> expected = 3.14;
+    CHECK(result == expected); // 
   }
 
   SUBCASE("cast function") {
@@ -22,4 +29,5 @@ TEST_CASE("widen and narrow functions") {
     optional<variant<int, double>> expected = 42;
     CHECK(result == expected);
   }
+
 }
