52b77f92d3328251f6e1d97392cb282ff4c5bbde
lambda7xx <lambda7xx@gmail.com>
2023-08-11 03:05:07 +0000

    fix the src/ops/layer.cc

--- lib/runtime/src/layer.cc
+++ lib/runtime/src/layer.cc
@@ -1,8 +1,8 @@
-#include "layer.h"
+#include "../../pcg/include/pcg/layer.h"
 
 namespace FlexFlow {
 
-Layer::Layer(CompGraphOperatorAttrs const &_attrs, std::string const &_name)
+Layer::Layer(CompGraphOperatorAttrs const &_attrs, optional<std::string> const &_name)
     : attrs(_attrs), name(_name) {}
 
 } // namespace FlexFlow
