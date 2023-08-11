6283528af178c0ac09b657618af2ab47402c921b
wmdi <wmengd@gmail.com>
2023-08-10 21:18:55 -0400

    view MultiDiGraph as labelled

--- lib/utils/include/utils/graph/labelled/views.h
+++ lib/utils/include/utils/graph/labelled/views.h
@@ -21,6 +21,41 @@ public:
       std::unordered_set<Node> const &);
 };
 
+template <typename NodeLabel, typename OutputLabel>
+struct ViewMultiDiGraphAsOutputLabelled
+    : public IOutputLabelledMultiDiGraphView<NodeLabel, OutputLabel> {
+public:
+  ViewMultiDiGraphAsOutputLabelled() = delete;
+  explicit ViewMultiDiGraphAsOutputLabelled(
+      MultiDiGraphView const &g,
+      std::function<NodeLabel(Node const &)> const &node_label,
+      std::function<OutputLabel(MultiDiOutput const &)> const &output_label)
+      : g(g), node_label(node_label), output_label(output_label) {}
+
+  virtual std::unordered_set<Node> query_nodes(NodeQuery const &q) const override {
+    return g.query_nodes(q);
+  }
+
+  virtual std::unordered_set<MultiDiEdge> query_edges(MultiDiEdgeQuery const &q) const override {
+    return g.query_edges(q);
+  }
+
+  virtual NodeLabel const &at(Node const &n) const override {
+    return node_label(n);
+  }
+
+  virtual OutputLabel &at(MultiDiOutput const &o) override {
+    return output_label(o);
+  }
+
+private:
+  MultiDiGraphView g;
+  std::function<NodeLabel(Node const &)> node_label;
+  std::function<OutputLabel(MultiDiOutput const &)> output_label;
+};
+
+CHECK_NOT_ABSTRACT(ViewMultiDiGraphAsOutputLabelled<test_types::hash_cmp, test_types::hash_cmp>);
+
 } // namespace FlexFlow
 
 #endif
