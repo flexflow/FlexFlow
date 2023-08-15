3da1b1a17309d39e72a7656990c3b45578030526
lambda7xx <lambda7xx@gmail.com>
2023-08-15 03:14:40 +0000

    add test for the perf_metrics

---
+++ lib/kernels/test/src/test_perf_metrics.cc
@@ -0,0 +1,48 @@
+#include "rapidcheck.h"
+#include "kernels/perf_metrics.h"
+
+using namespace FlexFlow;
+
+// 1. Generator for PerfMetrics
+namespace rc {
+
+template<>
+struct Arbitrary<PerfMetrics> {
+    static Gen<PerfMetrics> arbitrary() {
+        return gen::build<PerfMetrics>(
+            gen::set(&PerfMetrics::train_all),
+            gen::set(&PerfMetrics::train_correct),
+            gen::set(&PerfMetrics::cce_loss),
+            gen::set(&PerfMetrics::sparse_cce_loss),
+            gen::set(&PerfMetrics::mse_loss),
+            gen::set(&PerfMetrics::rmse_loss),
+            gen::set(&PerfMetrics::mae_loss),
+            gen::set(&PerfMetrics::start_time),
+            gen::set(&PerfMetrics::current_time)
+        );
+    }
+};
+
+}  // namespace rc
+
+// 2. Properties for PerfMetrics
+
+RC_GTEST_PROP(PerfMetricsTests, "Throughput non-negative", ()) {
+    const auto m = *rc::gen::arbitrary<PerfMetrics>();
+    RC_ASSERT(get_throughput(m) >= 0);
+}
+
+RC_GTEST_PROP(PerfMetricsTests, "Accuracy between 0 and 1", ()) {
+    const auto m = *rc::gen::arbitrary<PerfMetrics>();
+    float accuracy = get_accuracy(m);
+    RC_ASSERT(accuracy >= 0.0f && accuracy <= 1.0f);
+}
+
+RC_GTEST_PROP(PerfMetricsTests, "Update maintains non-negative values", ()) {
+    const auto lhs = *rc::gen::arbitrary<PerfMetrics>();
+    const auto rhs = *rc::gen::arbitrary<PerfMetrics>();
+    const auto result = update(lhs, rhs);
+    RC_ASSERT(result.train_all >= 0);
+    // Add other assertions for other fields...
+}
+
