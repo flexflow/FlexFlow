9bedf1a20d87a0aaa869f8b981b1cd9719994363
lambda7xx <lambda7xx@gmail.com>
2023-08-15 03:09:52 +0000

    use rapidcheck for datatype_dispatch

--- lib/kernels/test/src/test_datatype_dispatch.cc
+++ lib/kernels/test/src/test_datatype_dispatch.cc
@@ -1,5 +1,5 @@
 #include "doctest.h"
-#include "kernels/datatype_dispatch.h"
+#include "rapidcheck.h"
 
 using namespace FlexFlow;
 
@@ -8,41 +8,34 @@ struct TestFn {
     int operator()(int x) const {
         if (DT == DataType::FLOAT) return x + 1;
         else if (DT == DataType::DOUBLE) return x + 2;
-        // 添加其他类型的逻辑
         return 0;
     }
 };
 
-TEST_CASE("dispatch") {
-    SUBCASE("Dispatch float") {
-        CHECK(dispatch<TestFn>(DataType::FLOAT, 5) == 6);
-    }
-
-    SUBCASE("Dispatch double") {
-        CHECK(dispatch<TestFn>(DataType::DOUBLE, 5) == 7);
-    }
+RC_GTEST_PROP(dispatch, "Dispatch float", (int x)) {
+    RC_ASSERT(dispatch<TestFn>(DataType::FLOAT, x) == x + 1);
 }
 
-TEST_CASE("DataTypeDispatch1") {
+RC_GTEST_PROP(dispatch, "Dispatch double", (int x)) {
+    RC_ASSERT(dispatch<TestFn>(DataType::DOUBLE, x) == x + 2);
+}
+
+RC_GTEST_PROP(DataTypeDispatch1, "Dispatch1 float", (int x)) {
     DataTypeDispatch1<TestFn> dispatcher;
-
-    SUBCASE("Dispatch1 float") {
-        CHECK(dispatcher(DataType::FLOAT, 5) == 6);
-    }
-
-    SUBCASE("Dispatch1 double") {
-        CHECK(dispatcher(DataType::DOUBLE, 5) == 7);
-    }
+    RC_ASSERT(dispatcher(DataType::FLOAT, x) == x + 1);
 }
 
-TEST_CASE("DataTypeDispatch2") {
+RC_GTEST_PROP(DataTypeDispatch1, "Dispatch1 double", (int x)) {
+    DataTypeDispatch1<TestFn> dispatcher;
+    RC_ASSERT(dispatcher(DataType::DOUBLE, x) == x + 2);
+}
+
+RC_GTEST_PROP(DataTypeDispatch2, "Dispatch2 float", (int x)) {
     DataTypeDispatch2<TestFn> dispatcher;
+    RC_ASSERT(dispatcher(DataType::FLOAT, x) == x + 1);
+}
 
-    SUBCASE("Dispatch2 float") {
-        CHECK(dispatcher(DataType::FLOAT, 5) == 6);
-    }
-
-    SUBCASE("Dispatch2 double") {
-        CHECK(dispatcher(DataType::DOUBLE, 5) == 7);
-    }
+RC_GTEST_PROP(DataTypeDispatch2, "Dispatch2 double", (int x)) {
+    DataTypeDispatch2<TestFn> dispatcher;
+    RC_ASSERT(dispatcher(DataType::DOUBLE, x) == x + 2);
 }
