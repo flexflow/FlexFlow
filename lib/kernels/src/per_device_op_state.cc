bd668c88fa7418c744ed7a4f6191f3e14822f0dc
Kate Unger <32380357+KateUnger@users.noreply.github.com>
2023-08-21 11:23:33 -0700

    Remove outdated runtime files (#1006)
    * fix build errors
    
    * delete PerDeviceOpState

--- lib/kernels/src/per_device_op_state.cc
+++
@@ -1,9 +0,0 @@
-/* #include "kernels/per_device_op_state.h" */
-
-/* namespace FlexFlow { */
-
-/* PerDeviceOpState::PerDeviceOpState(PerDeviceFFHandle const &_handle) */
-/*   : handle(_handle) */
-/* { } */
-
-/* } */
