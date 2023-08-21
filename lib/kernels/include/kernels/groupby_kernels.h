7d8bac74ca8cd9763cd17b1e691c27a6e45bb9cb
Kate Unger <kateunger@ucsb.edu>
2023-08-21 11:44:03 -0700

    fix format

--- lib/kernels/include/kernels/groupby_kernels.h
+++ lib/kernels/include/kernels/groupby_kernels.h
@@ -1,7 +1,6 @@
 #ifndef _FLEXFLOW_KERNELS_INCLUDE_KERNELS_GROUPBY_H
 #define _FLEXFLOW_KERNELS_INCLUDE_KERNELS_GROUPBY_H
 
-
 namespace FlexFlow {
 
 class GroupByPerDeviceState : public PerDeviceOpState {
