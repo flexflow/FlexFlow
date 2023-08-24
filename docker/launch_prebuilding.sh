befa1e2d8fcaac689efd6eb9457df29cffcda372
DerrickYLJ <lijiey@andrew.cmu.edu>
2023-08-23 21:45:14 -0400

    non-running container

--- docker/launch_prebuilding.sh
+++ docker/launch_prebuilding.sh
@@ -12,18 +12,15 @@ export BUILD_LEGION_ONLY=ON
 # Build Docker Flexflow Container
 echo "building docker"
 ./docker/build.sh flexflow
-export detached=ON
-./docker/run.sh flexflow
+docker create --name prelegion flexflow-"${gpu_backend}"-"${cuda_version}":"${python_version}"
 
 # Copy legion libraries to host
 echo "copy legion libaries"
-echo "flexflow-${gpu_backend}-${cuda_version}:${python_version}"
-container_id=$(docker ps | grep flexflow-cuda-11.8:latest | awk '{print $1}')
-echo "$container_id"
-docker cp "$container_id":/usr/FlexFlow/build/deps ~/buildlegion
+docker cp prelegion:/usr/FlexFlow/build/deps ~/FlexFlow/prebuild_legion
+
 
 # Create the tarball file
-cd ~/buildlegion
+cd ~/FlexFlow/prebuild_legion
 export LEGION_TARBALL="legion_ubuntu-20.04_${gpu_backend}.tar.gz"
 echo "Creating archive $LEGION_TARBALL"
 touch "$LEGION_TARBALL"
@@ -31,6 +28,5 @@ tar --exclude="$LEGION_TARBALL" -zcvf "$LEGION_TARBALL" .
 echo "Checking the size of the Legion tarball..."
 du -h "$LEGION_TARBALL"
 
-# Stop and remove the container
-docker stop "$container_id"
-docker rm "$container_id"
+# Remove the container
+docker rm prelegion
