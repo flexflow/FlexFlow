b5b70b4ae1886b32da246c2b982fa9c05f390885
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-08-27 01:09:08 +0000

    multiple fixes

--- docker/launch_prebuilding.sh
+++ docker/launch_prebuilding.sh
@@ -6,27 +6,40 @@ python_version=${1:-latest}
 cuda_version=${2:-11.8}
 gpu_backend=${3:-cuda}
 
+# Cd into directory holding this script
+cd "${BASH_SOURCE[0]%/*}"
+
 export FF_CUDA_ARCH=all
 export BUILD_LEGION_ONLY=ON
+export INSTALL_DIR="/usr/legion"
 
 # Build Docker Flexflow Container
 echo "building docker"
-./docker/build.sh flexflow
+./build.sh flexflow
+
+# Cleanup any existing container with the same name
+docker rm prelegion
+
+# Create container to be able to copy data from the image
 docker create --name prelegion flexflow-"${gpu_backend}"-"${cuda_version}":"${python_version}"
 
 # Copy legion libraries to host
-echo "copy legion libaries"
-docker cp prelegion:/usr/FlexFlow/build/deps ~/FlexFlow/prebuild_legion
+echo "extract legion library assets"
+mkdir -p ../prebuild_legion
+rm -rf ../prebuild_legion/tmp || true
+docker cp prelegion:$INSTALL_DIR ../prebuild_legion/tmp
 
 
 # Create the tarball file
-cd ~/FlexFlow/prebuild_legion
+cd ../prebuild_legion/tmp
 export LEGION_TARBALL="legion_ubuntu-20.04_${gpu_backend}.tar.gz"
 echo "Creating archive $LEGION_TARBALL"
-touch "$LEGION_TARBALL"
-tar --exclude="$LEGION_TARBALL" -zcvf "$LEGION_TARBALL" .
+tar -zcvf "../$LEGION_TARBALL" ./
+cd ..
 echo "Checking the size of the Legion tarball..."
 du -h "$LEGION_TARBALL"
 
-# Remove the container
+
+# Cleanup
+rm -rf tmp/*
 docker rm prelegion
