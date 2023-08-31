75cc7bf281a9d7f47304c2f0c3c93aba9f166446
xinhaoc <chengxh_98@163.com>
2023-08-31 00:33:47 -0400

    fix fuse

--- src/ops/fused.cpp
+++ src/ops/fused.cpp
@@ -225,13 +225,16 @@ __host__ void FusedOp::forward_task(Task const *task,
                out_dim * batch_size);
         assert(my_input_accessor[0].domain.get_volume() == in_dim * batch_size);
         float const *bias_ptr = nullptr;
+        LinearMeta *m = (LinearMeta *)metas->meta[op];
         if (fused->op_num_weights[op] == 2) {
           assert(my_weight_accessor[1].domain.get_volume() == out_dim);
+          if (!m->add_bias_only_once || task->index_point.point_data[0] == 0) {
+            bias_ptr = my_weight_accessor[1].get_float_ptr();
+          }
           bias_ptr = my_weight_accessor[1].get_float_ptr();
         } else {
           assert(fused->op_num_weights[op] == 1);
         }
-        LinearMeta *m = (LinearMeta *)metas->meta[op];
         Kernels::Linear::forward_kernel_wrapper(
             m,
             my_input_accessor[0].get_float_ptr(),
@@ -298,8 +301,8 @@ __host__ void FusedOp::forward_task(Task const *task,
                                                        my_input_accessor[1],
                                                        my_output_accessor[0]);
         break;
-        break;
       }
+      case OP_GELU:
       case OP_RELU:
       case OP_SIGMOID:
       case OP_TANH:
@@ -337,6 +340,25 @@ __host__ void FusedOp::forward_task(Task const *task,
             my_input_accessor[0].get_float_ptr(),
             my_output_accessor[0].get_float_ptr(),
             my_input_accessor[0].domain.get_volume());
+        break;
+      }case OP_SOFTMAX: {
+        assert(fused->op_num_inputs[op] == 1);
+        assert(fused->op_num_weights[op] == 0);
+        assert(fused->op_num_outputs[op] == 1);
+        assert(my_input_accessor[0].domain.get_volume() ==
+               my_output_accessor[0].domain.get_volume());
+        SoftmaxMeta *m = (SoftmaxMeta *)metas->meta[op];
+        if (m->input_type == DT_HALF) {
+          Kernels::Softmax::forward_kernel_wrapper(
+              m,
+              my_input_accessor[0].get_half_ptr(),
+              my_output_accessor[0].get_half_ptr());
+        } else if (m->input_type == DT_FLOAT) {
+          Kernels::Softmax::forward_kernel_wrapper(
+              m,
+              my_input_accessor[0].get_float_ptr(),
+              my_output_accessor[0].get_float_ptr());
+        }
         break;
       }
       case OP_RESHAPE: {
@@ -396,6 +418,9 @@ __host__ void
   FusedOpMeta const *metas = *((FusedOpMeta **)task->local_args);
   FusedOp const *fused = metas->fused_op;
   BatchConfig const *bc = (BatchConfig *)task->args;
+  if (bc->num_tokens == 0) {
+    return;
+  }
   assert(metas->numOperators == fused->numOperators);
   assert(regions.size() == task->regions.size());
   assert((int)regions.size() ==
@@ -516,13 +541,15 @@ __host__ void
                out_dim * batch_size);
         assert(my_input_accessor[0].domain.get_volume() == in_dim * batch_size);
         void const *bias_ptr = nullptr;
+        LinearMeta *m = (LinearMeta *)metas->meta[op];
         if (fused->op_num_weights[op] == 2) {
           assert(my_weight_accessor[1].domain.get_volume() == out_dim);
-          bias_ptr = my_weight_accessor[1].ptr;
+          if (!m->add_bias_only_once || task->index_point.point_data[0] == 0) {
+            bias_ptr = my_weight_accessor[1].ptr;
+          }
         } else {
           assert(fused->op_num_weights[op] == 1);
         }
-        LinearMeta *m = (LinearMeta *)metas->meta[op];
         assert(m->input_type[0] == my_input_accessor[0].data_type);
         assert(m->input_type[0] == my_output_accessor[0].data_type);
         batch_size = bc->num_active_tokens();
@@ -591,7 +618,6 @@ __host__ void
                                                        my_input_accessor[1],
                                                        my_output_accessor[0]);
         break;
-        break;
       }
       case OP_EMBEDDING: {
         assert(fused->op_num_inputs[op] == 1);
@@ -658,20 +684,32 @@ __host__ void
                                                    effective_batch_size);
         break;
       }
+      case OP_GELU:
       case OP_RELU:
       case OP_SIGMOID:
       case OP_TANH:
-      case OP_ELU: {
+      case OP_ELU:
+      case OP_SCALAR_TRUE_DIV: {
         assert(fused->op_num_inputs[op] == 1);
         assert(fused->op_num_weights[op] == 0);
         assert(fused->op_num_outputs[op] == 1);
         assert(my_input_accessor[0].domain == my_output_accessor[0].domain);
         ElementUnaryMeta *m = (ElementUnaryMeta *)metas->meta[op];
-        ElementUnary::forward_kernel_wrapper(
-            m,
-            my_input_accessor[0].get_float_ptr(),
-            my_output_accessor[0].get_float_ptr(),
-            my_input_accessor[0].domain.get_volume());
+        if (m->data_type == DT_HALF) {
+          ElementUnary::forward_kernel_wrapper(
+              m,
+              my_input_accessor[0].get_half_ptr(),
+              my_output_accessor[0].get_half_ptr(),
+              my_input_accessor[0].domain.get_volume());
+        } else if (m->data_type == DT_FLOAT) {
+          ElementUnary::forward_kernel_wrapper(
+              m,
+              my_input_accessor[0].get_float_ptr(),
+              my_output_accessor[0].get_float_ptr(),
+              my_input_accessor[0].domain.get_volume());
+        } else {
+          assert(false && "Unsupported data type in ElementUnary forward");
+        }
         break;
       }
       case OP_RMS_NORM: {
@@ -711,8 +749,8 @@ __host__ void
         assert(fused->op_num_outputs[op] == 1);
         TreeIncMultiHeadSelfAttentionMeta *m =
             (TreeIncMultiHeadSelfAttentionMeta *)metas->meta[op];
-        TreeVerifyBatchConfig const *tree_bc =
-            (TreeVerifyBatchConfig *)task->args;
+        TreeVerifyBatchConfig const &tree_bc =
+            Future(task->futures[0]).get_result<TreeVerifyBatchConfig>();
         assert(fused->op_num_weights[op] == (1 + (int)(*m->bias)));
         GenericTensorAccessorR biases;
         if (*m->bias) {
@@ -721,7 +759,7 @@ __host__ void
         }
         TreeIncMultiHeadSelfAttention::inference_kernel_wrapper(
             m,
-            tree_bc,
+            &tree_bc,
             task->index_point.point_data[0],
             my_input_accessor[0],
             my_weight_accessor[0],
@@ -734,8 +772,10 @@ __host__ void
         assert(fused->op_num_outputs[op] == 1);
         SpecIncMultiHeadSelfAttentionMeta const *m =
             (SpecIncMultiHeadSelfAttentionMeta *)metas->meta[op];
-        BeamSearchBatchConfig const *beam_bc =
-            (BeamSearchBatchConfig *)task->args;
+        // BeamSearchBatchConfig const *beam_bc =
+        //     (BeamSearchBatchConfig *)task->args;
+        BeamSearchBatchConfig const &beam_bc =
+            Future(task->futures[0]).get_result<BeamSearchBatchConfig>();
         assert(fused->op_num_weights[op] == (1 + (int)(*m->bias)));
         GenericTensorAccessorR biases;
         if (*m->bias) {
@@ -744,7 +784,7 @@ __host__ void
         }
         SpecIncMultiHeadSelfAttention::inference_kernel_wrapper(
             m,
-            beam_bc,
+            &beam_bc,
             task->index_point.point_data[0],
             my_input_accessor[0],
             my_weight_accessor[0],
@@ -766,12 +806,32 @@ __host__ void
             m, my_input_accessor[0], my_output_accessor[0], gamma, beta);
         break;
       }
+      case OP_SOFTMAX: {
+        assert(fused->op_num_inputs[op] == 1);
+        assert(fused->op_num_weights[op] == 0);
+        assert(fused->op_num_outputs[op] == 1);
+        assert(my_input_accessor[0].domain.get_volume() ==
+               my_output_accessor[0].domain.get_volume());
+        SoftmaxMeta *m = (SoftmaxMeta *)metas->meta[op];
+        if (m->input_type == DT_HALF) {
+          Kernels::Softmax::forward_kernel_wrapper(
+              m,
+              my_input_accessor[0].get_half_ptr(),
+              my_output_accessor[0].get_half_ptr());
+        } else if (m->input_type == DT_FLOAT) {
+          Kernels::Softmax::forward_kernel_wrapper(
+              m,
+              my_input_accessor[0].get_float_ptr(),
+              my_output_accessor[0].get_float_ptr());
+        }
+        break;
+      }
       case OP_ALLREDUCE: {
         assert(fused->op_num_inputs[op] == 1);
         assert(fused->op_num_outputs[op] == 1);
         AllReduceMeta const *m = (AllReduceMeta *)metas->meta[op];
-        Kernels::AllReduce::forward_kernel_wrapper(
-            m, my_input_accessor[0], my_output_accessor[0]);
+        Kernels::AllReduce::inference_kernel_wrapper(
+            m, bc, my_input_accessor[0], my_output_accessor[0]);
         break;
       }
       default: {
@@ -1103,6 +1163,7 @@ __host__ void FusedOp::backward_task(Task const *task,
             my_input_grad_accessor[1].get_float_ptr());
         break;
       }
+      case OP_GELU:
       case OP_RELU:
       case OP_SIGMOID:
       case OP_TANH:
