d3342d5abe3b12cbd1e49e15cd5cc538b8f1d20b
Kate Unger <kateunger@ucsb.edu>
2023-09-01 10:56:18 -0700

    conv2d

--- lib/kernels/include/kernels/conv_2d_kernels.h
+++ lib/kernels/include/kernels/conv_2d_kernels.h
@@ -5,45 +5,50 @@
 
 namespace FlexFlow {
 
-class Conv2DPerDeviceState : public PerDeviceOpState {
-public:
-  Conv2DPerDeviceState(FFHandler handler);
-  ffTensorDescriptor_t inputTensor, biasTensor, outputTensor;
+struct Conv2DPerDeviceState {
+  PerDeviceFFHandle handle;
+  ffTensorDescriptor_t inputTensor;
+  ffTensorDescriptor_t biasTensor;
+  ffTensorDescriptor_t outputTensor;
   ffFilterDescriptor_t filterDesc;
   ffActivationDescriptor_t actiDesc;
   ffConvolutionDescriptor_t convDesc;
   ffConvolutionFwdAlgo_t fwdAlgo;
   ffConvolutionBwdFilterAlgo_t bwdFilterAlgo;
   ffConvolutionBwdDataAlgo_t bwdDataAlgo;
-  bool relu, use_bias;
-  char op_name[MAX_OPNAME];
+  req<optional<Activation>> activation;
+  req<bool> use_bias;
 };
+
+FF_VISITABLE_STRUCT_NO_EQ(Conv2DPerDeviceState,
+                          handle,
+                          inputTensor,
+                          biasTensor,
+                          outputTensor,
+                          filterDesc,
+                          actiDesc,
+                          convDesc,
+                          fwdAlgo,
+                          bwdFilterAlgo,
+                          bwdDataAlgo,
+                          activation,
+                          use_bias);
 
 namespace Kernels {
 namespace Conv2D {
 
-void init_kernel(Conv2DPerDeviceState *m,
-                 int input_w,
-                 int input_h,
-                 int input_c,
-                 int input_n,
-                 int output_w,
-                 int output_h,
-                 int output_c,
-                 int output_n,
-                 int kernel_h,
-                 int kernel_w,
-                 int groups,
-                 int stride_h,
-                 int stride_w,
-                 int pad_h,
-                 int pad_w,
-                 float const *input_ptr,
-                 float *output_ptr,
-                 float const *kernel_ptr,
-                 float *kernel_grad_ptr,
-                 float *forward_time = nullptr,
-                 float *backward_time = nullptr);
+Conv2DPerDeviceState init_kernel(PerDeviceFFHandle handle,
+                                ffTensorDescriptor_t inputTensor,
+                                ffTensorDescriptor_t biasTensor,
+                                ffTensorDescriptor_t outputTensor,
+                                ffFilterDescriptor_t filterDesc,
+                                ffActivationDescriptor_t actiDesc,
+                                ffConvolutionDescriptor_t convDesc,
+                                ffConvolutionFwdAlgo_t fwdAlgo,
+                                ffConvolutionBwdFilterAlgo_t bwdFilterAlgo,
+                                ffConvolutionBwdDataAlgo_t bwdDataAlgo,
+                                req<optional<Activation>> relu,
+                                bool use_bias);
 
 void forward_kernel(ffStream_t stream,
                     Conv2DPerDeviceState const *m,
@@ -58,8 +63,8 @@ void backward_kernel(ffStream_t stream,
                      float *input_grad_ptr,
                      float const *output_ptr,
                      float *output_grad_ptr,
-                     float const *kernel_ptr,
-                     float *kernel_grad_ptr,
+                     float const *filter_ptr,
+                     float *filter_grad_ptr,
                      float *bias_grad_ptr);
 
 } // namespace Conv2D
