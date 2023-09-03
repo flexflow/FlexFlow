a1bdc9f9dcaec2b6a2ea899e888a5e216b3015e0
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-09-03 11:45:09 -0400

    fix python tests

--- tests/python_interface_test.sh
+++ tests/python_interface_test.sh
@@ -8,12 +8,19 @@ check_python_interface() {
 	BATCHSIZE=$((GPUS * 64))
 	FSIZE=14048
 	ZSIZE=12192
+	ONLY_DATA_PARALLEL=true
 	interpreter=${1:-python}
 	installation_status=${2:-"before-installation"}
+	
+	# Generate configs JSON files
+	test_params=$(jq -n --arg num_gpus "$GPUS" --arg memory_per_gpu "$FSIZE" --arg zero_copy_memory_per_node "$ZSIZE" --arg batch_size "$BATCHSIZE" --arg only_data_parallel "$ONLY_DATA_PARALLEL" '{"num_gpus":$num_gpus,"memory_per_gpu":$memory_per_gpu,"zero_copy_memory_per_node":$zero_copy_memory_per_node,"batch_size":$batch_size,,"only_data_parallel":$only_data_parallel}')
+	mkdir -p /tmp/flexflow/multi_gpu_tests
+	echo "$test_params" > /tmp/flexflow/multi_gpu_tests/test_params.json
+	
 	if [[ "$interpreter" == "python" ]]; then
 		EXE="python"
 		echo "Running a single-GPU Python test to check the Python interface (native python interpreter)"
-		$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
+		$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py /tmp/flexflow/multi_gpu_tests/test_params.json
 	elif [[ "$interpreter" == "flexflow_python" ]]; then
 		if [[ "$installation_status" == "before-installation" ]]; then
 			EXE="$BUILD_FOLDER"/flexflow_python
@@ -21,7 +28,7 @@ check_python_interface() {
 			EXE="flexflow_python"
 		fi
 		echo "Running a single-GPU Python test to check the Python interface (flexflow_python interpreter)"
-		$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py -ll:py 1 -ll:gpu "$GPUS" -ll:fsize "$FSIZE" -ll:zsize "$ZSIZE" -b ${BATCHSIZE} --only-data-parallel
+		$EXE "$FF_HOME"/examples/python/keras/seq_mnist_mlp.py /tmp/flexflow/multi_gpu_tests/test_params.json
 	else
 		echo "Invalid Python interpreter"
 		exit 1
