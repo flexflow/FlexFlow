373b3144ccb6c5aa1ff24b03f26f710452e98365
Reyna Abhyankar <forvirenra@gmail.com>
2023-09-06 18:07:10 -0700

    Remove redundant code

--- lib/runtime/src/task_spec/task_invocation.cc
+++ lib/runtime/src/task_spec/task_invocation.cc
@@ -15,46 +15,5 @@ TaskSignature get_signature(task_id_t task_id) {
   return TaskSignature::task_sig_map.at(task_id);
 }
 
-template <typename F>
-void register_task(task_id_t task_id,
-                   std::string const &name,
-                   F const &func,
-                   optional<F const &> cpu_func = nullopt) {
-  // gpu task
-  {
-    TaskVariantRegistrar registrar(task_id, name);
-    registrar.add_constraint(ProcessorConstraint(Processor::TOC_PROC));
-    registrar.set_leaf();
-    Runtime::register_task_variant<func>(registrar);
-  }
-  // cpu task
-  {
-    if (cpu_func) {
-      TaskVariantRegistrar registrar(task_id, name);
-      registrar.add_constraint(ProcessorConstraint(Processor::LOC_PROC));
-      registrar.set_leaf();
-      Runtime::register_task_variant<cpu_func>(registrar);
-    }
-  }
-}
-
-template <typename F>
-void register_task(task_id_t task_id,
-                   std::string const &name,
-                   TaskSignature const &sig,
-                   F const &func) {
-  TaskSignature::task_sig_map.insert(task_id, sig);
-  register_task<F>(task_id, name, func);
-}
-
-template <typename F>
-void register_task(task_id_t task_id,
-                   std::string const &name,
-                   TaskSignature const &sig,
-                   F const &func,
-                   F const &cpu_func) {
-  TaskSignature::task_sig_map.insert(task_id, sig);
-  register_task<F>(task_id, name, func, cpu_func);
-}
 
 } // namespace FlexFlow
