9dbc79f964f217676d98f5042108dbea6e2c0062
Kate Unger <kateunger@ucsb.edu>
2023-09-12 12:58:27 -0700

    cast cuda

--- lib/kernels/include/kernels/cast_kernels.h
+++ lib/kernels/include/kernels/cast_kernels.h
@@ -3,35 +3,35 @@
 
 #include "kernels/accessor.h"
 #include "kernels/device.h"
+#include "kernels/ff_handle.h"
+#include "op-attrs/activation.h"
 
 namespace FlexFlow {
 
 struct CastPerDeviceState {
   PerDeviceFFHandle handle;
-  DataType input_data_type;
-  req<DataType> output_data_type;
-};
+  };
 
-FF_VISITABLE_STRUCT_NO_EQ(CastPerDeviceState,
-                          handle,
-                          input_data_type,
-                          output_data_type);
+FF_VISITABLE_STRUCT_NONSTANDARD_CONSTRUCTION(CastPerDeviceState, handle);
 
 namespace Kernels {
 namespace Cast {
 
-CastPerDeviceState
-    init_kernel(PerDeviceFFHandle const &, DataType input, DataType output);
+CastPerDeviceState init_kernel(PerDeviceFFHandle const &handle);
 
 void forward_kernel(ffStream_t stream,
-                    CastPerDeviceState const *,
+                    CastPerDeviceState const *m,
                     GenericTensorAccessorR const &input,
-                    GenericTensorAccessorW const &output);
+                    GenericTensorAccessorW const &output,
+                    DataType input_type,
+                    DataType output_type);
 
 void backward_kernel(ffStream_t stream,
-                     CastPerDeviceState const *,
+                     CastPerDeviceState const *m,
                      GenericTensorAccessorR const &input,
-                     GenericTensorAccessorW const &output);
+                     GenericTensorAccessorW const &output,
+                     DataType input_type,
+                     DataType output_type);
 
 } // namespace Cast
 } // namespace Kernels
