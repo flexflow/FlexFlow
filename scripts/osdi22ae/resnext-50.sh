8daa01ac24a2aedfc7ea32b75616a27b268706db
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-09-29 21:16:15 -0400

    remove obsolete stuff

--- scripts/osdi22ae/resnext-50.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-echo "Running ResNeXt-50 with a parallelization strategy discovered by Unity"
-"$FF_HOME"/build/examples/cpp/resnext50/resnext50 -ll:gpu 4 -ll:fsize 12000 -ll:zsize 14000 -b 16 --budget 20
-
-echo "Running ResNeXt-50 with data parallelism"
-"$FF_HOME"/build/examples/cpp/resnext50/resnext50 -ll:gpu 4 -ll:fsize 12000 -ll:zsize 14000 -b 16 --budget 20 --only-data-parallel
--- scripts/osdi22ae/xdl.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-echo "Running XDL with a parallelization strategy discovered by Unity"
-"$FF_HOME"/build/examples/cpp/XDL/xdl -ll:gpu 4 -ll:fsize 14000 -ll:zsize 14000 --budget 20
-
-echo "Running XDL with data parallelism"
-"$FF_HOME"/build/examples/cpp/XDL/xdl -ll:gpu 4 -ll:fsize 14000 -ll:zsize 14000 --budget 20 --only-data-parallel
