da958501b469a25913324aebacdc0270f54adf4d
lambda7xx <lambda7xx@gmail.com>
2023-10-02 18:46:07 +0000

    fix  the baichuan

--- python/flexflow/serve/models/baichuan.py
+++ python/flexflow/serve/models/baichuan.py
@@ -67,182 +67,182 @@ class FlexFlowBAICHUAN(FlexFlowModel):
             )
         self.build_model(max_tokens_per_batch)
 
-def build_model(self, max_tokens_per_batch):
-    ffmodel = FFModel(self.ffconfig)
-    
-    token_dims = [max_tokens_per_batch, 1]
-    input_tensor = ffmodel.create_tensor(token_dims, DataType.DT_INT32)
+    def build_model(self, max_tokens_per_batch):
+        ffmodel = FFModel(self.ffconfig)
+        
+        token_dims = [max_tokens_per_batch, 1]
+        input_tensor = ffmodel.create_tensor(token_dims, DataType.DT_INT32)
 
-    embed_init = UniformInitializer(random.randint(0, self.maxint), 0, 0)
+        embed_init = UniformInitializer(random.randint(0, self.maxint), 0, 0)
 
-    token = ffmodel.embedding(
-        input_tensor,
-        self.baichuan_config.vocab_size,
-        self.baichuan_config.hidden_size,
-        AggrMode.AGGR_MODE_NONE,
-        self.data_type,
-        None,
-        embed_init,
-        name = "token_embedding",
-    )
+        token = ffmodel.embedding(
+            input_tensor,
+            self.baichuan_config.vocab_size,
+            self.baichuan_config.hidden_size,
+            AggrMode.AGGR_MODE_NONE,
+            self.data_type,
+            None,
+            embed_init,
+            name = "token_embedding",
+        )
 
-    for i in range(self.baicuan_config.num_hidden_layers):
-        ffmodel.set_transformer_layer_id(i)
+        for i in range(self.baicuan_config.num_hidden_layers):
+            ffmodel.set_transformer_layer_id(i)
 
-        if i == 0:
-            attn_norm = ffmodel.rms_norm(
+            if i == 0:
+                attn_norm = ffmodel.rms_norm(
+                    token,
+                    self.baichuan_config.rms_norm_eps,
+                    self.baichuan_config.hidden_size,
+                    name = f"layers_{i}_attention_norm",
+                )
+
+            else:
+            token, attn_norm = ffmodel.residual_rms_norm(
                 token,
+                w2,
                 self.baichuan_config.rms_norm_eps,
                 self.baichuan_config.hidden_size,
-                name = f"layers_{i}_attention_norm",
+            name=f"layers_{i}_attention_norm",
+            )     
+
+            if self.mode == InferenceMode.BEAM_SEARCH_MODE:
+                mha = ffmodel.inc_multihead_self_attention(
+                    attn_norm,
+                    self.baichuan_config.hidden_size,
+                    self.baichuan_config.num_attention_heads,
+                    self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
+                    self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,   
+                    0.0, #dropout
+                    False, #bias 
+                    False, #add_bias_kv
+                    False, #add_zero_attn
+                    DataType.DT_NONE, #data_type
+                    None, #kernel_initializer
+                    True, #apply_rotary_pos_emb
+                    name=f"layers_{i}_attention",
+                )
+            elif self.mode == InferenceMode.TREE_VERIFY_MODE:
+                mha = ffmodel.inc_multihead_self_attention_verify(
+                    attn_norm,
+                    self.baichuan_config.hidden_size,
+                    self.baichuan_config.num_attention_heads,
+                    self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
+                    self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
+                    0.0, #dropout
+                    False, #bias
+                    False, #add_bias_kv
+                    False, #add_zero_attn
+                    DataType.DT_NONE, #data_type
+                    None, #kernel_initializer
+                    True, #apply_rotary_pos_emb
+                )
+            elif self.mode == InferenceMode.INC_DECODING_MODE:
+                mha = ffmodel.inc_multihead_self_attention(
+                    attn_norm,
+                    self.baichuan_config.hidden_size,
+                    self.baichuan_config.num_attention_heads,
+                    self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
+                    self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
+                    0.0, #dropout
+                    False, #bias
+                    False, #add_bias_kv
+                    False, #add_zero_attn
+                    DataType.DT_NONE, #data_type
+                    None, #kernel_initializer
+                    True, #apply_rotary_pos_emb
+                    name=f"layers_{i}_attention",
+                )
+            else:
+                assert False, "Invalid mode"
+            token, ff_norm = ffmodel.residual_rms_norm(
+                token,
+                mha,
+                self.baichuan_config.rms_norm_eps,
+                self.baichuan_config.hidden_size,
+                name=f"layers_{i}_residual_rms_norm",
+            )
+            
+            w1 = ffmodel.dense(
+                ff_norm,
+                self.baichuan_config.intermediate_size,
+                ActiMode.AC_MODE_NONE,
+                False,
+                name=f"layers_{i}_feed_forward_w1",
             )
 
-        else:
-           token, attn_norm = ffmodel.residual_rms_norm(
+            w3 = ffmodel.dense(
+                ff_norm,
+                self.baichuan_config.intermediate_size,
+                ActiMode.AC_MODE_NONE,
+                False ,
+                name=f"layers_{i}_feed_forward_w3",
+            )
+
+            multi = ffmodel.sigmoid_silu_multi(w1, w3)
+
+            w2 = ffmodel.dense(
+                multi,
+                self.baichuan_config.hidden_size,
+                ActiMode.AC_MODE_NONE,
+                False,
+                name=f"layers_{i}_feed_forward_w2",
+            )
+
+        token = ffmodel.rms_norm(
             token,
-            w2,
             self.baichuan_config.rms_norm_eps,
             self.baichuan_config.hidden_size,
-           name=f"layers_{i}_attention_norm",
-           )     
+            name=f"layers_{i}_output_norm",
+        )
+
+        dense = ffmodel.dense(
+            token, 
+            self.baichuan_config.vocab_size,
+            ActiMode.AC_MODE_NONE,
+            False,
+            name="output",
+        )
 
         if self.mode == InferenceMode.BEAM_SEARCH_MODE:
-            mha = ffmodel.inc_multihead_self_attention(
-                attn_norm,
-                self.baichuan_config.hidden_size,
-                self.baichuan_config.num_attention_heads,
-                self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
-                self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,   
-                0.0, #dropout
-                False, #bias 
-                False, #add_bias_kv
-                False, #add_zero_attn
-                DataType.DT_NONE, #data_type
-                None, #kernel_initializer
-                True, #apply_rotary_pos_emb
-                name=f"layers_{i}_attention",
-            )
-        elif self.mode == InferenceMode.TREE_VERIFY_MODE:
-            mha = ffmodel.inc_multihead_self_attention_verify(
-                attn_norm,
-                self.baichuan_config.hidden_size,
-                self.baichuan_config.num_attention_heads,
-                self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
-                self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
-                0.0, #dropout
-                False, #bias
-                False, #add_bias_kv
-                False, #add_zero_attn
-                DataType.DT_NONE, #data_type
-                None, #kernel_initializer
-                True, #apply_rotary_pos_emb
-            )
-        elif self.mode == InferenceMode.INC_DECODING_MODE:
-            mha = ffmodel.inc_multihead_self_attention(
-                attn_norm,
-                self.baichuan_config.hidden_size,
-                self.baichuan_config.num_attention_heads,
-                self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
-                self.baichuan_config.hidden_size // self.baichuan_config.num_attention_heads,
-                0.0, #dropout
-                False, #bias
-                False, #add_bias_kv
-                False, #add_zero_attn
-                DataType.DT_NONE, #data_type
-                None, #kernel_initializer
-                True, #apply_rotary_pos_emb
-                name=f"layers_{i}_attention",
-            )
-        else:
-            assert False, "Invalid mode"
-        token, ff_norm = ffmodel.residual_rms_norm(
-            token,
-            mha,
-            self.baichuan_config.rms_norm_eps,
-            self.baichuan_config.hidden_size,
-            name=f"layers_{i}_residual_rms_norm",
-        )
-        
-        w1 = ffmodel.dense(
-            ff_norm,
-            self.baichuan_config.intermediate_size,
-            ActiMode.AC_MODE_NONE,
-            False,
-            name=f"layers_{i}_feed_forward_w1",
-        )
-
-        w3 = ffmodel.dense(
-            ff_norm,
-            self.baichuan_config.intermediate_size,
-            ActiMode.AC_MODE_NONE,
-            False ,
-            name=f"layers_{i}_feed_forward_w3",
-        )
-
-        multi = ffmodel.sigmoid_silu_multi(w1, w3)
-
-        w2 = ffmodel.dense(
-            multi,
-            self.baichuan_config.hidden_size,
-            ActiMode.AC_MODE_NONE,
-            False,
-            name=f"layers_{i}_feed_forward_w2",
-        )
-
-    token = ffmodel.rms_norm(
-        token,
-        self.baichuan_config.rms_norm_eps,
-        self.baichuan_config.hidden_size,
-        name=f"layers_{i}_output_norm",
-    )
-
-    dense = ffmodel.dense(
-        token, 
-        self.baichuan_config.vocab_size,
-        ActiMode.AC_MODE_NONE,
-        False,
-        name="output",
-    )
-
-    if self.mode == InferenceMode.BEAM_SEARCH_MODE:
-        softmax = ffmodel.softmax(dense, -1)
-        #output = ffmodel.beam_top_k(softmax, self.llama_config.max_beam_width, False)
-        output = ffmodel.argmax(softmax, True)
-    else:
-        if self.generation_config.do_sample:
-            dense = ffmodel.scalar_true_divide(
-                dense, self.generation_config.temperature, False
-            )
             softmax = ffmodel.softmax(dense, -1)
-            output = ffmodel.sampling(softmax, self.generation_config.topp)
+            #output = ffmodel.beam_top_k(softmax, self.llama_config.max_beam_width, False)
+            output = ffmodel.argmax(softmax, True)
         else:
-            #output = ffmodel.arg_top_k(dense, 1, False)
-            output = ffmodel.argmax(dense, False)
+            if self.generation_config.do_sample:
+                dense = ffmodel.scalar_true_divide(
+                    dense, self.generation_config.temperature, False
+                )
+                softmax = ffmodel.softmax(dense, -1)
+                output = ffmodel.sampling(softmax, self.generation_config.topp)
+            else:
+                #output = ffmodel.arg_top_k(dense, 1, False)
+                output = ffmodel.argmax(dense, False)
 
-    self.ffmodel = ffmodel
+        self.ffmodel = ffmodel
 
-def convert_hf_model(model, dst_folder):
-    os.makedirs(dst_folder, exist_ok=True)
-    for name, params in model.named_parameters():
-        print("name: ", name, "params: ", params)
-        # name = (
-        #     name.replace(".", "_")
-        #     .replace("self_attn", "attention")
-        #     .replace("q_proj", "wq")
-        #     .replace("k_proj", "wk")
-        #     .replace("v_proj", "wv")
-        #     .replace("o_proj", "wo")
-        #     .replace("mlp", "feed_forward")
-        #     .replace("gate_proj", "w1")
-        #     .replace("down_proj", "w2")
-        #     .replace("up_proj", "w3")
-        #     .replace("input_layernorm", "attention_norm")
-        #     .replace("post_attention_layernorm", "ffn_norm")
-        #     .replace("embed_tokens", "tok_embeddings")
-        #     .replace("lm_head", "output")
-        #     .replace("model_", "")
-        # )
-        # params.detach().cpu().numpy().tofile(f"{dst_folder}/{name}")
+    def convert_hf_model(model, dst_folder):
+        os.makedirs(dst_folder, exist_ok=True)
+        for name, params in model.named_parameters():
+            print("name: ", name, "params: ", params)
+            # name = (
+            #     name.replace(".", "_")
+            #     .replace("self_attn", "attention")
+            #     .replace("q_proj", "wq")
+            #     .replace("k_proj", "wk")
+            #     .replace("v_proj", "wv")
+            #     .replace("o_proj", "wo")
+            #     .replace("mlp", "feed_forward")
+            #     .replace("gate_proj", "w1")
+            #     .replace("down_proj", "w2")
+            #     .replace("up_proj", "w3")
+            #     .replace("input_layernorm", "attention_norm")
+            #     .replace("post_attention_layernorm", "ffn_norm")
+            #     .replace("embed_tokens", "tok_embeddings")
+            #     .replace("lm_head", "output")
+            #     .replace("model_", "")
+            # )
+            # params.detach().cpu().numpy().tofile(f"{dst_folder}/{name}")
 
 
     
