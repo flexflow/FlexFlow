9e17b3831603a5c1148b1cb3528d08845e982aeb
lambda7xx <lambda7xx@gmail.com>
2023-10-03 15:37:39 +0000

    leave the index

--- lib/runtime/src/ops/topk.cc
+++ lib/runtime/src/ops/topk.cc
@@ -45,7 +45,7 @@ using namespace FlexFlow::Kernels::TopK;
 // (resp. vector along the last dimension). Thus,
 // values.shape = indices.shape = input.shape[:-1] + [k]
 
-enum Slots { INPUT, OUTPUT, ATTRS, PROFILING, PER_DEVICE_STATE };
+enum Slots { INPUT, OUTPUT, OUTPUT, ATTRS, PROFILING, PER_DEVICE_STATE };
 
 OpTaskInvocation init(TopKAttrs const &attrs) {
   OpTaskBinding binding;
@@ -102,34 +102,10 @@ static optional<float> forward_task_impl(TaskArgumentAccessor const &acc) {
   auto input = acc.get_tensor<Permissions::RO>(INPUT);
   auto output = acc.get_tensor<Permissions::WO>(OUTPUT);
 
-  // Note: this may have problem, we can fix later, the below code is copy from
-  // old code we use these elements to get length, batch_size, Question: hwo to
-  // get index_ptr?
-  Task *task = acc.task;
-  Context ctx = acc.ctx;
-  Runtime *runtime = acc.runtime;
-  Domain in1_domain = runtime->get_index_space_domain(
-      ctx, task->regions[0].region.get_index_space());
-  Domain out1_domain = runtime->get_index_space_domain(
-      ctx, task->regions[1].region.get_index_space());
-  Domain out2_domain = runtime->get_index_space_domain(
-      ctx, task->regions[2].region.get_index_space());
+  int length = input.shape.at(legion_dim_t(0)) +1;
+  size_t batch_size = input.shape.get_volume() / length;
 
-  int in_cols = in1_domain.hi()[0] - in1_domain.lo()[0] + 1;
-  int out1_cols = out1_domain.hi()[0] - out1_domain.lo()[0] + 1;
-  int out2_cols = out2_domain.hi()[0] - out2_domain.lo()[0] + 1;
-
-  assert(out1_domain == out2_domain);
-  for (int i = 1; i < in1_domain.get_dim(); i++) {
-    assert(in1_domain.lo()[i] == out1_domain.lo()[i]);
-    assert(in1_domain.hi()[i] == out1_domain.hi()[i]);
-  }
-
-  int *index_ptr = helperGetTensorPointerWO<int>(
-      regions[2], task->regions[2], FID_DATA, ctx, runtime);
-
-  int length = in1_domain.hi()[0] - in1_domain.lo()[0] + 1;
-  size_t batch_size = in1_domain.get_volume() / length;
+  int * index_ptr;//TODO NOT_IMPLEMENTED 
 
   return profiling(forward_kernel,
                    profiling,
@@ -161,29 +137,10 @@ static optional<float> backward_task_impl(TaskArgumentAccessor const &acc) {
   auto input_grad = acc.get_tensor_grad<Permissions::RO>(INPUT);
   auto output_grad = acc.get_tensor_grad<Permissions::WO>(OUTPUT);
 
-  // Note: this may have problem, we can fix later, the below code is copy from
-  // old code
-  Task *task = acc.task;
-  Context ctx = acc.ctx;
-  Runtime *runtime = acc.runtime;
-
-  Domain out1_domain = runtime->get_index_space_domain(
-      ctx, task->regions[0].region.get_index_space());
-  Domain out2_domain = runtime->get_index_space_domain(
-      ctx, task->regions[1].region.get_index_space());
-  Domain in_domain = runtime->get_index_space_domain(
-      ctx, task->regions[2].region.get_index_space());
-  assert(out1_domain == out2_domain);
-  for (int i = 1; i < in_domain.get_dim(); i++) {
-    assert(in_domain.lo()[i] == out1_domain.lo()[i]);
-    assert(in_domain.hi()[i] == out1_domain.hi()[i]);
-  }
-
   // Question: what's the indice_ptr, I think the value_grad_ptr is output_grad
-  int const *indices_ptr = helperGetTensorPointerRO<int>(
-      regions[1], task->regions[1], FID_DATA, ctx, runtime);
-  int length = in_domain.hi()[0] - in_domain.lo()[0] + 1;
-  size_t batch_size = in_domain.get_volume() / length;
+  int const *indices_ptr ;//TODO NOT_IMPLEMENTED
+  int length = input.shape.at(legion_dim_t(0)) +1;
+  size_t batch_size = input.shape.get_volume() / length;
 
   return profiling(backward_kernel,
                    profiling,
@@ -222,10 +179,9 @@ CostMetrics measure_operator_cost(SimEnvFactory const &sim_factory,
       init_task_impl(init_accessor);
 
   SimTaskBinding fwd_binding;
-  fwd_binding.bind_arg(PER_DEVICE_STATE,
-                       per_device_op_state<TopKPerDeviceState>());
+  fwd_binding.bind_arg(PER_DEVICE_STATE, per_device_state);
   fwd_binding.bind_arg(PROFILING, profiling_settings());
-  fwd_binding.bind(INPUT, input_tensor(0));
+  fwd_binding.bind(INPUT, input_parallel_tensor_shape(0));
   fwd_binding.bind(OUTPUT, output_tensor(0));
   fwd_binding.bind_arg(ATTRS, attrs);
 
