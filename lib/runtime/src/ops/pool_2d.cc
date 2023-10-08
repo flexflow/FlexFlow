76057c55383d93a5e608ee38c4fff5883ecf61ff
lambda7xx <lambda7xx@gmail.com>
2023-10-08 13:57:22 +0000

    implement the forward and backward

--- lib/runtime/src/ops/pool_2d.cc
+++ lib/runtime/src/ops/pool_2d.cc
@@ -2,6 +2,7 @@
 #include "kernels/pool_2d_kernels.h"
 #include "legion/legion_utilities.h"
 #include "op-attrs/ops/pool_2d.h"
+#include "utils/exception.decl.h"
 #include "utils/exceptions.h"
 #include "utils/hash-utils.h"
 #include "op-attrs/get_output_shapes.h"
@@ -635,7 +636,7 @@ size_t hash<FlexFlow::Pool2DParams>::operator()(
   return key;
 }
 
-enum Slots { INPUT, OUTPUT, ATTRS, PROFILING, PER_DEVICE, HANDLE};
+enum Slots { INPUT, OUTPUT, ATTRS, PROFILING, PER_DEVICE_STATE, HANDLE};
 
 OpTaskInvocation init(Pool2DAttrs const & attrs) {
   OpTaskBinding binding;
@@ -713,6 +714,74 @@ static DeviceSpecific<Pool2dPerDeviceState>  init_task(Task const *task,
               Runtime *runtime) {
   TaskArgumentAccessor acc(task, regions, ctx, runtime);
   return init_task_impl(acc);
+}
+
+OpTaskInvocation forward(Pool2DAttrs const & attrs) {
+    OpTaskBinding binding;
+    binding.bind(INPUT, input_tensor(0));
+    binding.bind(OUTPUT, output_tensor(0));
+
+    binding.bind_arg(PROFILING, profiling_settings());
+    binding.bind_arg(PER_DEVICE_STATE, per_device_op_state<Pool2dPerDeviceState>());
+
+    return {POOL2D_FWD_TASK_ID, binding};
+}
+
+OpTaskInvocation backward(Pool2DAttrs const &) {
+  OpTaskBinding b = infer_bwd_binding(forward(attrs).binding);
+
+  return {POOL2D_BWD_TASK_ID, b};
+}
+
+static optional<float> forward_task_impl(TaskArgumentAccessor const &acc) {
+  ProfilingSettings profiling = acc.get_argument<ProfilingSettings>(PROFILING);
+  Pool2dPerDeviceState state = acc.get_argument<Pool2dPerDeviceState>(PER_DEVICE_STATE);
+
+  auto input = acc.get_tensor<Permission::RO>(INPUT);
+  auto output = acc.get_tensor<Permission::WO>(OUTPUT);
+
+  return profile(forward_kernel,
+                  profilng,
+                  "[Pool2D] forward_time = %.2lfms\n",
+                  state,
+                  input.get_float_ptr(),
+                  output.get_float_ptr());
+}
+
+static void forward_task(Task const *task,
+                         std::vector<PhysicalRegion> const &regions,
+                         Context ctx,
+                         Runtime *runtime) {
+  TaskArgumentAccessor acc(task, regions, ctx, runtime);
+  forward_task_impl(acc);
+}
+
+static optional<float> backward_task_impl(TaskArgumentAccessor const &acc) {
+  ProfilingSettings profiling = acc.get_argument<ProfilingSettings>(PROFILING);
+  Pool2dPerDeviceState state = acc.get_argument<Pool2dPerDeviceState>(PER_DEVICE_STATE);
+
+  auto input = acc.get_tensor<Permission::RO>(INPUT);
+  auto input_grad = acc.get_tensor<Permission::RW>(INPUT);
+  auto output = acc.get_tensor<Permission::RO>(OUTPUT);
+  auto output_grad = acc.get_tensor<Permission::RO>(OUTPUT);
+
+  return profile(backward_kernel,
+                  profilng,
+                  "[Pool2D] backward_time = %.2lfms\n",
+                  state,
+                  input.get_float_ptr(),
+                  input_grad.get_float_ptr(),
+                  output.get_float_ptr(),
+                  output_grad.get_float_ptr());
+
+}
+
+static void backward_task(Task const *task,
+                          std::vector<PhysicalRegion> const &regions,
+                          Context ctx,
+                          Runtime *runtime) {
+  TaskArgumentAccessor acc(task, regions, ctx, runtime);
+  backward_task_impl(acc);
 }
 
 CostMetrics measure_operator_cost(SimEnvFactory const &sim_factory,
