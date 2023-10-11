10393725f2adfcd31a1d9288f6ec656819b799e2
lambda7xx <lambda7xx@gmail.com>
2023-10-11 14:59:42 +0000

    fix the transpose_kernels cu

--- lib/kernels/src/cuda/transpose_kernels.cu
+++ lib/kernels/src/cuda/transpose_kernels.cu
@@ -46,6 +46,23 @@ TransposePerDeviceState init_kernel(int num_dim, std::vector<int> const &perm) {
   return state;
 }
 
+__global__ void transpose_simple_kernel(std::size_t volume,
+                                        float const *in_ptr,
+                                        float *out_ptr,
+                                        const TransposeStrides info,
+                                        float const beta) {
+  CUDA_KERNEL_LOOP(o_idx, volume) {
+    std::size i_idx = 0;
+    std::size t = o_idx;
+    for (int i = info.num_dim - 1; i >= 0; i--) {
+      coord_t ratio = t / info.out_strides[i];
+      t -= ratio * info.out_strides[i];
+      i_idx += ratio * info.in_strides[info.perm[i]];
+    }
+    out_ptr[o_idx] += out_ptr[o_idx] * beta + in_ptr[i_idx];
+  }
+}
+
 void forward_kernel(cudaStream_t stream,
                     TransposePerDeviceState const &m,
                     float const *input_ptr,
@@ -54,25 +71,25 @@ void forward_kernel(cudaStream_t stream,
                     GenericTensorAccessorW const &output) {
 
   TransposeStrides info;
-  info.num_dim = out_domain.get_dim();
+  info.num_dim = input.shape.num_dims();
   assert(info.num_dim == m.num_dim);
   for (int i = 0; i < info.num_dim; i++) {
     if (i == 0) {
       info.in_strides[i] = 1;
       info.out_strides[i] = 1;
     } else {
-      int in_dim_size = (in_domain.hi()[i - 1] - in_domain.lo()[i - 1] + 1);
-      int out_dim_size = (out_domain.hi()[i - 1] - out_domain.lo()[i - 1] + 1);
+      int in_dim_size = input.shape[legion_dim_t(i)] + 1;
+      int out_dim_size = output.shape[legion_dim_t(i)] + 1;
       info.in_strides[i] = info.in_strides[i - 1] * in_dim_size;
       info.out_strides[i] = info.out_strides[i - 1] * out_dim_size;
     }
     info.perm[i] = m.perm[i];
   }
-  transpose_simple_kernel<<<GET_BLOCKS(out_domain.get_volume()),
+  transpose_simple_kernel<<<GET_BLOCKS(output.shape.get_volume()),
                             CUDA_NUM_THREADS,
                             0,
                             stream>>>(
-      out_domain.get_volume(), input_ptr, output_ptr, info, 0.0f /*beta*/);
+      output.shape.get_volume(), input.get_float_ptr(), output.get_float_ptr(), info, 0.0f /*beta*/);
 }
 
 void backward_kernel(TransposePerDeviceState const &m,
@@ -82,47 +99,28 @@ void backward_kernel(TransposePerDeviceState const &m,
                              GenericTensorAccessorR const & out_grad) {
 
   TransposeStrides info;
-  info.num_dim = in_grad_domain.get_dim();
+  info.num_dim =   in_grad.shape.num_dims();
   assert(info.num_dim == m.num_dim);
   for (int i = 0; i < info.num_dim; i++) {
     if (i == 0) {
       info.in_strides[i] = 1;
       info.out_strides[i] = 1;
     } else {
-      int in_dim_size =
-          (out_grad_domain.hi()[i - 1] - out_grad_domain.lo()[i - 1] + 1);
-      int out_dim_size =
-          (in_grad_domain.hi()[i - 1] - in_grad_domain.lo()[i - 1] + 1);
+      int in_dim_size = out_grad.shape[legion_dim_t(i)] + 1;
+      int out_dim_size = in_grad.shape[legion_dim_t(i)] + 1;
       info.in_strides[i] = info.in_strides[i - 1] * in_dim_size;
       info.out_strides[i] = info.out_strides[i - 1] * out_dim_size;
     }
     info.perm[m.perm[i]] = i;
   }
-  transpose_simple_kernel<<<GET_BLOCKS(in_grad_domain.get_volume()),
+  transpose_simple_kernel<<<GET_BLOCKS(in_grad.shape.get_volume()),
                             CUDA_NUM_THREADS,
                             0,
-                            stream>>>(in_grad_domain.get_volume(),
-                                      output_grad_ptr,
-                                      input_grad_ptr,
+                            stream>>>(in_grad.shape.get_volume(),
+                                      out_grad.get_float_ptr(),
+                                      in_grad.get_float_ptr(),
                                       info,
                                       1.0f /*beta*/);
-}
-
-__global__ void transpose_simple_kernel(coord_t volume,
-                                        float const *in_ptr,
-                                        float *out_ptr,
-                                        const TransposeStrides info,
-                                        float const beta) {
-  CUDA_KERNEL_LOOP(o_idx, volume) {
-    coord_t i_idx = 0;
-    coord_t t = o_idx;
-    for (int i = info.num_dim - 1; i >= 0; i--) {
-      coord_t ratio = t / info.out_strides[i];
-      t -= ratio * info.out_strides[i];
-      i_idx += ratio * info.in_strides[info.perm[i]];
-    }
-    out_ptr[o_idx] += out_ptr[o_idx] * beta + in_ptr[i_idx];
-  }
 }
 
 } // namespace Transpose
