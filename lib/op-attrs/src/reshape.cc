a5f1a0e3629e3c432de44d6bfaf468d517a79642
lambda7xx <lambda7xx@gmail.com>
2023-10-17 15:50:24 +0000

    add reshape

--- lib/op-attrs/src/reshape.cc
+++ lib/op-attrs/src/reshape.cc
@@ -1,34 +1,45 @@
 #include "op-attrs/ops/reshape.h"
 #include "op-attrs/ff_dim.h"
+#include "utils/exception.h"
 
 namespace FlexFlow {
 
-// pytorch: the input: [2,3,4], shape maybe [-1,6]， should we add this? and the
-// output is [4, 6]
-bool ReshapeAttrs::is_valid(ParallelTensorShape const &input) const {
-  if (!input.is_valid()) {
-    return false;
-  }
-  std::size_t input_volume = 1;
-  for (int i = 0; i < input.num_dims(); i++) {
-    input_volume *= input.at(ff_dim_t(i)).size;
-  }
-  std::size_t attrs_volume = 1;
-  for (int i = 0; i < this->shape.dims.num_dims(); i++) {
-    attrs_volume *= this->shape.at(ff_dim_t(i));
-  }
-  return (input_volume == attrs_volume);
-}
-
+//https://pytorch.org/docs/stable/generated/torch.reshape.html
 // pytorch: the input: [2,3,4], shape maybe [-1,6]， should we add this? and the
 // output is [4, 6] currently we doesn't consider the case of -1,we can support
 // this later the input:[2,3,4], attrs.shape:[4,6], the output is [4, 6]
 ParallelTensorShape get_output_shape(ReshapeAttrs const &attrs,
                                      ParallelTensorShape const &input) {
+  std::size_t input_volume = input.dims.get_volume();
+  std::size_t attrs_volume = 1;
+  for (int i = 0; i < attrs.shape.dims.num_dims(); i++) {
+    attrs_volume *= attrs.shape.at(ff_dim_t(i));
+  }
+  if(input_volume != attrs_volume) {
+    throw mk_runtime_error("ReshapeAttrs: input_volume != attrs_volume");
+  }
 
-  assert(attrs.is_valid(input) && "input is not valid");
-  ParallelTensorDims dims{attrs.shape.dims};
-  ParallelTensorShape output{dims, input.data_type};
+  ParallelTensorShape output = input;
+  output.data_type = input.data_type;
+  if(attrs.shape.dims.num_dims() == 1) {
+      //infer the shape
+      if(attrs.shape.at(ff_dim_t(0)) == -1) {
+       
+        output.at(ff_dim_t(0)).size = input_volume ;
+        output.at(ff_dim_t(0)).degree = 1;
+        output.at(ff_dim_t(0)).is_replica_dim = false;
+      } else {
+        output.at(ff_dim_t(0)).size = attrs.shape.at(ff_dim_t(0));
+        output.at(ff_dim_t(1)).size = input_volume / attrs.shape.at(ff_dim_t(0));
+        for(int i = 0; i < 2; i++) {
+          output.at(ff_dim_t(i)).degree = 1;
+          output.at(ff_dim_t(i)).is_replica_dim = false;
+        }
+      }
+  } else {
+      ParallelTensorDims dims{attrs.shape.dims};
+      output = {dims, input.data_type};
+  }
   return output;
 }
 
