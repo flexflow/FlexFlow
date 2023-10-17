f2a50e39b65a4445b21674d6fc06673b33e840f6
lambda7xx <lambda7xx@gmail.com>
2023-10-17 15:14:27 +0000

    add pool2d

--- lib/op-attrs/src/linear.cc
+++ lib/op-attrs/src/linear.cc
@@ -1,44 +1,48 @@
 #include "op-attrs/ops/linear.h"
 #include "op-attrs/ff_dim.h"
-#include "utils/exception.decl.h"
 #include "utils/exception.h"
 
 namespace FlexFlow {
 
-//https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
-//torch.nn.Linear(in_features, out_features, bias=True, device=None, dtype=None)
-// pytorch: input shape:{batch_size, input_channels}
-// pytorch linearattrs: should be {input_channels, output_channels}
-// pytorch: output shape:{batch_size, output_channels}
-// question: the Linearattrs doesn't have input_channels
+// https://pytorch.org/docs/stable/generated/torch.nn.Linear.html
+// torch.nn.Linear(in_features, out_features, bias=True, device=None,
+// dtype=None)
+//  pytorch: input shape:{batch_size, input_channels}
+//  pytorch linearattrs: should be {input_channels, output_channels}
+//  pytorch: output shape:{batch_size, output_channels}
+//  question: the Linearattrs doesn't have input_channels
 ParallelTensorShape get_output_shape(LinearAttrs const &atts,
                                      ParallelTensorShape const &input) {
-                          
+
   ParallelTensorShape out_shape = input;
-  if(input.num_dims() != 2) {
+  if (input.num_dims() != 2) {
     throw mk_runtime_error("LinearAttrs: input shape should be 2D");
   }
 
   out_shape.at(ff_dim_t(1)).size = atts.out_channels;
-  //linear shoud consider the degree
-  //case 1: input:[N, K], weight:[K, M], degree is 1
-  if(input.at(ff_dim_t(0)).degree == 1 && input.at(ff_dim_t(1)).degree == 1  ) {
+  // linear shoud consider the degree
+  // case 1: input:[N, K], weight:[K, M], degree is 1
+  if (input.at(ff_dim_t(0)).degree == 1 && input.at(ff_dim_t(1)).degree == 1) {
     out_shape.at(ff_dim_t(0)).degree = 1;
-    for(int i = 0; i < input.num_dims(); i++) {
+    for (int i = 0; i < input.num_dims(); i++) {
       out_shape.at(ff_dim_t(i)).is_replica_dim = false;
       out_shape.at(ff_dim_t(i)).degree = 1;
     }
-  } else if(input.at(ff_dim_t(0)).degree == 1 && input.at(ff_dim_t(1)).degree > 1) {
-    //case 2: input [N, k/x], weight [k/x, M], output [N, M], degree is x
+  } else if (input.at(ff_dim_t(0)).degree == 1 &&
+             input.at(ff_dim_t(1)).degree > 1) {
+    // case 2: input [N, k/x], weight [k/x, M], output [N, M], degree is x
     out_shape.at(ff_dim_t(1)).degree = input.at(ff_dim_t(1)).degree;
     out_shape.at(ff_dim_t(1)).is_replica_dim = true;
-  } else if(input.at(ff_dim_t(0)).degree > 1 && input.at(ff_dim_t(1)).degree == 1)  {
-    //case 3: input [N/X, K], weight [K, M/X], output [N/X, M], degree is X  
+  } else if (input.at(ff_dim_t(0)).degree > 1 &&
+             input.at(ff_dim_t(1)).degree == 1) {
+    // case 3: input [N/X, K], weight [K, M/X], output [N/X, M], degree is X
     out_shape.at(ff_dim_t(0)).degree = input.at(ff_dim_t(0)).degree;
     out_shape.at(ff_dim_t(0)).is_replica_dim = true;
-  } else if(input.at(ff_dim_t(0)).degree > 1 && input.at(ff_dim_t(1)).degree > 1) {
-    //case 4: input [N/X, K/Y], weight [K/Y, M/X], output [N/X, M/X], degree is X
-    for(int i = 0; i < input.num_dims(); i++) {
+  } else if (input.at(ff_dim_t(0)).degree > 1 &&
+             input.at(ff_dim_t(1)).degree > 1) {
+    // case 4: input [N/X, K/Y], weight [K/Y, M/X], output [N/X, M/X], degree is
+    // X
+    for (int i = 0; i < input.num_dims(); i++) {
       out_shape.at(ff_dim_t(i)).is_replica_dim = true;
       out_shape.at(ff_dim_t(i)).degree = input.at(ff_dim_t(i)).degree;
     }
--- lib/op-attrs/src/pool_2d.cc
+++ lib/op-attrs/src/pool_2d.cc
@@ -2,6 +2,7 @@
 #include "op-attrs/ff_dim.h"
 #include "parallel_dim_mapping_record.h"
 #include "parallel_dim_mapping_record_solver.h"
+#include "utils/exception.h"
 
 namespace FlexFlow {
 
@@ -47,15 +48,21 @@ bool Pool2DAttrs::is_valid(ParallelTensorShape const &input) const {
   return true;
 }
 
-// pytorch: we have two type of pool2d, maxpool2d and avgpool2d
-// input shape: (batch_size, channels, input_height, input_width)
-// for avgpool2d, output shape: (batch_size, channels, 1, 1)
-// for maxpool2d, output shape: (batch_size, channels, output_height,
-// output_width) output_height = (input_height + 2 * padding_h - kernel_h) /
-// stride_h + 1 output_width = (input_width + 2 * padding_w - kernel_w) /
-// stride_w + 1
+// https://pytorch.org/docs/stable/generated/torch.nn.AvgPool2d.html
+// https://pytorch.org/docs/stable/generated/torch.nn.MaxPool2d.html
+//  pytorch: we have two type of pool2d, maxpool2d and avgpool2d
+//  input shape: (batch_size, channels, input_height, input_width)
+//  for avgpool2d, output shape: (batch_size, channels, 1, 1)
+//  for maxpool2d, output shape: (batch_size, channels, output_height,
+//  output_width) output_height = (input_height + 2 * padding_h - kernel_h) /
+//  stride_h + 1 output_width = (input_width + 2 * padding_w - kernel_w) /
+//  stride_w + 1
 ParallelTensorShape get_output_shape(Pool2DAttrs const &attrs,
                                      ParallelTensorShape const &input) {
+
+  if (input.num_dims() != 4) {
+    throw mk_runtime_error("Pool2DAttrs: input shape should be 4D");
+  }
   ParallelTensorShape output_shape = input;
   if (attrs.pool_type == PoolOp::AVG) {
     output_shape.at(ff_dim_t(2)).size = 1;
@@ -70,8 +77,44 @@ ParallelTensorShape get_output_shape(Pool2DAttrs const &attrs,
             attrs.stride_w +
         1;
   } else {
-    assert(false && "unsupported pool type");
+    throw mk_runtime_error("Pool2DAttrs: pool type is not supported");
   }
+
+  // case 1: input:[N, C, H, W], output:[N, C, 1, 1], degree is 1 for avgpool2d
+  // input: [N, C, H, W], output: [N, C, output_height, output_width], degree is  1 for maxpool2d
+  if (input.at(ff_dim_t(2)).degree == 1 && input.at(ff_dim_t(3)).degree == 1) {
+    for (int i = 2; i < input.num_dims(); i++) {
+      output_shape.at(ff_dim_t(i)).is_replica_dim = false;
+      output_shape.at(ff_dim_t(i)).degree = 1;
+    }
+  } else if (input.at(ff_dim_t(2)).degree > 1 &&
+             input.at(ff_dim_t(3)).degree == 1) {
+    // case 2: input [N, C, H/X, W] output [N, C, 1, 1], degree is X
+    // input [N, C, H/X, W] output [N, C, output_height/x, output_width], degree  is X
+    output_shape.at(ff_dim_t(2)).degree = input.at(ff_dim_t(2)).degree;
+    output_shape.at(ff_dim_t(2)).is_replica_dim = true;
+    output_shape.at(ff_dim_t(3)).degree = 1;
+    output_shape.at(ff_dim_t(3)).is_replica_dim = false;
+  } else if (input.at(ff_dim_t(2)).degree == 1 &&
+             input.at(ff_dim_t(3)).degree > 1) {
+    // case 3: input [N, C, H, W/X] output [N, C, 1, 1], degree is X
+    // input [N, C, H, W/X] output [N, C, output_height, output_width/x], degree is X
+    output_shape.at(ff_dim_t(2)).degree = 1;
+    output_shape.at(ff_dim_t(2)).is_replica_dim = false;
+    output_shape.at(ff_dim_t(3)).degree = input.at(ff_dim_t(3)).degree;
+    output_shape.at(ff_dim_t(3)).is_replica_dim = true;
+  } else if (input.at(ff_dim_t(2)).degree > 1 &&
+             input.at(ff_dim_t(3)).degree > 1) {
+    // case 4: input [N, C, H/X, W/Y] output [N, C, 1, 1], degree is X and Y for
+    // avgpool2d input [N, C, H/X, W/Y] output [N, C, output_height/x, output_width/y], degree is X and Y for maxpool2d
+    for (int i = 2; i < input.num_dims(); i++) {
+      output_shape.at(ff_dim_t(i)).is_replica_dim = true;
+      output_shape.at(ff_dim_t(i)).degree = input.at(ff_dim_t(i)).degree;
+    }
+  } else {
+    throw mk_runtime_error("Pool2DAttrs: degree is not supported");
+  }
+
   return output_shape;
 }
 
