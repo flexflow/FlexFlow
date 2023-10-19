33ed1638b419b848c1653e13d087c4b0e6a40a6e
lambda7xx <lambda7xx@gmail.com>
2023-10-19 21:14:04 +0000

    format the code

--- lib/kernels/src/cuda/reshape_kernels.cu
+++ lib/kernels/src/cuda/reshape_kernels.cu
@@ -57,14 +57,14 @@ void forward_kernel(cudaStream_t stream,
                     ReshapePerDeviceState const &m,
                     GenericTensorAccessorR const &input,
                     GenericTensorAccessorW const &output) {
-  DataTypeDispatch1<ForwardKernel>{}(m.data_type, stream, m, input, output);
+  DataTypeDispatch1<ForwardKernel>{}(m.data_type, stream, input, output);
 }
 
 void backward_kernel(cudaStream_t stream,
                      ReshapePerDeviceState const &m,
                      GenericTensorAccessorW const &input,
                      GenericTensorAccessorR const &output) {
-  DataTypeDispatch1<BackwardKernel>{}(m.data_type, stream, m, input, output);
+  DataTypeDispatch1<BackwardKernel>{}(m.data_type, stream, input, output);
 }
 
 } // namespace Reshape
