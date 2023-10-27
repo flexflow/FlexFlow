7e23eb3302ab744f041540bc0d3462e345585cde
lambda7xx <lambda7xx@gmail.com>
2023-10-27 14:46:15 +0000

    add element binary

--- lib/op-attrs/src/element_binary.cc
+++ lib/op-attrs/src/element_binary.cc
@@ -1,4 +1,5 @@
 #include "op-attrs/ops/element_binary.h"
+#include "op-attrs/ff_dim.h"
 #include "utils/exception.h"
 
 namespace FlexFlow {
@@ -7,7 +8,9 @@ ParallelTensorShape get_output_shape(ElementBinaryAttrs const &atts,
                                      ParallelTensorShape const &lhs,
                                      ParallelTensorShape const &rhs) {
   ParallelTensorShape output = lhs.num_dims() >= rhs.num_dims() ? lhs : rhs;
-  for (int i = 0; i < output.num_dims(); i++) {
+  // how to decide its degree  and size for replicate_num
+  output.at(ff_dim_t(0)).is_replica_dim = false;
+  for (int i = 1; i < output.num_dims(); i++) {
     if (i >= lhs.num_dims()) {
       output.at(ff_dim_t(i)) = rhs.at(ff_dim_t(i));
     } else if (i >= rhs.num_dims()) {
