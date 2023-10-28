6c824660f0c6f5d687f28e272e3098853923d7dc
lambda7xx <lambda7xx@gmail.com>
2023-10-28 02:28:21 +0000

    add transpose

--- lib/op-attrs/src/transpose.cc
+++ lib/op-attrs/src/transpose.cc
@@ -4,18 +4,20 @@
 
 namespace FlexFlow {
 
-// assume we have [x, y, z, l], perms is [0,2] we return [z, y, x, l]
+// assume input:[<ri, di1, t>, <x,di2, f> , <y, di3, f>, <z, di4, f>]
+// perem is [1,2]
+// output:[<ri, di1, t>,  <y, di3, f>, <x,di2, f>, <z, di4, f> ]
 ParallelTensorShape get_output_shape(TransposeAttrs const &attrs,
                                      ParallelTensorShape const &input) {
   if (attrs.perm.size() != 2) {
     throw mk_runtime_error("TransposeAttrs: perm.size() != 2");
   }
 
-  auto dim0 = attrs.perm[0];
+  auto dim0 = attrs.perm[0]; // dim0 and dim1 should not be 0
   auto dim1 = attrs.perm[1];
-  if (dim0 < 0 || dim1 < 0 || dim0 >= input.num_dims() ||
+  if (dim0 <= 0 || dim1 <= 0 || dim0 >= input.num_dims() ||
       dim1 >= input.num_dims()) {
-    throw mk_runtime_error("TransposeAttrs: dim0 < 0 || dim1 < 0 || dim0 >= "
+    throw mk_runtime_error("TransposeAttrs: dim0 <= 0 || dim1 <= 0 || dim0 >= "
                            "input.num_dims() || dim1 >= input.num_dims()");
   }
 
@@ -26,10 +28,8 @@ ParallelTensorShape get_output_shape(TransposeAttrs const &attrs,
   output.at(ff_dim_t(dim1)).size = temp;
   output.at(ff_dim_t(dim0)).degree = input.at(ff_dim_t(dim1)).degree;
   output.at(ff_dim_t(dim1)).degree = degree;
-  output.at(ff_dim_t(dim0)).is_replica_dim =
-      output.at(ff_dim_t(dim0)).degree > 1;
-  output.at(ff_dim_t(dim1)).is_replica_dim =
-      output.at(ff_dim_t(dim1)).degree > 1;
+  output.at(ff_dim_t(dim0)).is_replica_dim = dim0 == 0;
+  output.at(ff_dim_t(dim1)).is_replica_dim = dim1 == 0;
   return output;
 }
 
