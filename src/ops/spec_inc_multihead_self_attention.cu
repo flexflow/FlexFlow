3cf49a6d89b9ce60efde018fc99565390ee37eb7
Gabriele Oliaro <goliaro@cs.cmu.edu>
2023-12-12 03:55:52 -0500

    [Documentation] - Annotate attention kernel with shapes of tensors (#1244)
    * add attention shape annotations
    
    * linting
    
    * fix

--- src/ops/spec_inc_multihead_self_attention.cu
+++ src/ops/spec_inc_multihead_self_attention.cu
@@ -492,7 +492,7 @@ void inference_kernel(SpecIncMultiHeadSelfAttentionMeta const *m,
   // compute output production and bias together for all tokens
   int num_tokens =
       bc->num_active_tokens() * BeamSearchBatchConfig::MAX_BEAM_WIDTH;
-        
+
   compute_o_prod_bias(
       m, bc, shard_id, output_ptr, weight_ptr, bias_ptr, num_tokens, stream);
 }
