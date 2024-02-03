64a59d891ae3db48c8234af9bf46fadf48c4bd9b
Gabriele Oliaro <goliaro@cs.cmu.edu>
2024-02-03 17:17:56 +0000

    only run peft_bwd if peft is enabled

--- src/runtime/request_manager.cc
+++ src/runtime/request_manager.cc
@@ -2206,7 +2206,9 @@ GenerationResult RequestManager::generate_incr_decoding(
     BatchConfigFuture bcf =
         prepare_next_batch(next_batch.first, next_batch.second);
     FutureMap fm = im->inference(llm, 0, bcf);
-    im->peft_bwd(llm, 0, bcf);
+    if (llm->config.enable_peft) {
+      im->peft_bwd(llm, 0, bcf);
+    }
     assert(fm.get_future_map_domain().get_volume() == 1);
     InferenceResultFuture irf = fm.get_future(0);
     batch_pipeline.push(std::make_pair(bcf, irf));
