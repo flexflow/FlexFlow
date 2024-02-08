3b22ce30b66be63d7f72b9ef908f0bf40c65a9a7
Reyna Abhyankar <forvirenra@gmail.com>
2024-02-07 19:36:03 -0800

    Fix

--- lib/op-attrs/include/op-attrs/ops/element_unary.h
+++ lib/op-attrs/include/op-attrs/ops/element_unary.h
@@ -17,7 +17,8 @@ CHECK_VALID_OP_ATTR(ElementUnaryAttrs);
 
 struct ElementScalarUnaryAttrs : ElementUnaryAttrs {
   req<float> scalar;
-} FF_VISITABLE_STRUCT(ElementScalarUnaryAttrs, op_type, scalar);
+};
+FF_VISITABLE_STRUCT(ElementScalarUnaryAttrs, op_type, scalar);
 CHECK_VALID_OP_ATTR(ElementScalarUnaryAttrs);
 
 } // namespace FlexFlow
