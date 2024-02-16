a69f6ef070f8ef580a55103d88a0cce0c3c4c5dd
lambda7xx <lambda7xx@gmail.com>
2024-02-16 09:29:22 -0500

    add llam-68m

--- llama2.sh
+++ llama2.sh
@@ -11,3 +11,6 @@
 
 
 #./build/inference/incr_decoding/incr_decoding -ll:gpu 4  -ll:cpu 4 -ll:fsize 20000 -ll:zsize 30000 -llm-model meta-llama/Llama-2-7b-hf -cache-folder /home/xiaoxias/weights --fusion -prompt /home/xiaoxias/chatgpt.json  -pipeline-parallelism-degree 4 > 0215-catalyst-llama2-cuda-graph-bs2-pp4.log 2>&1
+
+
+ ./build/inference/incr_decoding/incr_decoding -ll:gpu 1  -ll:cpu 4 -ll:fsize 20000 -ll:zsize 30000 -llm-model JackFram/llama-68m -cache-folder /home/xiaoxias/weights --fusion -prompt /home/xiaoxias/chatgpt.json > llama2-cuda-graph-bs64-JackFram-llama-68m-1gpu.log 2>&1 
