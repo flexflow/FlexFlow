d24c862aa8ed10d5cf3ddd3f6c8b6122cc35d8cf
lambda7xx <lambda7xx@gmail.com>
2024-02-27 07:53:58 -0500

    delete the unused

--- llama2-specinfer.sh
+++
@@ -1,1 +0,0 @@
-./build/inference/spec_infer/spec_infer -ll:gpu 4 -ll:fsize 14000 -ll:zsize 30000 -llm-model meta-llama/Llama-2-7b-hf -cache-folder /home/xiaoxias/weights  -ssm-model JackFram/llama-68m -prompt /home/xiaoxias/serving/inference/prompt/chatgpt.json -tensor-parallelism-degree 4 --fusion > llama2-speciner_cuda_graph.log 2>&1 
--- op.sh
+++
@@ -1,3 +0,0 @@
-for i in {0..3}; do
-    grep "shard:\[$i\]" llama2-cuda-graph-break-down.log | awk -F' ' '{print $3, $4}' > "shard_${i}_ops_detailed.txt"
-done
