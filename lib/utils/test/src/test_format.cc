e14028c56f1c74282ed74f56d3b9d69f3f23388a
lambda7xx <lambda7xx@gmail.com>
2024-04-08 09:36:00 -0400

    pass the  lib/utils/test/src/test_dot_file.cc

--- lib/utils/test/src/test_format.cc
+++ lib/utils/test/src/test_format.cc
@@ -1,40 +1,40 @@
-// #include "test/utils/doctest.h"
-// #include "utils/record_formatter.h"
+#include "test/utils/doctest.h"
+#include "utils/record_formatter.h"
 
-// std::string formatRecord(RecordFormatter const &formatter) {
-//   std::ostringstream oss;
-//   oss << formatter;
-//   return oss.str();
-// }
+std::string formatRecord(RecordFormatter const &formatter) {
+  std::ostringstream oss;
+  oss << formatter;
+  return oss.str();
+}
 
-// TEST_SUITE(FF_TEST_SUITE) {
-//   TEST_CASE("RecordFormatter") {
-//     RecordFormatter formatter;
-//     SUBCASE("Appending string") {
-//       formatter << "Hello";
-//       formatter << "World";
-//       CHECK(formatRecord(formatter) == "{ Hello | World }");
-//     }
+TEST_SUITE(FF_TEST_SUITE) {
+  TEST_CASE("RecordFormatter") {
+    RecordFormatter formatter;
+    SUBCASE("Appending string") {
+      formatter << "Hello";
+      formatter << "World";
+      CHECK(formatRecord(formatter) == "{ Hello | World }");
+    }
 
-//     SUBCASE("Appending integer and float") {
-//       formatter << 42;
-//       formatter << 3.14f;
-//       CHECK(formatRecord(formatter) == "{ 42 | 3.140000e+00 }");
-//     }
+    SUBCASE("Appending integer and float") {
+      formatter << 42;
+      formatter << 3.14f;
+      CHECK(formatRecord(formatter) == "{ 42 | 3.140000e+00 }");
+    }
 
-//     SUBCASE("Appending another RecordFormatter") {
-//       RecordFormatter subFormatter;
-//       subFormatter << "Sub";
-//       subFormatter << "Formatter";
+    SUBCASE("Appending another RecordFormatter") {
+      RecordFormatter subFormatter;
+      subFormatter << "Sub";
+      subFormatter << "Formatter";
 
-//       RecordFormatter formatter;
-//       formatter << "Hello";
-//       formatter << subFormatter;
+      RecordFormatter formatter;
+      formatter << "Hello";
+      formatter << subFormatter;
 
-//       std::ostringstream oss;
-//       oss << formatter;
+      std::ostringstream oss;
+      oss << formatter;
 
-//       CHECK(formatRecord(formatter) == "{ Hello | { Sub | Formatter } }");
-//     }
-//   }
-// }
+      CHECK(formatRecord(formatter) == "{ Hello | { Sub | Formatter } }");
+    }
+  }
+}
