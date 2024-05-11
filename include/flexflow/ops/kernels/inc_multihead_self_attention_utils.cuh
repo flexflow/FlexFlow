cfae2eefd77f54d8b4f704450209a9040f1c694d
zikun-li <lizikunzk@gmail.com>
2024-05-10 22:50:25 -0400

    Small fix.

--- include/flexflow/ops/kernels/inc_multihead_self_attention_utils.cuh
+++ include/flexflow/ops/kernels/inc_multihead_self_attention_utils.cuh
@@ -523,5 +523,5 @@ struct threads_per_value_t {
 #define test_bit(bit_mask, idx, pos)                                           \
   (((bit_mask)[idx].bits[(pos) / 64] & (1ULL << ((pos) % 64))) != 0)
 
-#endif // _FLEXFLOW_OPS_KERNELS_INC_MULTIHEAD_SELF_UTILS_H
 } // namespace FlexFlow
+#endif // _FLEXFLOW_OPS_KERNELS_INC_MULTIHEAD_SELF_UTILS_H
