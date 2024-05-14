798f4924dd7ac07ca7126c63d3d936d5f048b026
Pietro Max Marsella <marsella@stanford.edu>
2024-05-14 16:34:56 -0700

    Moved cost estimator to separate file

---
+++ lib/compiler/include/compiler/cost_estimator.h
@@ -0,0 +1,32 @@
+#ifndef _FLEXFLOW_COMPILER_MACHINE_MAPPING_H
+#define _FLEXFLOW_COMPILER_MACHINE_MAPPING_H
+
+#include "cost_estimate.h"
+#include "pcg/machine_specification.h"
+#include "pcg/machine_view.h"
+#include "pcg/parallel_computation_graph.h"
+#include "substitutions/sub_parallel_computation_graph.h"
+
+namespace FlexFlow {
+
+float parallel_estimate_cost(
+    SubParallelComputationGraphView const &g,
+    CostEstimator const &estimator,
+    MachineMapping const &device_mapping,
+    std::unordered_map<OpenMultiDiEdge, MachineView> const
+        &frontier_machine_views);
+
+} //FlexFlow
+
+namespace std {
+
+template <>
+struct hash<std::unordered_map<FlexFlow::Node, FlexFlow::MachineMapping>> {
+  size_t operator()(
+      std::unordered_map<FlexFlow::Node, FlexFlow::MachineMapping> const &g)
+      const;
+};
+
+}; // namespace std
+
+#endif
