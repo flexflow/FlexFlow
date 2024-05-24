19d0aba36a0fde632db4858539d138af40e46209
Reyna Abhyankar <forvirenra@gmail.com>
2024-05-24 10:04:36 -0700

    Computation Graph and Builder (#1388)
    * Computation Graph and Builder
    
    * Fix comp graph
    
    * Simplify edge set obtain
    
    ---------
    
    Co-authored-by: Colin Unger <unger@stanford.edu>

--- lib/pcg/src/tensor.cc
+++ lib/pcg/src/tensor.cc
@@ -10,4 +10,8 @@ TensorShape Tensor::get_shape() const {
   return TensorShape(*this);
 }
 
+Tensor construct_tensor_from_output_shape(TensorShape const &shape) {
+  return Tensor{shape.dims, shape.data_type, std::nullopt, false, std::nullopt};
+}
+
 } // namespace FlexFlow
