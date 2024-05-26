8d89acdeee834e4b70caed7505937c5dbc07a121
Gabriele Oliaro <goliaro@cs.cmu.edu>
2024-05-26 22:18:03 +0000

    fix

--- inference/python/spec_infer.py
+++ inference/python/spec_infer.py
@@ -79,7 +79,7 @@ def get_configs():
                     "full_precision": False,
                 }
             ],
-            # "prompt": "",
+            "prompt": "",
             "output_file": "",
         }
         # Merge dictionaries
