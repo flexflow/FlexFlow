55971f26764bcbb1261bdcd5772cf7ebc5a3bcc6
Reyna Abhyankar <forvirenra@gmail.com>
2024-05-30 20:11:42 -0700

    Fix substitutions

--- lib/substitutions/src/operator_attributes.cc
+++ lib/substitutions/src/operator_attributes.cc
@@ -129,7 +129,7 @@ std::optional<OperatorAttributeValue> get_attribute(GatherAttrs const &p,
                                                     OperatorAttributeKey key) {
   switch (key) {
     case OperatorAttributeKey::AXIS:
-      return p.dim;
+      return p.legion_dim;
     default:
       return std::nullopt;
   }
