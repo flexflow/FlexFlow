af3a76af03c8468b153dce73ab796ce69681f2b1
VitalC-3026 <1184649102@qq.com>
2024-06-07 21:20:39 -0400

    tensorrt cmake file

--- cmake/tensorrt-llm.cmake
+++ cmake/tensorrt-llm.cmake
@@ -1,16 +1,16 @@
 message(STATUS "Building TensorRT-LLM from source")
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/tensorrt-llm)
 list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/deps/tensorrt-llm/cpp/tensorrt_llm/include/)
-set(TENSORRT_LIBRARY Tensorrt)
-set(TENSORRT_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/deps/tensorrt-llm/cpp/tensorrt_llm)
-set(TENSORRT_DEF_DIR ${CMAKE_BINARY_DIR}/deps/tensorrt-llm/cpp/tensorrt_llm)
+set(TENSORRT_LLM_LIBRARY TensorRT_LLM)
+set(TENSORRT_LLM_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/deps/tensorrt-llm/cpp/tensorrt_llm)
+set(TENSORRT_LLM_DEF_DIR ${CMAKE_BINARY_DIR}/deps/tensorrt-llm/cpp/tensorrt_llm)
 
-# Add TensorRT to the FlexFlow build
-if(TENSORRT_LIBRARY)
+# Add TensorRTLLM to the FlexFlow build
+if(TENSORRT_LLM_LIBRARY)
     list(APPEND FF_CC_FLAGS -DUSE_TENSORRT)
     list(APPEND FF_NVCC_FLAGS -DUSE_TENSORRT)
-    target_include_directories(flexflow PRIVATE ${TENSORRT_INCLUDE_DIR})
-    target_link_libraries(flexflow PRIVATE ${TENSORRT_LIBRARY})
+    target_include_directories(flexflow PRIVATE ${TENSORRT_LLM_INCLUDE_DIR})
+    target_link_libraries(flexflow PRIVATE ${TENSORRT_LLM_LIBRARY})
 endif()
 
 if (FF_GPU_BACKEND STREQUAL "cuda")
---
+++ cmake/tensorrt.cmake
@@ -0,0 +1,3 @@
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/deps/tensorrt)
+
+list(APPEND FLEXFLOW_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/deps/tensorrt/include/)
