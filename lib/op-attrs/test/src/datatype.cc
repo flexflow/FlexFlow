6830f71426f050e37a0ee2d0a0ea341002ecc1e9
Reyna Abhyankar <forvirenra@gmail.com>
2024-06-19 12:09:44 -0700

    Revert

--- lib/op-attrs/test/src/datatype.cc
+++ lib/op-attrs/test/src/datatype.cc
@@ -19,5 +19,11 @@ TEST_SUITE(FF_TEST_SUITE) {
       RC_PRE(can_strictly_promote_datatype_from_to(l, r));
       RC_ASSERT(!can_strictly_promote_datatype_from_to(r, l));
     });
+
+    RC_SUBCASE("is transitive", [](DataType d1, DataType d2, DataType d3) {
+      RC_PRE(can_strictly_promote_datatype_from_to(d1, d2));
+      RC_PRE(can_strictly_promote_datatype_from_to(d2, d3));
+      RC_ASSERT(can_strictly_promote_datatype_from_to(d1, d3));
+    });
   }
 }
