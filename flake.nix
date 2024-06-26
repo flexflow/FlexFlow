f9b129e7b5a713522386a3470ab815e64bfe1257
Colin Unger <lockshaw@lockshaw.net>
2024-06-26 13:47:08 -0700

    Fix proj url

--- flake.nix
+++ flake.nix
@@ -18,8 +18,7 @@
     flake-utils.url = "github:numtide/flake-utils";
 
     proj-repo = {
-      # url = "github:lockshaw/proj";
-      url = "git+file:///home/lockshaw/x/proj/proj";
+      url = "github:lockshaw/proj";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
