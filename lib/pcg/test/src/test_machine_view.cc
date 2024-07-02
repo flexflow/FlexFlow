6b0d511bef002ac94f0be1c721f72dc2db5a8df1
Pietro Max Marsella <marsella@stanford.edu>
2024-07-02 14:51:54 -0700

    Minor Test correction

--- lib/pcg/test/src/test_machine_view.cc
+++ lib/pcg/test/src/test_machine_view.cc
@@ -16,18 +16,21 @@ TEST_SUITE(FF_TEST_SUITE) {
 
   TEST_CASE("MachineView make_1d_machine_view") {
     StridedRectangle rect{{StridedRectangleSide{num_points_t{7}, 5}}};
-    gpu_id_t start_gpu(1);
-    cpu_id_t start_cpu(3);
-    MachineView gpu_mv{device_id_t{start_gpu}, rect};
-    MachineView cpu_mv{device_id_t{start_cpu}, rect};
+    device_id_t start_gpu{gpu_id_t{1}};
+    device_id_t start_cpu{cpu_id_t{3}};
 
-    CHECK(make_1d_machine_view(start_gpu, gpu_id_t(1 + 7 * 5), 5) == gpu_mv);
+
+    MachineView gpu_mv{start_gpu, rect};
+    MachineView cpu_mv{start_cpu, rect};
+    
+    
+    CHECK(make_1d_machine_view(start_gpu, device_id_t{gpu_id_t(1 + 7 * 5)}, 5) == gpu_mv);
     CHECK(make_1d_machine_view(start_gpu, num_points_t{7}, 5) == gpu_mv);
     CHECK(make_1d_machine_view(start_gpu,
                                get_side_size(rect.sides.at(ff_dim_t{0})),
                                5) == gpu_mv);
 
-    CHECK(make_1d_machine_view(start_cpu, cpu_id_t(3 + 7 * 5), 5) == cpu_mv);
+    CHECK(make_1d_machine_view(start_cpu, device_id_t{cpu_id_t(3 + 7 * 5)}, 5) == cpu_mv);
     CHECK(make_1d_machine_view(start_cpu, num_points_t{7}, 5) == cpu_mv);
     CHECK(make_1d_machine_view(start_cpu,
                                get_side_size(rect.sides.at(ff_dim_t{0})),
