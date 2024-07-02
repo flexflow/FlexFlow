4928c9e52dd83db9b762259e102acc3107bfa521
Pietro Max Marsella <marsella@stanford.edu>
2024-07-02 14:54:53 -0700

    formatting

--- lib/pcg/test/src/test_machine_view.cc
+++ lib/pcg/test/src/test_machine_view.cc
@@ -19,18 +19,18 @@ TEST_SUITE(FF_TEST_SUITE) {
     device_id_t start_gpu{gpu_id_t{1}};
     device_id_t start_cpu{cpu_id_t{3}};
 
-
     MachineView gpu_mv{start_gpu, rect};
     MachineView cpu_mv{start_cpu, rect};
-    
-    
-    CHECK(make_1d_machine_view(start_gpu, device_id_t{gpu_id_t(1 + 7 * 5)}, 5) == gpu_mv);
+
+    CHECK(make_1d_machine_view(
+              start_gpu, device_id_t{gpu_id_t(1 + 7 * 5)}, 5) == gpu_mv);
     CHECK(make_1d_machine_view(start_gpu, num_points_t{7}, 5) == gpu_mv);
     CHECK(make_1d_machine_view(start_gpu,
                                get_side_size(rect.sides.at(ff_dim_t{0})),
                                5) == gpu_mv);
 
-    CHECK(make_1d_machine_view(start_cpu, device_id_t{cpu_id_t(3 + 7 * 5)}, 5) == cpu_mv);
+    CHECK(make_1d_machine_view(
+              start_cpu, device_id_t{cpu_id_t(3 + 7 * 5)}, 5) == cpu_mv);
     CHECK(make_1d_machine_view(start_cpu, num_points_t{7}, 5) == cpu_mv);
     CHECK(make_1d_machine_view(start_cpu,
                                get_side_size(rect.sides.at(ff_dim_t{0})),
