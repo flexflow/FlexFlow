2f3d67a3ce9924e677976df679cfe630358784be
Colin Unger <lockshaw@lockshaw.net>
2024-08-08 00:41:08 -0700

    Add test for permute_input_ids

--- lib/utils/src/utils/graph/open_dataflow_graph/algorithms/permute_input_ids.cc
+++ lib/utils/src/utils/graph/open_dataflow_graph/algorithms/permute_input_ids.cc
@@ -1,92 +1,42 @@
 #include "utils/graph/open_dataflow_graph/algorithms/permute_input_ids.h"
 #include "utils/containers/transform.h"
 #include "utils/overload.h"
+#include "utils/graph/open_dataflow_graph/algorithms/get_graph_data.h"
+#include "utils/graph/open_dataflow_graph/algorithms/from_open_dataflow_graph_data.h"
 
 namespace FlexFlow {
 
-struct PermuteInputIdsView final : public IOpenDataflowGraphView {  
-public:
-  PermuteInputIdsView(OpenDataflowGraphView const &g,
-                      bidict<NewDataflowGraphInput, DataflowGraphInput> const &input_mapping)
-    : g(g), input_mapping(input_mapping) { }
+OpenDataflowGraphView permute_input_ids(OpenDataflowGraphView const &g,
+                                        bidict<NewDataflowGraphInput, DataflowGraphInput> const &input_mapping) {
+  auto new_input_from_old = [&](DataflowGraphInput const &old_input) -> DataflowGraphInput {
+    return input_mapping.at_r(old_input).raw_input;
+  };
 
-  std::unordered_set<Node> query_nodes(NodeQuery const &new_query) const override {
-    return this->g.query_nodes(new_query);
-  }
-
-  std::unordered_set<OpenDataflowEdge> query_edges(OpenDataflowEdgeQuery const &new_query) const override {
-    OpenDataflowEdgeQuery old_query = OpenDataflowEdgeQuery{
-      DataflowInputEdgeQuery{
-        this->old_input_query_set_from_new(new_query.input_edge_query.srcs),
-        new_query.input_edge_query.dst_nodes,
-        new_query.input_edge_query.dst_idxs,
-      },
-      new_query.standard_edge_query,
-    };
-
-    return transform(this->g.query_edges(old_query),
-                     [&](OpenDataflowEdge const &old_edge) { 
-                       return this->new_open_dataflow_edge_from_old(old_edge);
-                     });
-  }
-
-  std::unordered_set<DataflowOutput> query_outputs(DataflowOutputQuery const &new_query) const override {
-    return this->g.query_outputs(new_query);
-  }
-
-  std::unordered_set<DataflowGraphInput> get_inputs() const override {
-    return transform(this->g.get_inputs(), [&](DataflowGraphInput const &old_input) { return this->new_input_from_old(old_input); });
-  }
-
-  PermuteInputIdsView *clone() const override {
-    return new PermuteInputIdsView{
-      this->g,
-      this->input_mapping,
-    };
-  }
-private:
-  query_set<DataflowGraphInput> old_input_query_set_from_new(query_set<DataflowGraphInput> const &new_query_set) const {
-    if (!is_matchall(new_query_set)) {
-      std::unordered_set<DataflowGraphInput> old_nodes = transform(allowed_values(new_query_set),
-                                                     [&](DataflowGraphInput const &new_input) { return this->old_input_from_new(new_input); });
-      return query_set<DataflowGraphInput>{old_nodes};
-    } else {
-      return new_query_set;
-    }
-  }
-
-  OpenDataflowEdge new_open_dataflow_edge_from_old(OpenDataflowEdge const &old_edge) const {
-    return old_edge.visit<OpenDataflowEdge>(overload {
-      [&](DataflowInputEdge const &old) {
+  auto new_edge_from_old = [&](OpenDataflowEdge const &e) {
+    return e.visit<OpenDataflowEdge>(overload {
+      [&](DataflowInputEdge const &input_e) { 
         return OpenDataflowEdge{
           DataflowInputEdge{
-            this->new_input_from_old(old.src),
-            old.dst,
+            new_input_from_old(input_e.src),
+            input_e.dst,
           },
         };
       },
-      [&](DataflowEdge const &old) {
-        return OpenDataflowEdge{old};
+      [&](DataflowEdge const &standard_e) {
+        return OpenDataflowEdge{standard_e};
       },
     });
-  }
+  };
 
-  DataflowGraphInput new_input_from_old(DataflowGraphInput const &old_input) const {
-    return this->input_mapping.at_r(old_input).raw_input;
-  }
+  OpenDataflowGraphData old_data = get_graph_data(g);
+  OpenDataflowGraphData permuted_data = OpenDataflowGraphData{
+    old_data.nodes,
+    transform(old_data.edges, new_edge_from_old),
+    transform(old_data.inputs, new_input_from_old),
+    old_data.outputs,
+  };
 
-  DataflowGraphInput old_input_from_new(DataflowGraphInput const &new_input) const {
-    return this->input_mapping.at_l(NewDataflowGraphInput{new_input});
-  }
-
-private:
-  OpenDataflowGraphView g;
-  bidict<NewDataflowGraphInput, DataflowGraphInput> input_mapping;
-};
-
-OpenDataflowGraphView permute_input_ids(OpenDataflowGraphView const &g,
-                                        bidict<NewDataflowGraphInput, DataflowGraphInput> const &input_mapping) {
-  return OpenDataflowGraphView::create<PermuteInputIdsView>(g, input_mapping);
+  return from_open_dataflow_graph_data(permuted_data);
 }
 
 } // namespace FlexFlow
---
+++ lib/utils/test/src/utils/graph/open_dataflow_graph/algorithms/permute_input_ids.cc
@@ -0,0 +1,78 @@
+#include <doctest/doctest.h>
+#include "utils/containers/get_only.h"
+#include "utils/graph/open_dataflow_graph/algorithms/open_dataflow_graph_data.dtg.h"
+#include "utils/graph/open_dataflow_graph/algorithms/permute_input_ids.h"
+#include "utils/graph/instances/unordered_set_dataflow_graph.h"
+#include "utils/graph/open_dataflow_graph/open_dataflow_graph.h"
+#include "utils/graph/open_dataflow_graph/algorithms/get_graph_data.h"
+
+using namespace ::FlexFlow;
+
+TEST_SUITE(FF_TEST_SUITE) {
+  TEST_CASE("permute_input_ids(OpenDataflowGraphView, bidict<NewDataflowGraphInput, DataflowGraphInput>)") {
+    OpenDataflowGraph g = OpenDataflowGraph::create<
+      UnorderedSetDataflowGraph>();
+
+    DataflowGraphInput i0 = g.add_input();
+    DataflowGraphInput i1 = g.add_input();
+
+    NodeAddedResult n0_added = g.add_node({OpenDataflowValue{i0}}, 1);
+    Node n0 = n0_added.node;
+    DataflowOutput n0_output = get_only(n0_added.outputs);
+
+    NodeAddedResult n1_added = g.add_node({OpenDataflowValue{n0_output}}, 1);
+    Node n1 = n1_added.node;
+    DataflowOutput n1_output = get_only(n1_added.outputs);
+
+    DataflowGraphInput new_i0 = DataflowGraphInput{6};
+    DataflowGraphInput new_i1 = DataflowGraphInput{7};
+
+    bidict<NewDataflowGraphInput, DataflowGraphInput> input_mapping = {
+      {NewDataflowGraphInput{new_i0}, i0},
+      {NewDataflowGraphInput{new_i1}, i1},
+    };
+
+    OpenDataflowGraphView result = permute_input_ids(g, input_mapping);
+    OpenDataflowGraphData result_data = get_graph_data(result);
+
+    OpenDataflowGraphData correct_data = OpenDataflowGraphData{
+      {n0, n1},
+      {
+        OpenDataflowEdge{
+          DataflowInputEdge{
+            new_i0,
+            DataflowInput{
+              n0,
+              0,
+            },
+          },
+        },
+        OpenDataflowEdge{
+          DataflowEdge{
+            DataflowOutput{
+              n0,
+              0,
+            },
+            DataflowInput{
+              n1,
+              0,
+            },
+          },
+        },
+      },
+      {new_i0, new_i1},
+      {
+        DataflowOutput{
+          n0,
+          0,
+        },
+        DataflowOutput{
+          n1,
+          0,
+        },
+      },
+    };
+
+    CHECK(result_data == correct_data);
+  }
+}
