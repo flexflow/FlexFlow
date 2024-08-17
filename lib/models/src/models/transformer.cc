b1f74fc9359049dd7e5ffd1181c487c046077ca9
hsdfzhsdfz <t53507501@gmail.com>
2024-08-17 07:11:23 -0400

    Pass test

--- lib/models/src/models/transformer.cc
+++ lib/models/src/models/transformer.cc
@@ -4,25 +4,28 @@ namespace FlexFlow {
 
 ComputationGraph
     get_transformer_computation_graph(TransformerConfig const &tfConfig) {
+  ComputationGraphBuilder cgb;
+  int kdim = tfConfig.hidden_size / tfConfig.num_heads;
+  int vdim = tfConfig.hidden_size / tfConfig.num_heads;
+
   /**
    * @brief Helper to create an attention encoder layer
    */
-  auto create_attention_encoder = [&](ComputationGraphBuilder *cgb,
-                                      tensor_guid_t const &input,
-                                      int hidden_dim,
-                                      int num_heads,
-                                      int kdim,
-                                      int vdim) -> tensor_guid_t {
-    tensor_guid_t t = cgb->multihead_attention(
-        input, input, input, hidden_dim, num_heads, kdim, vdim);
-    return cgb->dense(
-        cgb->dense(t, hidden_dim, Activation::RELU, false /*bias*/),
-        hidden_dim,
-        Activation::RELU,
+  auto create_attention_encoder =
+      [&](tensor_guid_t const &input) -> tensor_guid_t {
+    tensor_guid_t t = cgb.multihead_attention(input,
+                                              input,
+                                              input,
+                                              tfConfig.hidden_size,
+                                              tfConfig.num_heads,
+                                              kdim,
+                                              vdim);
+    return cgb.dense(
+        cgb.dense(t, tfConfig.hidden_size, Activation::RELU, false /*bias*/),
+        tfConfig.hidden_size,
+        std::nullopt,
         false /*bias*/);
   };
-
-  ComputationGraphBuilder cgb;
 
   TensorShape input_shape = TensorShape{
       TensorDims{FFOrdered<size_t>{
@@ -31,17 +34,11 @@ ComputationGraph
   };
 
   tensor_guid_t input = cgb.create_tensor(input_shape, CreateGrad::YES);
-
   tensor_guid_t t = input;
   for (int i = 0; i < tfConfig.num_layers; i++) {
-    t = create_attention_encoder(&cgb,
-                                 t,
-                                 tfConfig.hidden_size,
-                                 tfConfig.num_heads,
-                                 tfConfig.hidden_size / tfConfig.num_heads,
-                                 tfConfig.hidden_size / tfConfig.num_heads);
+    t = create_attention_encoder(t);
   }
-  t = cgb.dense(t, 1, Activation::RELU, false /*bias*/);
+  t = cgb.dense(t, 1, std::nullopt, false /*bias*/);
 
   return cgb.computation_graph;
 }
--- lib/models/test/src/test_transformer.cc
+++ lib/models/test/src/test_transformer.cc
@@ -5,9 +5,15 @@
 using namespace ::FlexFlow;
 
 TEST_SUITE(FF_TEST_SUITE) {
-  TEST_CASE("TransformerTest") {
-    TransformerConfig config(1024, 1024, 16, 12, 512, 64);
-    ComputationGraph cg = get_transformer_computation_graph(config);
-    CHECK(get_layers(cg).size() == 33);
+  TEST_CASE("get_transformer_computation_graph") {
+    TransformerConfig config = TransformerConfig{1024, 1024, 16, 12, 512, 64};
+
+    ComputationGraph result = get_transformer_computation_graph(config);
+
+    SUBCASE("num layers") {
+      int result_num_layers = get_layers(result).size();
+      int correct_num_layers = 75;
+      CHECK(result_num_layers == correct_num_layers);
+    }
   }
 }
