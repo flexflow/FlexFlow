ba0a174e1dbf3391f250a394da988e7f76ada840
Colin Unger <lockshaw@lockshaw.net>
2024-08-23 20:57:10 -0700

    Cleanup old isomorphism checking code

--- lib/utils/src/utils/graph/open_dataflow_graph/algorithms/find_isomorphism.cc
+++ lib/utils/src/utils/graph/open_dataflow_graph/algorithms/find_isomorphism.cc
@@ -172,30 +172,6 @@ std::unordered_set<OpenDataflowGraphIsomorphism> find_isomorphisms(OpenDataflowG
   }
 
   return result;
-  // OLD CODE
-  // std::vector<Node> src_nodes = as_vector(get_nodes(src));
-  // std::vector<DataflowGraphInput> src_inputs = as_vector(get_inputs(src));
-  //
-  // for (std::vector<Node> const &dst_nodes : get_all_permutations(get_nodes(dst))) {
-  //   for (std::vector<DataflowGraphInput> const &dst_inputs : get_all_permutations(get_inputs(dst))) {
-  //     std::vector<std::pair<Node, Node>> zipped_nodes = zip(src_nodes, dst_nodes);
-  //     bidict<Node, Node> node_mapping{zipped_nodes.cbegin(), zipped_nodes.cend()};
-  //
-  //     std::vector<std::pair<DataflowGraphInput, DataflowGraphInput>> zipped_inputs = zip(src_inputs, dst_inputs);
-  //     bidict<DataflowGraphInput, DataflowGraphInput> input_mapping{zipped_inputs.cbegin(), zipped_inputs.cend()};
-  //
-  //     OpenDataflowGraphIsomorphism candidate_isomorphism = OpenDataflowGraphIsomorphism{
-  //       node_mapping, 
-  //       input_mapping,
-  //     };
-  //
-  //     if (is_isomorphic_under(src, dst, candidate_isomorphism)) {
-  //       return candidate_isomorphism;
-  //     }
-  //   }
-  // }
-  //
-  // return std::nullopt;
 }
 
 std::optional<OpenDataflowGraphIsomorphism> find_isomorphism(OpenDataflowGraphView const &src,
