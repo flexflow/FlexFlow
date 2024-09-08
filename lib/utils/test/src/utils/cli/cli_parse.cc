728b80fe3367f5777240e26bfba49897ef67356e
Colin Unger <lockshaw@lockshaw.net>
2024-09-07 17:03:46 -0700

    Add basic test for cli_parse on raw argv

--- lib/utils/test/src/utils/cli/cli_parse.cc
+++ lib/utils/test/src/utils/cli/cli_parse.cc
@@ -385,4 +385,65 @@ TEST_SUITE(FF_TEST_SUITE) {
       }
     }
   }
+
+  TEST_CASE("cli_parse(CLISpec, int argc, char const * const *argv)") {
+    // most cases are checked in the other overload,
+    // i.e., cli_parse(CLISpec, std::vector<string>), 
+    // so here we just throw in a single check to make sure
+    // nothing has unexpectedly gone wrong
+    CLISpec cli = CLISpec{
+      {
+        CLIFlagSpec{
+          "flag1",
+          'f',
+          std::nullopt,
+        },
+        CLIFlagSpec{
+          "flag2",
+          std::nullopt,
+          std::nullopt,
+        },
+        CLIFlagSpec{
+          "flag3",
+          'a',
+          std::nullopt,
+        },
+      },
+      {
+        CLIPositionalArgumentSpec{
+          "posarg1",
+          std::vector<std::string>{"red", "blue", "green"},
+          std::nullopt,
+        },
+        CLIPositionalArgumentSpec{
+          "posarg2",
+          std::nullopt,
+          std::nullopt,
+        },
+      },
+    };
+    CLIFlagKey key_flag1 = CLIFlagKey{0};
+    CLIFlagKey key_flag2 = CLIFlagKey{1};
+    CLIFlagKey key_flag3 = CLIFlagKey{2};
+    CLIPositionalArgumentKey key_posarg1 = CLIPositionalArgumentKey{0};
+    CLIPositionalArgumentKey key_posarg2 = CLIPositionalArgumentKey{1};
+
+    int argc = 5;
+    char const *argv[] = {"prog_name", "red", "-f", "world", "--flag3"};
+
+    tl::expected<CLIParseResult, std::string> result = cli_parse(cli, argc, argv);
+    tl::expected<CLIParseResult, std::string> correct = CLIParseResult{
+      {
+        {key_flag1, true},
+        {key_flag2, false},
+        {key_flag3, true},
+      },
+      {
+        {key_posarg1, "red"},
+        {key_posarg2, "world"},
+      },
+    };
+
+    CHECK(result == correct);
+  }
 }
