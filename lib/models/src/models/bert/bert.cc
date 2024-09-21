cc8f8d97f584486f3e0e12e2c7d867b3f55e184b
hsdfzhsdfz <t53507501@gmail.com>
2024-09-21 11:34:13 -0400

    Add additional bert configs

--- lib/models/src/models/bert/bert.cc
+++ lib/models/src/models/bert/bert.cc
@@ -1,5 +1,6 @@
 #include "models/bert/bert.h"
 #include "pcg/computation_graph.h"
+#include "pcg/initializers/truncated_norm_initializer_attrs.dtg.h"
 
 namespace FlexFlow {
 
@@ -9,39 +10,63 @@ BertConfig get_default_bert_config() {
                     /*num_encoder_layers=*/12,
                     /*num_heads=*/12,
                     /*dim_feedforward=*/3072,
-                    /*dropout=*/0.1,
+                    /*hidden_act=*/"GELU",
+                    /*hidden_dropout_prob=*/0.1,
+                    /*attention_probs_dropout_prob=*/0.1,
+                    /*initializer_range=*/0.02,
                     /*layer_norm_eps=*/1e-12,
+                    /*classifier_dropout=*/0.1,
                     /*sequence_length=*/512,
                     /*batch_size=*/64);
 }
 
-tensor_guid_t create_feedforward_network(ComputationGraphBuilder &cgb,
-                                         BertConfig const &config,
-                                         tensor_guid_t const &input) {
-  tensor_guid_t layer1_out = cgb.dense(
-      input, config.dim_feedforward, Activation::GELU, /*use_bias=*/true);
-  tensor_guid_t dropout_out = cgb.dropout(layer1_out, config.dropout);
-  tensor_guid_t layer2_out = cgb.dense(dropout_out,
-                                       config.hidden_size,
-                                       /*activation=*/std::nullopt,
-                                       /*use_bias=*/true);
-  return cgb.dropout(layer2_out, config.dropout);
+tensor_guid_t
+    create_feedforward_network(ComputationGraphBuilder &cgb,
+                               BertConfig const &config,
+                               tensor_guid_t const &input,
+                               Activation const &activation,
+                               InitializerAttrs const &kernel_initializer) {
+  tensor_guid_t layer1_out =
+      cgb.dense(input,
+                config.dim_feedforward,
+                activation,
+                /*use_bias=*/true,
+                /*data_type=*/DataType::FLOAT,
+                /*kernel_initializer=*/kernel_initializer);
+  tensor_guid_t dropout_out =
+      cgb.dropout(layer1_out, config.hidden_dropout_prob);
+  tensor_guid_t layer2_out =
+      cgb.dense(dropout_out,
+                config.hidden_size,
+                /*activation=*/std::nullopt,
+                /*use_bias=*/true,
+                /*data_type=*/DataType::FLOAT,
+                /*kernel_initializer=*/kernel_initializer);
+  return cgb.dropout(layer2_out, config.hidden_dropout_prob);
 };
 
-tensor_guid_t create_bert_encoder_layer(ComputationGraphBuilder &cgb,
-                                        BertConfig const &config,
-                                        tensor_guid_t const &input) {
+tensor_guid_t
+    create_bert_encoder_layer(ComputationGraphBuilder &cgb,
+                              BertConfig const &config,
+                              tensor_guid_t const &input,
+                              Activation const &activation,
+                              InitializerAttrs const &kernel_initializer) {
   std::vector<int> layer_norm_axis = {2}; // Normalize the last dim
   int kdim = config.dim_feedforward / config.num_heads;
   int vdim = config.dim_feedforward / config.num_heads;
-  tensor_guid_t self_attention = cgb.multihead_attention(input,
-                                                         input,
-                                                         input,
-                                                         config.hidden_size,
-                                                         config.num_heads,
-                                                         kdim,
-                                                         vdim,
-                                                         config.dropout);
+  tensor_guid_t self_attention =
+      cgb.multihead_attention(input,
+                              input,
+                              input,
+                              config.hidden_size,
+                              config.num_heads,
+                              kdim,
+                              vdim,
+                              /*dropout=*/config.attention_probs_dropout_prob,
+                              /*bias=*/true,
+                              /*add_bias_kv=*/false,
+                              /*add_zero_attn=*/false,
+                              /*initializer=*/kernel_initializer);
   assert(are_tensor_guid_shapes_equivalent(
       cgb.computation_graph, input, self_attention));
 
@@ -52,8 +77,8 @@ tensor_guid_t create_bert_encoder_layer(ComputationGraphBuilder &cgb,
   assert(are_tensor_guid_shapes_equivalent(
       cgb.computation_graph, input, normalized));
 
-  tensor_guid_t feedforward_output =
-      create_feedforward_network(cgb, config, normalized);
+  tensor_guid_t feedforward_output = create_feedforward_network(
+      cgb, config, normalized, activation, kernel_initializer);
   assert(are_tensor_guid_shapes_equivalent(
       cgb.computation_graph, input, feedforward_output));
   return cgb.layer_norm(cgb.add(normalized, feedforward_output),
@@ -64,16 +89,41 @@ tensor_guid_t create_bert_encoder_layer(ComputationGraphBuilder &cgb,
 
 tensor_guid_t create_bert_encoder(ComputationGraphBuilder &cgb,
                                   BertConfig const &config,
-                                  tensor_guid_t const &input) {
+                                  tensor_guid_t const &input,
+                                  Activation const &activation,
+                                  InitializerAttrs const &kernel_initializer) {
   tensor_guid_t t = input;
   for (int i = 0; i < config.num_encoder_layers; i++) {
-    t = create_bert_encoder_layer(cgb, config, t);
+    t = create_bert_encoder_layer(
+        cgb, config, t, activation, kernel_initializer);
   }
   return t;
 };
 
 ComputationGraph get_bert_computation_graph(BertConfig const &config) {
+
+  auto get_activation_type = [&]() -> Activation {
+    if (config.hidden_act == "GELU") {
+      return Activation::GELU;
+    } else if (config.hidden_act == "RELU") {
+      return Activation::RELU;
+    } else if (config.hidden_act == "SIGMOID") {
+      return Activation::SIGMOID;
+    } else if (config.hidden_act == "TANH") {
+      return Activation::TANH;
+    } else {
+      throw mk_runtime_error("The given hidden_act is not supported. The "
+                             "hidden_act string is: {}\n",
+                             config.hidden_act);
+    }
+  };
+
   ComputationGraphBuilder cgb;
+  Activation activation = get_activation_type();
+  InitializerAttrs kernel_initializer = InitializerAttrs{
+      TruncatedNormInitializerAttrs{/*seed=*/0,
+                                    /*mean=*/0,
+                                    /*stddev=*/config.initializer_range}};
 
   TensorShape input_shape = TensorShape{
       TensorDims{FFOrdered<size_t>{
@@ -82,12 +132,16 @@ ComputationGraph get_bert_computation_graph(BertConfig const &config) {
   };
   tensor_guid_t input = cgb.create_tensor(input_shape, CreateGrad::YES);
 
-  tensor_guid_t encoder_output = create_bert_encoder(cgb, config, input);
+  tensor_guid_t encoder_output =
+      create_bert_encoder(cgb, config, input, activation, kernel_initializer);
 
-  tensor_guid_t out_prob = cgb.softmax(cgb.dense(encoder_output,
-                                                 /*outDim=*/config.vocab_size,
-                                                 Activation::GELU,
-                                                 /*use_bias=*/true));
+  tensor_guid_t out_prob =
+      cgb.softmax(cgb.dense(encoder_output,
+                            /*outDim=*/config.vocab_size,
+                            /*activation=*/activation,
+                            /*use_bias=*/true,
+                            /*data_type=*/DataType::FLOAT,
+                            /*kernel_initializer=*/kernel_initializer));
   return cgb.computation_graph;
 }
 
