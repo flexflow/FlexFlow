6f4249fd618b535d31df907c821294ef28d8e8c7
Gabriele Oliaro <goliaro@cs.cmu.edu>
2024-10-11 06:59:09 +0000

    add script

---
+++ run_suffix_decoding.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+set -e
+set -x
+
+# Cd into parent directory of folder holding this script
+cd "${BASH_SOURCE[0]%/*}/build"
+
+# Download models
+python ../inference/utils/download_hf_model.py --half-precision-only meta-llama/Meta-Llama-3-8B Felladrin/Llama-160M-Chat-v1
+
+./suffix_decoding/suffix_decoding \
+    -ll:gpu 4 -ll:cpu 4 -ll:util 4 \
+    -tensor-parallelism-degree 4 \
+    -ll:fsize 20000 -ll:zsize 30000 \
+    -llm-model meta-llama/Meta-Llama-3-8B \
+    -ssm-model Felladrin/Llama-160M-Chat-v1 \
+    -partition-name "" \
+    -prompt ../../suffix_decoding/trace/spider_v2.json \
+    -output-file ../inference/output/spider_v2.out
+
