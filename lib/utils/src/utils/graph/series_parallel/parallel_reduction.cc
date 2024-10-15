44b32f88c47b1a10eeaab6d42843afb9d090d7c0
Pietro Max Marsella <marsella@stanford.edu>
2024-10-14 19:04:17 -0700

    fmt

--- lib/utils/src/utils/graph/series_parallel/parallel_reduction.cc
+++ lib/utils/src/utils/graph/series_parallel/parallel_reduction.cc
@@ -1,9 +1,9 @@
 #include "utils/graph/series_parallel/parallel_reduction.h"
+#include "utils/graph/multidigraph/algorithms/get_edge_counts.h"
 #include "utils/graph/multidigraph/algorithms/get_edges.h"
-#include "utils/graph/node/algorithms.h"
 #include "utils/graph/multidigraph/algorithms/get_incoming_edges.h"
 #include "utils/graph/multidigraph/algorithms/get_outgoing_edges.h"
-#include "utils/graph/multidigraph/algorithms/get_edge_counts.h"
+#include "utils/graph/node/algorithms.h"
 
 namespace FlexFlow {
 
@@ -17,12 +17,16 @@ std::optional<ParallelReduction>
 
   for (auto const &[directed_edge, count] : get_edge_counts(g)) {
 
-    if (count <= 1) {continue;}
+    if (count <= 1) {
+      continue;
+    }
 
-    std::unordered_set<MultiDiEdge> const &outgoing_edges = get_outgoing_edges(g, directed_edge.src);
+    std::unordered_set<MultiDiEdge> const &outgoing_edges =
+        get_outgoing_edges(g, directed_edge.src);
     for (MultiDiEdge const &e1 : outgoing_edges) {
       for (MultiDiEdge const &e2 : outgoing_edges) {
-        if (e1 != e2 && g.get_multidiedge_dst(e1) == g.get_multidiedge_dst(e2)) {
+        if (e1 != e2 &&
+            g.get_multidiedge_dst(e1) == g.get_multidiedge_dst(e2)) {
           return make_parallel_reduction(e1, e2);
         }
       }
